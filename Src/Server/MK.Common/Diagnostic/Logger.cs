d8d0a30b9e4ca49f854a113123ef843080d903bc
apcurium <devnull@localhost>
2012-07-04 16:10:59 -0400

    MIssing file

---
+++ Src/Server/MK.Common/Diagnostic/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace apcurium.MK.Common.Diagnostic
+{
+    public class Logger : ILogger
+    {
+        public void LogError(Exception ex)
+        {
+            Trace.TraceError(ex.Message);
+        }
+
+        public void LogMessage(string message)
+        {
+            Trace.WriteLine(message);            
+        }
+
+        public void LogStack()
+        {
+            
+        }
+    }
+}
