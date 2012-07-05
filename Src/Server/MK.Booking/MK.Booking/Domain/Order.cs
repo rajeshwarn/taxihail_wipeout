3d99f28cd9fafe00f02088deb9c54062bb308da0
apcurium <devnull@localhost>
2012-07-05 17:20:23 -0400

    MKTAXI-43 : Order domain class created

---
+++ Src/Server/MK.Booking/MK.Booking/Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.EventSourcing;
+
+namespace apcurium.MK.Booking.Domain
+{
+    public class Order : EventSourced
+    {
+        protected Order(Guid id)
+            : base(id)
+        {
+        
+    }
+    }
+}
