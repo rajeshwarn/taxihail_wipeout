b73aa9d458c9eb9ad3f6a96023b7fddac7acb898
vincent costel <vincent.costel@Vincent-PC.apcurium.local>
2012-07-05 17:37:43 -0400

    Amend last commit

---
+++ Src/Server/MK.Booking.Api/Validation/SaveFavoriteAddressValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.FluentValidation;
+using ServiceStack.ServiceInterface;
+using apcurium.MK.Booking.Api.Contract.Requests;
+
+namespace apcurium.MK.Booking.Api.Validation
+{
+    public class SaveFavoriteAddressValidator : AbstractValidator<SaveFavoriteAddress>
+    {
+        public SaveFavoriteAddressValidator()
+        {
+            //Validation rules for all requests
+            RuleFor(r => r.AccountId).NotEmpty();
+            RuleFor(r => r.Id).NotEmpty();
+            
+
+            //Validation rules for POST and PUT request
+            RuleSet(ApplyTo.Post | ApplyTo.Put, () =>
+            {
+                RuleFor(r => r.FriendlyName).NotEmpty();
+                RuleFor(r => r.FullAddress).NotEmpty();
+                RuleFor(r => r.Latitude).InclusiveBetween(-90d, 90d);
+                RuleFor(r => r.Longitude).InclusiveBetween(-180d, 180d);
+            });
+        }
+    }
+}
