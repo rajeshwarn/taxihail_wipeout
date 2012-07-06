826e046a1e7d2f4b8b35d8e040c459277f8c4ca7
vincent costel <vincent.costel@Vincent-PC.apcurium.local>
2012-07-06 10:11:18 -0400

    Fixed WebService issue when running multiple fixtures

--- Src/Server/apcurium.MK.Web.Tests/BaseTest.cs
+++ Src/Server/apcurium.MK.Web.Tests/BaseTest.cs
@@ -8,18 +8,21 @@ namespace apcurium.MK.Web.Tests
 {
     public class BaseTest
     {
-        private AppHost _appHost;
+        static readonly AppHost _appHost;
         protected string BaseUrl { get { return "http://localhost:6901/"; }}
 
         protected Account TestAccount { get; set; }
         protected string TestAccountPassword { get { return "password1"; } }
 
-        protected void Setup()
+        static BaseTest()
         {
             Database.DefaultConnectionFactory = new ServiceConfigurationSettingConnectionFactory(Database.DefaultConnectionFactory);
-
             _appHost = new AppHost();
             _appHost.Init();
+        }
+
+        protected void Setup()
+        {
             _appHost.Start(BaseUrl);
 
             var sut = new AccountServiceClient(BaseUrl, null);
