2c3a1fce5acf4f7f2769ac7d3a99e338f0bacf08
matthieugd <devnull@localhost>
2012-07-06 08:45:36 -0400

    MKTAXI-32 update booking settings

---
+++ Src/Server/MK.Booking.Api/Services/BookingSettingsService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Infrastructure.Messaging;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+using apcurium.MK.Booking.Api.Contract.Requests;
+using apcurium.MK.Booking.Commands;
+
+namespace apcurium.MK.Booking.Api.Services
+{
+    public class BookingSettingsService : RestServiceBase<BookingSettingsRequest>
+    {
+        private readonly ICommandBus _commandBus;
+
+        public BookingSettingsService(ICommandBus commandBus)
+        {
+            _commandBus = commandBus;
+            AutoMapper.Mapper.CreateMap<BookingSettingsRequest, Commands.UpdateBookingSettings>();
+        }
+
+        public override object OnPut(BookingSettingsRequest request)
+        {
+            if (!request.AccountId.Equals(new Guid(this.GetSession().UserAuthId)))
+            {
+                throw HttpError.Unauthorized("Unauthorized");
+            }
+            var command = new UpdateBookingSettings();
+            AutoMapper.Mapper.Map(request, command);
+
+            _commandBus.Send(command);
+
+            return new HttpResult(HttpStatusCode.OK);
+        }
+    }
+}
