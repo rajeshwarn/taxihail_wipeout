5cc9d1b9ebe85781a0dea58c402339e44bced206
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2012-08-28 11:47:30 -0400

    MKTAXI-194 add test for special address

--- Src/Server/apcurium.MK.Web.Tests/GeolocFixture.cs
+++ Src/Server/apcurium.MK.Web.Tests/GeolocFixture.cs
@@ -38,6 +38,25 @@ namespace apcurium.MK.Web.Tests
         }
 
         [Test]
+        public void AdvancedNameSearch()
+        {
+            var sut = new GeocodingServiceClient(BaseUrl, SessionId);
+            var addresses = sut.Search("2100 Research Rd, Ottawa MacDonald-Cartier Airport (YOW), Gloucester, ON K1V 2B1, Canada");
+            Assert.AreEqual(1, addresses.Addresses.Count());
+            Assert.AreEqual(true, addresses.Addresses.ElementAt(0).FullAddress.Contains("Research"));
+        }
+
+        [Test]
+        public void SpecialNameSearch()
+        {
+            var sut = new GeocodingServiceClient(BaseUrl, SessionId);
+            //var addresses = sut.Search("265-297 Convair Private, Ottawa MacDonald-Cartier Airport (YOW), Ottawa, ON K1V 1S3, Canada");
+            var addresses = sut.Search("265-297 Convair Private, Ottawa, ON K1V 1S3, Canada");
+            Assert.AreEqual(1, addresses.Addresses.Count());
+            Assert.AreEqual(true, addresses.Addresses.ElementAt(0).FullAddress.Contains("Convair"));
+        }
+
+        [Test]
         public void BasicCoordinateSearch()
         {
             var sut = new GeocodingServiceClient(BaseUrl, SessionId);
