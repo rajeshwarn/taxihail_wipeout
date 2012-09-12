197c00e3612c8e79d8e8aaf0c59cd2c297fd4c81
dboisson <daniel.boissonneault@danielboissonneault.com>
2012-09-12 15:16:05 -0400

    Added files

---
+++ Src/Mobile/Common/ViewModels/AddressViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using apcurium.MK.Booking.Api.Contract.Resources;
+
+namespace apcurium.MK.Booking.Mobile
+{
+	public class AddressViewModel
+	{
+		public AddressViewModel ()
+		{
+		}
+
+		public Address Address { get; set; }
+
+		public bool ShowRightArrow { get; set; }
+		public bool ShowPlusSign { get; set; }
+		public bool IsFirst { get; set; }
+		public bool IsLast { get; set; }
+	}
+}
+
