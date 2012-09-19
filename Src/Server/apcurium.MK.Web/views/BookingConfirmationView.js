00ef5202a0dfb5b5c1a44de125164a12f354619c
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-09-19 14:32:20 -0400

    MKTAXI-68 : call api to update booking settings

--- Src/Server/apcurium.MK.Web/views/BookingConfirmationView.js
+++ Src/Server/apcurium.MK.Web/views/BookingConfirmationView.js
@@ -3,7 +3,8 @@
     TaxiHail.BookingConfirmationView = TaxiHail.TemplatedView.extend({
         events: {
             'click [data-action=book]': 'book',
-            'click [data-action=edit]': 'edit'
+            'click [data-action=edit]': 'edit',
+            'change :text': 'onPropertyChanged',
         },
 
         render: function () {
@@ -23,11 +24,34 @@
             $("input").attr("disabled", !$("input").attr("disabled"));
             if ($("input").attr("disabled")) {
                 $("#editButton").html(TaxiHail.localize('Edit'));
+                jQuery.ajax({
+                    type: 'PUT',
+                    url: 'api/account/bookingsettings',
+                    data: this.model.get('settings'),
+                    success: function () {
+                        
+                    },
+                    dataType: 'json'
+                });
             } else {
                 $("#editButton").html(TaxiHail.localize('Save'));
+                
+               
             }
             
+        },
+        
+        callback : function () {
+            
+        },
+        
+        onPropertyChanged: function (e) {
+            var $input = $(e.currentTarget);
+
+            var settings = this.model.get('settings');
+            settings[$input.attr('name')] = $input.val();
         }
+        
     });
 
 }());
