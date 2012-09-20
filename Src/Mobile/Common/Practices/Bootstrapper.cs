9ff35bc38e59efaa83b0e38dfb80ecfc6d3b5558
Alex <devnull@localhost>
2012-09-20 11:12:00 -0400

    Cleanup IOC registration

--- Src/Mobile/Common/Practices/Bootstrapper.cs
+++ Src/Mobile/Common/Practices/Bootstrapper.cs
@@ -1,34 +1,34 @@
-using System;
-using System.Linq;
-using apcurium.Framework.Extensions;
-using System.Collections.Generic;
-using apcurium.MK.Booking.Mobile.Practices;
-using apcurium.MK.Booking.Mobile.AppServices;
+//using System;
+//using System.Linq;
+//using apcurium.Framework.Extensions;
+//using System.Collections.Generic;
+//using apcurium.MK.Booking.Mobile.Practices;
+//using apcurium.MK.Booking.Mobile.AppServices;
 
-namespace apcurium.MK.Booking.Mobile
-{
-	public class Bootstrapper
-	{
+//namespace apcurium.MK.Booking.Mobile
+//{
+//    public class Bootstrapper
+//    {
 		
-		private IModule[] _modules;
+//        private IModule[] _modules;
 		
-		public Bootstrapper (IModule[] modules)
-		{			
+//        public Bootstrapper (IModule[] modules)
+//        {			
 			
-			var list = new List<IModule>();
-			if ( modules.Count() > 0 )
-			{
-				list.AddRange( modules );
-			}
-			list.Add( new ModuleService() );
-			 _modules = list.ToArray();
-		}
+//            var list = new List<IModule>();
+//            if ( modules.Count() > 0 )
+//            {
+//                list.AddRange( modules );
+//            }
+//            list.Add( new ModuleService() );
+//             _modules = list.ToArray();
+//        }
 		
-		public void Run()
-		{												
-			_modules.ForEach(  m => m.Initialize() );
+//        public void Run()
+//        {												
+//            _modules.ForEach(  m => m.Initialize() );
 			
-		}
-	}
-}
+//        }
+//    }
+//}
 
