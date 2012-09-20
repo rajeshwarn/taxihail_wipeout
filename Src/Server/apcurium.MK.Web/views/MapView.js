b77a80d9402297d31cc7937aedf3fefb9606e5ff
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-09-20 15:26:32 -0400

    MKT-68 : Begin map + geoloc + display pickup and dropoff pin on map

--- Src/Server/apcurium.MK.Web/views/MapView.js
+++ Src/Server/apcurium.MK.Web/views/MapView.js
@@ -3,11 +3,12 @@
     var initialLocation;
 
     var browserSupportFlag = new Boolean();
-
+    var map;
     TaxiHail.MapView = Backbone.View.extend({
         
         initialize: function () {
-
+            
+            
             this.$el.addClass('map-canvas');
             
             var mapOptions = {
@@ -15,15 +16,37 @@
                 center: new google.maps.LatLng(-34.397, 150.644),
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
-            var _map = new google.maps.Map(this.el, mapOptions);
+            map = new google.maps.Map(this.el, mapOptions);
+            var pos;
             
+            this.model.on('change:pickupAddress', function (model, value) {
+                this.addMarker(new google.maps.LatLng(value.latitude, value.longitude), map, 'http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+            }, this);
+
+            this.model.on('change:dropOffAddress', function (model, value) {
+                this.addMarker(new google.maps.LatLng(value.latitude, value.longitude),map,'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+            }, this);
+
+            this.geolocalize();
+            
+            
+
+        },
+        
+        geolocalize : function () {
             // Try W3C Geolocation (Preferred)
             if (navigator.geolocation) {
                 browserSupportFlag = true;
                 navigator.geolocation.getCurrentPosition(function (position) {
                     initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                    _map.setCenter(initialLocation);
-                    
+
+                    map.setCenter(initialLocation);
+                    marker = new google.maps.Marker({
+                        position: initialLocation,
+                        map: map
+
+                    });
+
                 }, function () {
                     handleNoGeolocation(browserSupportFlag);
                 });
@@ -33,19 +56,27 @@
                 browserSupportFlag = false;
                 handleNoGeolocation(browserSupportFlag);
             }
-
-            function handleNoGeolocation(errorFlag) {
-                if (errorFlag == true) {
-                    alert("Geolocation service failed.");
-                   // initialLocation = newyork;
-                } else {
-                    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-                    //initialLocation = siberia;
-                }
-                _map.setCenter(initialLocation);
-            }
-
         },
+        
+            addMarker : function(location, mapc, iconImage) {
+                    marker = new google.maps.Marker({
+            position: location,
+            map: mapc,
+            icon : iconImage
+            });
+                
+        },
+
+        handleNoGeolocation : function (errorFlag) {
+            if (errorFlag == true) {
+                alert("Geolocation service failed.");
+                // initialLocation = newyork;
+            } else {
+                alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+                //initialLocation = siberia;
+            }
+            map.setCenter(initialLocation);
+        }
         
     });
 
