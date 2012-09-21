bb95fc8e87bed42d25c3633fb3ebb60dbfb70a71
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-09-21 13:17:26 -0400

    MKTAXI-282: refactoring of Address Selector

--- Src/Server/apcurium.MK.Web/views/BookView.js
+++ Src/Server/apcurium.MK.Web/views/BookView.js
@@ -8,16 +8,12 @@
         initialize: function () {
             _.bindAll(this, "renderEstimateResults");
             //this.model.on('change', this.render, this);
-            
-
 
             this.model.on('change:pickupAddress', function(model, value) {
-                this.renderAddressControl('.pickup-address-container', new Backbone.Model(value), this.selectPickupAddress);
                 this.actualizeEstimate();
             }, this);
 
             this.model.on('change:dropOffAddress', function(model, value) {
-                this.renderAddressControl('.drop-off-address-container', new Backbone.Model(value), this.selectDropOffAddress);
                 this.actualizeEstimate();
             }, this);
             
@@ -26,8 +22,18 @@
         render: function () {
             this.$el.html(this.renderTemplate(this.model.toJSON()));
 
-            this.renderAddressControl('.pickup-address-container', new Backbone.Model(), this.selectPickupAddress);
-            this.renderAddressControl('.drop-off-address-container', new Backbone.Model(), this.selectDropOffAddress);
+            this.renderAddressControl('.pickup-address-container', new Backbone.Model())
+                .on('selected', function(){
+                    this.model.set({
+                        pickupAddress: model.toJSON()
+                    });
+                }, this);
+            this.renderAddressControl('.drop-off-address-container', new Backbone.Model())
+                .on('selected', function(){
+                    this.model.set({
+                        dropOffAddress: model.toJSON()
+                    });
+                }, this);
 
             return this;
         },
@@ -51,51 +57,15 @@
             //this.render();
         },
 
-        renderAddressControl: function(selector, model, onselect) {
+        renderAddressControl: function(selector, model) {
 
             var addressControlView = new TaxiHail.AddressControlView({
                 model: model
             });
-            addressControlView.on('select', onselect, this);
 
             this.$(selector).html(addressControlView.render().el);
-        },
-        
-        selectPickupAddress: function (model) {
-            this.showAddressList(function (model) {
-                this.model.set({
-                    pickupAddress: model.toJSON()
-                });
-            });
 
-        },
-        
-        selectDropOffAddress: function(model) {
-            this.showAddressList(function (model) {
-                this.model.set({
-                    dropOffAddress: model.toJSON()
-                });
-            });
-            
-        },
-        
-        showAddressList: function (onAddressSelected) {
-
-            var view = new TaxiHail.AddressSelectionView();
-
-            this.$('#pickup-drop-off-container').addClass('hidden-left');
-
-            this.$('#address-list-container')
-                .removeClass('hidden-right')
-                .html(view.render().el);
-
-            view.on('selected', function () {
-
-                onAddressSelected.apply(this, arguments);
-                this.$('#pickup-drop-off-container').removeClass('hidden-left');
-                this.$('#address-list-container').addClass('hidden-right')
-                
-            }, this);
+            return addressControlView;
         },
         
         book: function (e) {
