29d8d91c823b6bc698923f5c82ef30af978262d8
dboisson <daniel.boissonneault@danielboissonneault.com>
2012-09-25 19:41:04 -0400

    iOs: - alex's list of bugs and changes

--- Src/Mobile/Common/Infrastructure/IMessageService.cs
+++ Src/Mobile/Common/Infrastructure/IMessageService.cs
@@ -9,6 +9,7 @@ namespace apcurium.MK.Booking.Mobile.Infrastructure
     {
         void ShowMessage(string title, string message);
 		void ShowMessage(string title, string message, string additionnalActionButtonTitle, Action additionalAction );
+		void ShowProgress( bool show );
 
         void ShowToast(string message, ToastDuration duration);
     }
