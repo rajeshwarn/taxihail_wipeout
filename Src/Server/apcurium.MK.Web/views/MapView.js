93d107b013d99872d343f1fb6898503dfd39bce1
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-09-26 17:44:22 -0400

    MKT-288: add target and adapt pin position according to window height

--- Src/Server/apcurium.MK.Web/views/MapView.js
+++ Src/Server/apcurium.MK.Web/views/MapView.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    
+    var offsetY = -200;
     TaxiHail.MapView = Backbone.View.extend({
         
         events: {
@@ -10,8 +10,6 @@
             _.bindAll(this, "geoloc");
         },
         
-        
-        
         setModel: function(model) {
             if(this.model) {
                 this.model.off(null, null, this);
@@ -20,26 +18,50 @@
 
             this.model.on('change:pickupAddress', function (model, value) {
                 var location = new google.maps.LatLng(value.latitude, value.longitude);
+                var loc = this.offsetX(location, 0, offsetY);
                 if (this._pickupPin) {
+                   // this.offsetCenter(location, 0, -200);
+
                     this._pickupPin.setPosition(location);
+
                 } else {
                     this._pickupPin = this.addMarker(location, 'http://maps.google.com/mapfiles/ms/icons/green-dot.png');
                 }
-                this._map.setCenter(location);
+                
+                this._map.setCenter(loc);
+                //this._map.setCenter(location);
             }, this);
 
             this.model.on('change:dropOffAddress', function (model, value) {
                 var location = new google.maps.LatLng(value.latitude, value.longitude);
+                var loc = this.offsetX(location, 0, offsetY);
                 if (this._dropOffPin) {
                     this._dropOffPin.setPosition(location);
                 } else {
                     this._dropOffPin = this.addMarker(location, 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
                 }
+                this._map.setCenter(loc);
+            }, this);
+            
+
+            offsetY = -(this.$el.height() / 4);
+            var targetX = ( this.$el.width() / 2 ) -13;
+            var targetY = ( this.$el.height() / 1.5) + 45 ;
+            this.model.on('change:isPickupActive change:isDropOffActive', function (model, value) {
+                if (model.get('isPickupActive') == true || model.get('isDropOffActive') == true) {
+                    if ($("#target-icon").length==0) {
+                        this.$el.append($('<img id="target-icon" src="themes/generic/img/target.png"  style=" width : 25px ; position : absolute; top :' + targetY + 'px ; left :' + targetX + 'px;">'));
+                    }
+                } else {
+                    $("#target-icon").remove();
+                }
             }, this);
             
         },
+        
+        
+
            
-            
         render: function() {
 
             var mapOptions = {
@@ -47,13 +69,16 @@
                 center: new google.maps.LatLng(-34.397, 150.644),
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 zoomControlOptions: {
-                    position: google.maps.ControlPosition.LEFT_CENTER
+                    position: google.maps.ControlPosition.LEFT_CENTER,
+
                 }
 
             };
             this._map = new google.maps.Map(this.el, mapOptions);
             google.maps.event.addListener(this._map, 'dragend', this.geoloc);
 
+            
+            
 
             return this;
 
@@ -72,32 +97,55 @@
         geoloc: function () {
             if (this.model.get('isPickupActive') == true || this.model.get('isDropOffActive') == true) {
                 if (this.model.get('isPickupActive')) {
-               if (this._pickupPin) {
-                this._pickupPin.setPosition(this._map.getCenter());
-            } else {
-                this._pickupPin = this.addMarker(this._map.getCenter(), 'http://maps.google.com/mapfiles/ms/icons/green-dot.png');
-            }
-            
-            TaxiHail.geocoder.geocode(this._pickupPin.getPosition().Xa, this._map.getCenter().Ya)
-                        .done(this.geolocdone);
+                    if (this._pickupPin) {
+                        var loc = this.offsetX(this._map.getCenter(), 0, -offsetY);
+                        this._pickupPin.setPosition(loc);
+                    } else {
+
+                        this._pickupPin = this.addMarker(this._map.getCenter(), 'http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+
+                    }
+                    //TaxiHail.geocoder.geocode(this.offsetX(this._map.getCenter(), 0, 200).Xa, this.offsetX(this._map.getCenter(), 0, -offsetY).Ya)
+                   //     .done(this.geolocdone);
             } else {
                 if (this._dropOffPin) {
-                    this._dropOffPin.setPosition(this._map.getCenter());
+                    //this._dropOffPin.setPosition(this._map.getCenter());
+                    var loc = this.offsetX(this._map.getCenter(), 0, -offsetY);
+                    this._dropOffPin.setPosition(loc);
                 } else {
                     this._dropOffPin = this.addMarker(this._map.getCenter(), 'http://maps.google.com/mapfiles/ms/icons/red-dot.png');
                 }
 
-                TaxiHail.geocoder.geocode(this._dropOffPin.getPosition().Xa, this._map.getCenter().Ya)
-                            .done(this.geolocdone);
+                    //TaxiHail.geocoder.geocode(this.offsetX(this._map.getCenter(), 0, -offsetY).Xa, this.offsetX(this._map.getCenter(), 0, -offsetY).Ya)
+                    //        .done(this.geolocdone);
             }
             }
-            
             
         },
+        
+        offsetX: function (latlng, offsetx, offsety) {
 
-        addMarker : function(location, iconImage) {
+
+            var point1 = this._map.getProjection().fromLatLngToPoint(
+        (latlng instanceof google.maps.LatLng) ? latlng : this._map.getCenter()
+           );
+            var point2 = new google.maps.Point(
+                ((typeof (offsetx) == 'number' ? offsetx : 0) / Math.pow(2, this._map.getZoom())) || 0,
+                ((typeof (offsety) == 'number' ? offsety : 0) / Math.pow(2, this._map.getZoom())) || 0
+            );
+            return this._map.getProjection().fromPointToLatLng(new google.maps.Point(
+                point1.x - point2.x,
+                point1.y + point2.y));
+            
+            
+
+        },
+
+
+        addMarker: function (location, iconImage) {
+            var loc = this.offsetX(location, 0, -offsetY);
             return new google.maps.Marker({
-                position: location,
+                position: loc,
                 map: this._map,
                 icon: iconImage
             } );
