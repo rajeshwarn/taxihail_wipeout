4b9a1df189b846647eda7aef6e5a5c900df8a5ab
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-09-27 18:15:27 -0400

    [WebApp] refactor : merge booksettingsview and bookconfirmationview, thus we can use settings control along pickup control

--- Src/Server/apcurium.MK.Web/views/BookingConfirmationView.js
+++ Src/Server/apcurium.MK.Web/views/BookingConfirmationView.js
@@ -6,8 +6,9 @@
         
         events: {
             'click [data-action=book]': 'book',
-            'change :text': 'onPropertyChanged',
-            
+            'change :text[data-action=changepickup]': 'onPickupPropertyChanged',
+            'change :text[data-action=changesettings]': 'onSettingsPropertyChanged',
+            'change :input[data-action=changesettings]': 'onSettingsPropertyChanged',
         },
         initialize: function () {   
 
@@ -18,12 +19,39 @@
             if (pickup && dest) {
                 TaxiHail.directionInfo.getInfo(pickup['latitude'], pickup['longitude'], dest['latitude'], dest['longitude']).done(this.renderResults);
             }
+            
+
+
+            this.referenceData = new TaxiHail.ReferenceData();
+            this.referenceData.fetch();
+            this.referenceData.on('change', this.render, this);
+
         },
 
         render: function (param) {
 
             this.$el.html(this.renderTemplate(this.model.toJSON()));
-            this.renderItem(this.model);
+            //this.renderItem(this.model);
+            
+
+            Handlebars.registerHelper('ifCond', function (v1, v2, options) {
+                if (v1 == v2) {
+                    return options.fn(this);
+                } else {
+                    return options.inverse(this);
+                }
+            });
+
+            var data = this.model.toJSON();
+
+            _.extend(data, {
+                vehiclesList: this.referenceData.attributes.vehiclesList,
+                paymentsList: this.referenceData.attributes.paymentsList
+            });
+
+            this.$el.html(this.renderTemplate(data));
+
+
             return this;
         },
         
@@ -39,7 +67,7 @@
         book: function (e) {
             this.$('#bookBt').button('loading');
             e.preventDefault();
-            this.model.set('settings', settings);
+            //this.model.set('settings', settings);
             this.model.save({},{success : function (model) {
                 TaxiHail.app.navigate('status/' + model.id, { trigger: true });
                 },
@@ -64,22 +92,30 @@
             this.$('.errors').html($alert);
         },
         
-        renderItem: function (model) {
+        /*renderItem: function (model) {
 
             var settingsView = new TaxiHail.SettingsEditView({
                 model: settings = new TaxiHail.Settings(model.get('settings')) 
             });
 
             this.$('div#settingsContent').prepend(settingsView.render().el);
-        },
+        },*/
         
-        onPropertyChanged: function (e) {
+        onPickupPropertyChanged: function (e) {
             var $input = $(e.currentTarget);
             var pickup = this.model.get('pickupAddress');
             
             pickup[$input.attr("name")] = $input.val();
             settingschanged = true;
         },
+        
+        onSettingsPropertyChanged : function (e) {
+            var $input = $(e.currentTarget);
+            var pickup = this.model.get('settings');
+
+            pickup[$input.attr("name")] = $input.val();
+            settingschanged = true;
+        }
         
     });
 
