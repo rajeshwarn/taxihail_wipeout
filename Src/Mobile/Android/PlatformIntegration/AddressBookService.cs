cfab33c9bef3325eb40ceb31963dfa47e7c23902
dboisson <daniel.boissonneault@danielboissonneault.com>
2012-09-28 17:08:22 -0400

    iOS: - changed xamarin.mobile.dll for an experimental all that deactivate compass calibration prompt
    Android:
    - made modifications for Contact search (IAddressBookService and AddressBookService)

--- Src/Mobile/Android/PlatformIntegration/AddressBookService.cs
+++ Src/Mobile/Android/PlatformIntegration/AddressBookService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Xamarin.Contacts;
 using System.Linq;
 using apcurium.MK.Booking.Mobile.Infrastructure;
+using TinyIoC;
 
 
 namespace apcurium.MK.Booking.Mobile.Client
@@ -18,7 +19,7 @@ namespace apcurium.MK.Booking.Mobile.Client
 			var book = TinyIoCContainer.Current.Resolve<AddressBook>();
 	        book.PreferContactAggregation = true;
 
-			return book.ToList();;
+			return book.ToList();
 		}
 
 	}
