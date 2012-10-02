547c68bd02809209983c5c1318dec5f7e874f31d
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-10-02 18:00:42 -0400

    MKTAXI-313: nicely formatted date

--- Src/Server/apcurium.MK.Web/utils.js
+++ Src/Server/apcurium.MK.Web/utils.js
@@ -24,5 +24,38 @@
         return new Handlebars.SafeString(TaxiHail.localize(resourceName, this.resourceSet));
     });
 
+    Handlebars.registerHelper('niceDate', function (date) {
+        if(_.isString(date) && date.indexOf('-') && date.indexOf('T') && date.indexOf(':'))
+        {
+            // Wild assumption that we have a date in the format : yyyy-mm-ddThh:mm:ss
+            var parts = date.split('T');
+            var dateParts = parts[0].split('-');
+            var timeParts = parts[1].split(':');
+            if(dateParts.length === 3 && timeParts.length >= 2) {
+                var year = dateParts[0];
+                var month = dateParts[1];
+                var day = dateParts[2];
+                var hour = timeParts[0];
+                var minute = timeParts[1];
+
+                var meridian = "AM";
+                if (hour === 0) {
+                    hour = 12;
+                } else if (hour >= 12) {
+                    if (hour > 12) {
+                        hour = hour - 12;
+                    }
+                    meridian = "PM";
+                } else {
+                   meridian = "AM";
+                }
+
+                return new Handlebars.SafeString(month + '/' + day + '/' + year + '\u00A0@\u00A0' + hour + ":" + minute + "\u00A0" + meridian);
+            }
+        }
+        // not needed yet
+        return '';
+    });
+
 
 }());
