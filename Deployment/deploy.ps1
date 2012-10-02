19121245622631cd6839d6a0ee8b2fdbfc717279
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2012-10-02 07:54:48 -0400

    TAXIHAILOPS-1 deploy web app

--- Deployment/deploy.ps1
+++ Deployment/deploy.ps1
@@ -1,28 +1,67 @@
-﻿$scriptpath = $MyInvocation.MyCommand.Path
+﻿Import-Module -Name WebAdministration #see cmdlets http://technet.microsoft.com/en-us/library/ee790599.aspx
+$scriptpath = $MyInvocation.MyCommand.Path
 $base_dir = Split-Path $scriptpath
 Write-Host "***************Base Directory $base_dir *************************"
-. $base_dir'\deployProps.ps1'
+#. $base_dir'\deployProps.ps1'
+$companyName= "MyCompany"
+$dbtoolPath = "$base_dir\DatabaseInitializer\"
+$dbTool = "$dbtoolPath\DatabaseInitializer.exe" 
 
-Write-Host "***************Stop Website $companyName ************************"
-Import-Module -Name WebAdministration
-Stop-WebSite -Name $companyName
+$websiteFiles = "$base_dir\Website\"
+$deployWwwRoot = "C:\Data\TaxiHail"
+$site = 'Default Web Site'
+
+$version = Read-Host "Version ?"
+
+$existingWebApp = Get-WebApplication -Name $companyName
+if($existingWebApp)
+{
+    Write-Host "***************Stop WebApp Pool $companyName ************************"  
+    Stop-WebAppPool $companyName
+}
+  
 
 
-$actionDb = Read-Host "[C]reate (with delete existing) or [U]pdate database ? C/U"
+$deplyoDB = Read-Host "Deploy Database? Y/N"
+if($deplyoDB -eq 'Y')
+{
+    Write-Host "***************Start Deploy DB ************************" 
+    $actionDb = Read-Host "[C]reate (with delete existing) or [U]pdate database? C/U"
 
-$psi = New-Object System.Diagnostics.ProcessStartInfo($dbTool, "$companyName $actionDb")
-$psi.FileName = $dbTool
-$psi.Arguments = "$companyName $actionDb"
-$psi.WorkingDirectory = $dbtoolPath
-$process = [Diagnostics.Process]::Start($psi)
-$process.WaitForExit()
+    $psi = New-Object System.Diagnostics.ProcessStartInfo($dbTool, "$companyName $actionDb")
+    $psi.FileName = $dbTool
+    $psi.Arguments = "$companyName $actionDb"
+    $psi.WorkingDirectory = $dbtoolPath
+    $process = [Diagnostics.Process]::Start($psi)
+    $process.WaitForExit()
+    Write-Host "***************End Deploy DB ************************" 
+ }
+
+ $deplyoWebsite = Read-Host "Deploy WebSite? Y/N"
+if($deplyoWebsite -eq 'Y')
+{
+    Write-Host "***************Start Deploy WebSite ************************" 
+    $targetDir = "$deployWwwRoot\$companyname\$version"
+    #create dir and copy files
+    New-Item $targetDir -type directory    
+    Get-ChildItem $websiteFiles -Recurse | Copy-Item -Filter "*.*" -Force -Destination {Join-Path $targetDir $_.FullName.Substring($websiteFiles.length)} 
+    #change IIS configuration  
+    if($existingWebApp)
+    {
+        Remove-WebApplication -Site $site  -Name $companyName
+        New-WebApplication -Site $site  -Name $companyName -PhysicalPath $targetDir -ApplicationPool $companyName        
+
+    }else{
+
+        $appPool = New-WebAppPool -Name $companyName
+        $existingWebApp = New-WebApplication -Site $site  -Name $companyName -PhysicalPath $targetDir -ApplicationPool $appPool.Name
+    }
+    Write-Host "***************End Deploy WebSite ************************"
+}
 
 
-#here deploy website
 
-#change connectionString
-Set-WebConfigurationProperty '/connectionStrings/add[@name="MkWeb"]' -PSPath "IIS:\sites\$companyName" -Name "connectionString" -Value "DataSource=.;Initial Catalog=$companyName;Integrated Security=True; MultipleActiveResultSets=True"
 
 
 Write-Host "***************Start Website $companyName ************************"
-Start-WebSite -Name $companyName
+Start-WebAppPool -Name $companyName
--- Deployment/deployProps.ps1
+++
@@ -1,3 +0,0 @@
-﻿$companyName= "MyCompany"
-$dbtoolPath = "C:\Users\matthieu.duluc\Documents\Dev\MkTaxi\Src\Server\DatabaseInitializer\bin\Debug";
-$dbTool = "$dbtoolPath\DatabaseInitializer.exe"; 
