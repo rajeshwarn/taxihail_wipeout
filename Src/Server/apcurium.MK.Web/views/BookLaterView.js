f4a23414063e1f7742576416aba1d6d559b80620
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-10-02 09:24:00 -0400

    MKTAXI-313: added missing files to solution

--- Src/Server/apcurium.MK.Web/views/BookLaterView.js
+++ Src/Server/apcurium.MK.Web/views/BookLaterView.js
@@ -2,6 +2,12 @@
 
     TaxiHail.BookLaterView = TaxiHail.TemplatedView.extend({
 
+        tagName: 'form',
+
+        events: {
+            'submit': 'onsubmit'
+        },
+
         render: function() {
 
             var now = new Date();
@@ -14,6 +20,17 @@
             this.$('[data-role=timepicker]').timepicker();
 
             return this;
+
+        },
+
+        onsubmit: function(e) {
+            e.preventDefault();
+            var date = this.$('[data-role=datepicker] :text').val();
+            var time = this.$('[data-role=timepicker]').val();
+
+            this.model.set('pickupDate', date + 'T' + time);
+            this.model.saveLocal();
+            TaxiHail.app.navigate('confirmationbook', { trigger: true });
 
         }
 
