5eaa1ecc8ab2b023b01772379b1eee428ac325b3
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-10-02 17:20:12 -0400

    MKT-71 : Begin order history ( screen + display ibs order id )

--- Src/Server/apcurium.MK.Web/taxi-hail.js
+++ Src/Server/apcurium.MK.Web/taxi-hail.js
@@ -35,8 +35,8 @@ TaxiHail.loader = {
 };
 $(function () {
     TaxiHail.loader.load(
-        /* Localizations */["Home", "Book", "Login", "AddressSelection", "BookingConfirmation", "SettingsEdit", "Signup", "LoginStatus", "Map", "BookingStatus", "Profile", "UpdatePassword", "ResetPassword", "Global"],
-        /* Templates*/["Home", "Book", "Login", "AddressSelection", "AddressItem", "AddressControl", "AddressSearch", "BookingConfirmation", "SettingsEdit", "Signup", "LoginStatus", "BookingStatus", "Profile", "UserAccount", "UpdatePassword", "ResetPassword"],
+        /* Localizations */["Home", "Book", "Login", "AddressSelection", "BookingConfirmation", "SettingsEdit", "Signup", "LoginStatus", "Map", "BookingStatus", "Profile", "UpdatePassword", "ResetPassword", "OrderHistory", "Global"],
+        /* Templates*/["Home", "Book", "Login", "AddressSelection", "AddressItem", "AddressControl", "AddressSearch", "BookingConfirmation", "SettingsEdit", "Signup", "LoginStatus", "BookingStatus", "Profile", "UserAccount", "UpdatePassword", "ResetPassword", "OrderHistory"],
         function () {
 
             // Application starts here
---
+++ Src/Server/apcurium.MK.Web/views/OrderHistoryView.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    TaxiHail.OrderHistoryView = TaxiHail.TemplatedView.extend({
+        events: {
+        },
+
+        initialize: function () {
+            
+        },
+
+        render: function () {
+            this.$el.html(this.renderTemplate());
+
+            this.$el.empty();
+            if (this.collection.length) {
+                this.collection.each(this.renderItem, this);
+            } else {
+                this.$el.append($('<li>').addClass('no-result').text(TaxiHail.localize('search.no-result')));
+            }
+
+
+            return this;
+        },
+        
+        renderItem: function (model) {
+
+            this.$el.append('Ibs Order Id : ' + model.get('iBSOrderId'));
+        }
+
+
+       
+    });
+
+}());
