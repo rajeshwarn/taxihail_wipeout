99399101a67dd399590f2112a31715930fa79089
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-10-02 15:16:46 -0400

    MKTAXI-313: Date/Time parsing and order creation with pickup date

--- Src/Server/apcurium.MK.Web/views/BookLaterView.js
+++ Src/Server/apcurium.MK.Web/views/BookLaterView.js
@@ -25,15 +25,49 @@
 
         onsubmit: function(e) {
             e.preventDefault();
-            var date = this.$('[data-role=datepicker] :text').val();
-            var time = this.$('[data-role=timepicker]').val();
+            var date = this.$('[data-role=datepicker]').data('datepicker').date,
+                hour = this.$('[data-role=timepicker]').data('timepicker').hour,
+                minute = this.$('[data-role=timepicker]').data('timepicker').minute,
+                meridian = this.$('[data-role=timepicker]').data('timepicker').meridian;
 
-            this.model.set('pickupDate', date + 'T' + time);
+            if(meridian.toUpperCase() === "PM") {
+                if(hour < 12) {
+                    hour+= 12;
+                }
+            } else if(meridian.toUpperCase() === "AM") {
+                if(hour === 12) {
+                    hour = 0;
+                }
+            }
+
+            var pickupDate = new Date(date.toString());
+            pickupDate.setHours(hour);
+            pickupDate.setMinutes(minute);
+
+
+            this.model.set('pickupDate', toISO8601(pickupDate));
             this.model.saveLocal();
             TaxiHail.app.navigate('confirmationbook', { trigger: true });
 
         }
 
     });
+
+    function toISO8601(date) {
+        var year = date.getFullYear(),
+            month = date.getMonth() + 1,
+            day = date.getDate(),
+            hour = date.getHours(),
+            minute = date.getMinutes(),
+            second = date.getSeconds();
+
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+        hour = hour < 10 ? '0' + hour : hour;
+        minute = minute < 10 ? '0' + minute : minute;
+        second = second < 10 ? '0' + second : second;
+
+        return year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':' + second;
+    }
 
 }());
