57918ec20e8aad343d0cf3f872597c2ab8081c9d
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2012-10-03 16:30:15 -0400

    MKTAXI-296 add spinner to loading of favorites

--- Src/Server/apcurium.MK.Web/views/AddressSelectionView.js
+++ Src/Server/apcurium.MK.Web/views/AddressSelectionView.js
@@ -170,14 +170,33 @@
             },
 
             places: function () {
+
+                var opts = {
+                    lines: 11, // The number of lines to draw
+                    length: 3, // The length of each line
+                    width: 3, // The line thickness
+                    radius: 6, // The radius of the inner circle
+                    corners: 1, // Corner roundness (0..1)
+                    rotate: 0, // The rotation offset
+                    speed: 1, // Rounds per second
+                    trail: 60, // Afterglow percentage
+                    shadow: false, // Whether to render a shadow
+                    hwaccel: false, // Whether to use hardware acceleration
+                    className: 'spinner-address', // The CSS class to assign to the spinner
+                    zIndex: 2e9 // The z-index (defaults to 2000000000)
+                };
+            
+               
+                var spinner = new Spinner(opts).spin();
+                this.$('.tab-content').html(spinner.el);
                 
-                TaxiHail.geolocation.getCurrentPosition()
-                        .done(TaxiHail.postpone(_.bind(function (address) {
-                            this.fetchPlaces(address.latitude, address.longitude);
-                        }, this)))
-                        .fail(_.bind(function () {
-                            this.fetchPlaces(TaxiHail.parameters.defaultLatitude, TaxiHail.parameters.defaultLongitude);
-                        }, this));
+               TaxiHail.geolocation.getCurrentPosition()
+                .done(TaxiHail.postpone(_.bind(function (address) {
+                    this.fetchPlaces(address.latitude, address.longitude);
+                }, this)))
+                .fail(_.bind(function () {
+                    this.fetchPlaces(TaxiHail.parameters.defaultLatitude, TaxiHail.parameters.defaultLongitude);
+                }, this));
             }
         },
         
