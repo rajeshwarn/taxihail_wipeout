97d59260f73a8c8228629642c952b7159897d626
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2012-10-04 08:32:01 -0400

    manage error code in database initializer

---
+++ Deployment/deploy.Dev.ps1
@@ -0,0 +1,19 @@
+﻿$companyName= "MkWebDev" 
+
+$sqlServerInstance = "MSSQL11.MSSQLSERVER" 
+
+$dbtoolPath = "C:\Users\matthieu.duluc\Documents\Dev\MkTaxi\Src\Server\DatabaseInitializer\bin\Debug" 
+
+$deplyoDB = 'Y'
+
+$actionDb = "C" 
+
+$deplyoWebsite = 'N' 
+
+$websiteFiles = "C:\Data\teamcity\server\buildAgent\work\ada9756f55920a09\Src\Server\apcurium.MK.Web\obj\\Debug\Package\PackageTmp\" 
+
+$deployWwwRoot = "C:\Data\TaxiHail" 
+
+$version = "1.0" + [DateTime]::Now.Ticks
+
+$site = 'Default Web Site'
