a2b52e0cbe494919d58b755ec857a3f710452b36
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2012-10-15 17:05:07 -0400

    config styletransportation

---
+++ Deployment/Server/deploy.StyleTransportation.ps1
@@ -0,0 +1,20 @@
+﻿#see staging file for documentation
+$companyName= "StyleTransportation" 
+
+$sqlServerInstance = "MSSQL10_50.MSSQLSERVER"
+
+$deployWwwRoot = "C:\Data\TaxiHail Sites"
+
+$version = "1.1." + [DateTime]::Now.Ticks #todo replace with tag...
+
+$dbtoolPath = "$base_dir\DatabaseInitializer"
+
+$websiteFiles = "$base_dir\WebSites\"
+
+$site = 'Default Web Site'
+
+$deplyoDB = 'Y'
+
+$actionDb = "U"
+
+$deplyoWebsite = 'Y'
