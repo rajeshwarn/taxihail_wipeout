f7eadf9b2115b98f1381c487b844021afe8488c7
Florent Jacquemin <Florent.Jacquemin@FLORENT-PC.apcurium.local>
2012-10-30 17:07:18 -0400

    MKTAXI-465 : modify design of administration interface for update settings page

--- Src/Server/apcurium.MK.Web/admin/views/SettingsItemView.js
+++ Src/Server/apcurium.MK.Web/admin/views/SettingsItemView.js
@@ -1,24 +1,28 @@
 ﻿(function () {
-
+    var canExecute = true;
     TaxiHail.SettingsItemView = TaxiHail.TemplatedView.extend({
-
-        tagName: 'li',
+        
+        tagName: 'div',
 
         events: {
             'click [data-action=save-settings]': 'saveSettings',
             'change :input': 'onPropertyChanged'
         },
+        
+        initialize: function () {
+            this.model.on('sync', this.changeButtonState, this);
+        },
 
         render: function () {
             this.$el.html(this.renderTemplate(this.model.toJSON()));
-
             return this;
         },
 
         saveSettings: function (e) {
             e.preventDefault();
-
-            this.model.trigger('selected', this.model, this.model.collection);
+            if (canExecute == true) {
+                var f = this.model.trigger('selected', this.model, this.model.collection);
+            }
         },
         
         onPropertyChanged: function (e) {
@@ -27,6 +31,15 @@
 
                 this.model.set('value', $input.val());
             
+        },
+        changeButtonState: function (e) {
+            canExecute = false
+            var attr = this.model.attributes.key;
+            $(".btn[name='" + attr + "']").addClass('btn-success').text('Saved');
+            var timeoutID = window.setTimeout(function () {
+                $(".btn[name='" + attr + "']").removeClass('btn-success').text('Save');
+                canExecute = true;
+            }, 3000);
         }
 
     });
