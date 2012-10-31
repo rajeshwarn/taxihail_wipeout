7ca8d0343da51cc681ac593979cd0eae2e1e0b48
Maxime Seguin <Maxime.Seguin@max-apcurium-pc.apcurium.local>
2012-10-31 10:41:36 -0400

    MKTAXI-434 : Added possibility to get ReferenceData without filtering

--- Src/Server/MK.Booking.Api.Contract/Requests/ReferenceDataRequest.cs
+++ Src/Server/MK.Booking.Api.Contract/Requests/ReferenceDataRequest.cs
@@ -6,6 +6,6 @@ namespace apcurium.MK.Booking.Api.Contract.Requests
     [RestService("/referencedata", "GET")]
     public class ReferenceDataRequest : BaseDTO
     {
-         
+        public bool WithoutFiltering { get; set; }
     }
 }
