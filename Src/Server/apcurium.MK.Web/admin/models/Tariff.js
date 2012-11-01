060681abfc5d3c9a1fa41651afb00a26dae78747
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-11-01 13:48:03 -0400

    MKTAXI-380: BEtter defautl startTime and EndTime for tariff

--- Src/Server/apcurium.MK.Web/admin/models/Tariff.js
+++ Src/Server/apcurium.MK.Web/admin/models/Tariff.js
@@ -2,15 +2,17 @@
 
     var Tariff = TaxiHail.Tariff = Backbone.Model.extend({
         urlRoot: TaxiHail.parameters.apiRoot + '/admin/tariffs',
-        defaults: {
-            name: '',
-            flatRate: 0,
-            pricePerPassenger: 0,
-            distanceMultiplicator: 0,
-            timeAdjustmentFactor: 20,
-            startTime: null,
-            endTime: null,
-            daysOfTheWeek: 0
+        defaults: function(){
+            var today = new Date();
+            return { name: '',
+                flatRate: 0,
+                pricePerPassenger: 0,
+                distanceMultiplicator: 0,
+                timeAdjustmentFactor: 20,
+                startTime: TaxiHail.date.toISO8601(new Date(today.getYear(), today.getMonth(), today.getDate())),
+                endTime: TaxiHail.date.toISO8601(new Date(today.getYear(), today.getMonth(), today.getDate() + 1)),
+                daysOfTheWeek: 0
+            };
         }
     }, {
         type: {
