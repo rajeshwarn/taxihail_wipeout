8611abd3ce8705bb19daeb81115232156ba23bb1
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-11-13 16:20:07 -0500

    Removed unused usings

--- Src/Mobile/Android/Activities/Book/DateTimePickerActivity.cs
+++ Src/Mobile/Android/Activities/Book/DateTimePickerActivity.cs
@@ -1,16 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Java.Lang;
-using Android.Views.InputMethods;
 using apcurium.MK.Booking.Mobile.Client.Helpers;
 using TinyIoC;
 using TinyMessenger;
