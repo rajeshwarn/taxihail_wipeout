59a9a9f5999501dcb2cb5c65fa04a5595a5efa23
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-11-13 16:23:08 -0500

    Small refactoring in DateTimePickerActivity

--- Src/Mobile/Android/Activities/Book/DateTimePickerActivity.cs
+++ Src/Mobile/Android/Activities/Book/DateTimePickerActivity.cs
@@ -22,13 +22,13 @@ namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
             long dateTimeticks = this.Intent.GetLongExtra("SelectedDate", 0);
             DateTime selected = dateTimeticks > 0 ? new DateTime(dateTimeticks) :DateTime.Now;
 
-            FindViewById<TimePicker>(Resource.Id.timePickerCtl).SetIs24HourView(Java.Lang.Boolean.True);
-            FindViewById<DatePicker>(Resource.Id.datePickerCtl).UpdateDate(selected.Year, selected.Month - 1, selected.Day);
-            FindViewById<TimePicker>(Resource.Id.timePickerCtl).CurrentHour = new Integer(selected.Hour);
-            FindViewById<TimePicker>(Resource.Id.timePickerCtl).CurrentMinute = new Integer(selected.Minute);
-            FindViewById<TimePicker>(Resource.Id.timePickerCtl).SetIs24HourView(Java.Lang.Boolean.False);
-                       
+            var timePicker = FindViewById<TimePicker>(Resource.Id.timePickerCtl);
+            timePicker.SetIs24HourView(Java.Lang.Boolean.True);
+            timePicker.CurrentHour = new Integer(selected.Hour);
+            timePicker.CurrentMinute = new Integer(selected.Minute);
+            timePicker.SetIs24HourView(Java.Lang.Boolean.False);
 
+            FindViewById<DatePicker>(Resource.Id.datePickerCtl).UpdateDate(selected.Year, selected.Month - 1, selected.Day);
             FindViewById<Button>(Resource.Id.DoneButton).Click += DoneOnClick;
             FindViewById<Button>(Resource.Id.NowButton).Click += TimeOnClick;
         }
