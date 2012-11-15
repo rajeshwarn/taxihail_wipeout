36d31fd84f90e24a34ce073c635bcd0d0c64e2b5
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2012-11-15 16:03:09 -0500

    MKTAXI-529: [Staging] IE Order Historic is not refreshed

--- Src/Server/MK.Booking.Api.Contract/Requests/AccountOrderListRequest.cs
+++ Src/Server/MK.Booking.Api.Contract/Requests/AccountOrderListRequest.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
+using apcurium.MK.Booking.Api.Contract.Http;
+using apcurium.MK.Booking.Api.Contract.Resources;
 
 namespace apcurium.MK.Booking.Api.Contract.Requests
 {
     [Authenticate]
     [RestService("/account/orders", "GET")]
-    public class AccountOrderListRequest : BaseDTO { }
+    public class AccountOrderListRequest { }
+
+    [NoCache]
+    public class AccountOrderListRequestResponse: List<Order>
+    {
+        
+    }
 }
