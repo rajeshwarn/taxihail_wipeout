df426e05a2c07db7e891a808f407de14817c10de
Florent <Florent@Flo-win8>
2012-11-16 16:54:57 -0500

    fix clr exception

--- Src/Server/MK.Booking.Maps/Impl/Addresses.cs
+++ Src/Server/MK.Booking.Maps/Impl/Addresses.cs
@@ -25,10 +25,15 @@ namespace apcurium.MK.Booking.Maps.Impl
 
         public Address[] Search(string name, double latitude, double longitude)
         {
-            var term = name.Substring(0, 1);
+            var isNumeric = false;
+            if (!string.IsNullOrEmpty(name))
+            {
+                var term = name.Substring(0, 1);
 
-            int n;
-            var isNumeric = int.TryParse(term, out n);
+                int n;
+                isNumeric = int.TryParse(term, out n);
+            }
+            
 
             IEnumerable<Address> addresses = null;
             if (isNumeric)
@@ -48,7 +53,6 @@ namespace apcurium.MK.Booking.Maps.Impl
                 var nearbyService = new Places(_client, _configManager, _popularAddressProvider);
                 addresses = (Address[])nearbyService.SearchPlaces( name, latitude,longitude, null );
             }
-
             return addresses.ToArray();
         }
 
