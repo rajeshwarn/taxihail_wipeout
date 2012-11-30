afa5aba567a631eb98d80c595f9a9cf146d498b6
Matthieu Guyonnet-Duluc <matthieu.duluc@apcurium.com>
2012-11-30 16:20:49 -0500

    MKTAXI-601 crash on my locations

--- Src/Mobile/iOS/Controls/Binding/BindableAddressTableViewSource.cs
+++ Src/Mobile/iOS/Controls/Binding/BindableAddressTableViewSource.cs
@@ -21,15 +21,17 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls.Binding
 
 		public override int NumberOfSections (UITableView tableView)
 		{
+            var nb = 0;
 			var dataSource =  ((BindableAddressTableViewSource)tableView.Source).ItemsSource as IEnumerable<SectionAddressViewModel>;
-			var nb =  dataSource.Where( ds => (ds.Addresses != null) && ds.Addresses.Count() > 0 ).Count();
+            if(dataSource != null)
+			    nb =  dataSource.Where( ds => (ds.Addresses != null) && ds.Addresses.Count() > 0 ).Count();
             return nb;
 		}
 
 		public override int RowsInSection (UITableView tableview, int section)
 		{
 			var dataSource =  ((BindableAddressTableViewSource)tableview.Source).ItemsSource as IEnumerable<SectionAddressViewModel>;
-			return dataSource.ElementAt( section ).Addresses.Count();
+            return dataSource.ElementAt( section ).Addresses.Count();
 		}
 
 		protected override object GetItemAt (NSIndexPath indexPath)
