fa871c09cb7305cd9307f56d523ee547c88be6cc
matthieu duluc <matthieu.duluc@TSIEN.apcurium.local>
2013-01-07 11:42:56 -0500

    MKTAXI-719 Add command and service to detect a change of the status of an order

---
+++ Src/Server/MK.Booking/MK.Booking.Commands/ChangeOrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure.Messaging;
+using apcurium.MK.Common.Entity;
+
+namespace apcurium.MK.Booking.Commands
+{
+    public class ChangeOrderStatus : ICommand
+    {
+        public ChangeOrderStatus()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; private set; }
+
+        public OrderStatusDetail Status { get; set; }
+    }
+}
--- Src/Server/apcurium.MK.Web/App_Start/UpdateOrderStatusJob.cs
+++ Src/Server/apcurium.MK.Web/App_Start/UpdateOrderStatusJob.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Web;
 using System.Web.Caching;
 using Infrastructure.Messaging;
+using ServiceStack.ServiceClient.Web;
+using apcurium.MK.Booking.Api.Contract.Resources;
+using apcurium.MK.Booking.Commands;
+using apcurium.MK.Booking.Google.Resources;
 using apcurium.MK.Booking.IBS;
+using apcurium.MK.Booking.ReadModel;
 using apcurium.MK.Booking.ReadModel.Query;
+using apcurium.MK.Common;
 using apcurium.MK.Common.Configuration;
+using apcurium.MK.Common.Entity;
+using apcurium.MK.Common.Extensions;
 using log4net;
+using OrderStatus = apcurium.MK.Common.Entity.OrderStatus;
 
 namespace apcurium.MK.Web.App_Start
 {
@@ -17,6 +29,8 @@ namespace apcurium.MK.Web.App_Start
         private readonly ICommandBus _commandBus;
         private const string CacheKey = "OrderStatusJob";
         private static readonly ILog Logger = LogManager.GetLogger(typeof(UpdateOrderStatusJob));
+        private const string AssignedStatus = "wosASSIGNED";
+        private const string DoneStatus = "wosDONE";
 
         public UpdateOrderStatusJob(IOrderDao orderDao, IConfigurationManager configManager, IBookingWebServiceClient bookingWebServiceClient, ICommandBus commandBus)
         {
@@ -41,6 +55,98 @@ namespace apcurium.MK.Web.App_Start
             try
             {
                 var orders = _orderDao.GetOrdersInProgress();
+                foreach (var orderStatusDetail in orders)
+                {
+                    Logger.Debug("Get Status for " + orderStatusDetail.OrderId);
+                    var ibsStatus = _bookingWebServiceClient.GetOrderStatus(orderStatusDetail.IBSOrderId.Value, 0, string.Empty);
+
+                    if (ibsStatus.Status.HasValue()
+                        && orderStatusDetail.IBSStatusId != ibsStatus.Status)
+                    {
+                        string description = null;
+                        Logger.Debug("Status Changed for " + orderStatusDetail.OrderId);
+                        orderStatusDetail.IBSStatusId = ibsStatus.Status;
+                        orderStatusDetail.Status = OrderStatus.Pending;
+
+                        if (ibsStatus.Status.SoftEqual(AssignedStatus))
+                        {
+                            var orderDetails = _bookingWebServiceClient.GetOrderDetails(orderStatusDetail.IBSOrderId.Value, 0, string.Empty);
+                            if ((orderDetails != null) && (orderDetails.VehicleNumber.HasValue()))
+                            {
+                                Logger.Debug("Vehicle number :  " + orderDetails.VehicleNumber);
+                                orderStatusDetail.VehicleNumber = orderDetails.VehicleNumber;
+                                description = string.Format(_configManager.GetSetting("OrderStatus.CabDriverNumberAssigned"),
+                                                     orderDetails.VehicleNumber);
+                                if (!string.IsNullOrEmpty(orderDetails.CallNumber))
+                                {
+                                    var driverInfos = _bookingWebServiceClient.GetDriverInfos(orderDetails.CallNumber);
+                                    orderStatusDetail.DriverInfos.FirstName = driverInfos.FirstName;
+                                    orderStatusDetail.DriverInfos.LastName = driverInfos.LastName;
+                                    orderStatusDetail.DriverInfos.MobilePhone = driverInfos.MobilePhone;
+                                    orderStatusDetail.DriverInfos.VehicleColor = driverInfos.VehicleColor;
+                                    orderStatusDetail.DriverInfos.VehicleMake = driverInfos.VehicleMake;
+                                    orderStatusDetail.DriverInfos.VehicleModel = driverInfos.VehicleModel;
+                                    orderStatusDetail.DriverInfos.VehicleRegistration = driverInfos.VehicleRegistration;
+                                    orderStatusDetail.DriverInfos.VehicleType = driverInfos.VehicleType;
+                                }
+                            }
+                            
+
+                            if (_configManager.GetSetting("OrderStatus.DemoMode") == "true")
+                            {
+                                Logger.Debug("DEMO MODE IS ACTIVE!");
+                                DemoModeFakePosition(orderStatusDetail);
+                            }
+                            else if (ibsStatus.VehicleLatitude.HasValue && ibsStatus.VehicleLongitude.HasValue)
+                            {
+                                Logger.Debug(string.Format("Vehicle poistion : Lat {0} : Lng{1}", ibsStatus.VehicleLatitude, ibsStatus.VehicleLongitude));
+                                orderStatusDetail.VehicleLatitude = ibsStatus.VehicleLatitude;
+                                orderStatusDetail.VehicleLongitude = ibsStatus.VehicleLongitude;
+                            }
+                            else
+                            {
+                                Logger.Debug("CANNOT GET VEHICULE POSITION");
+                            }
+                        }
+
+                        if (ibsStatus.Status.SoftEqual(DoneStatus))
+                        {
+                            orderStatusDetail.Status = OrderStatus.Completed;
+                            var orderDetails = _bookingWebServiceClient.GetOrderDetails(orderStatusDetail.IBSOrderId.Value, 0, string.Empty);
+
+                            if ((orderDetails != null) && ((orderDetails.Fare.HasValue || orderDetails.Tip.HasValue || orderDetails.Toll.HasValue)))
+                            {
+                                //FormatPrice
+                                var total = Params.Get<double?>(orderDetails.Toll, orderDetails.Fare, orderDetails.Tip).Where(amount => amount.HasValue).Select(amount => amount.Value).Sum();
+                                description = string.Format(_configManager.GetSetting("OrderStatus.OrderDoneFareAvailable"), FormatPrice(total));
+                                orderStatusDetail.FareAvailable = true;
+                            }
+
+                            var completeOrder = new CompleteOrder
+                                                    {
+                                                        Date = DateTime.UtcNow,
+                                                        OrderId = orderStatusDetail.OrderId,
+                                                        Fare = orderDetails != null ? orderDetails.Fare : null,
+                                                        Toll = orderDetails != null ? orderDetails.Toll : null,
+                                                        Tip = orderDetails != null ? orderDetails.Tip : null
+                                                    };
+                            _commandBus.Send(completeOrder);
+                            
+                        }
+
+                        if (description.HasValue())
+                        {
+                            orderStatusDetail.IBSStatusDescription = description;
+                        }
+                        else
+                        {
+                            orderStatusDetail.IBSStatusDescription = _configManager.GetSetting("OrderStatus." + ibsStatus.Status);
+                        }
+
+                        
+                        _commandBus.Send(new ChangeOrderStatus { Status = orderStatusDetail});
+                    }
+                }
             }
             catch (Exception e)
             {
@@ -50,6 +156,65 @@ namespace apcurium.MK.Web.App_Start
             {
                 HttpRuntime.Cache.Insert(CacheKey, new object(), null, DateTime.Now.AddSeconds(10), Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, CacheItemRemoved);
             }
+        }
+
+        private string FormatPrice(double? price)
+        {
+            var culture = _configManager.GetSetting("PriceFormat");
+            return string.Format(new CultureInfo(culture), "{0:C}", price.HasValue ? price.Value : 0);
+        }
+
+        private void DemoModeFakePosition(OrderStatusDetail status)
+        {
+            var order = _orderDao.FindById(status.OrderId);
+            //For demo purpose only
+            var leg = BuildFakeDirectionForOrder(status.OrderId, order.PickupAddress.Latitude, order.PickupAddress.Longitude);
+
+            if (leg != null)
+            {
+                status.VehicleLatitude = leg.Lat;
+                status.VehicleLongitude = leg.Lng;
+            }
+
+        }
+
+        private static readonly Dictionary<Guid, List<Location>> _fakeTaxiPositions = new Dictionary<Guid, List<Location>>();
+        private static readonly Dictionary<Guid, int> _fakeTaxiPositionsIndex = new Dictionary<Guid, int>();
+
+        private Location BuildFakeDirectionForOrder(Guid guid, double lat, double lng)
+        {
+
+            if (!_fakeTaxiPositions.ContainsKey(guid))
+            {
+                var result = new DirectionInfo();
+
+                var client = new JsonServiceClient("http://maps.googleapis.com/maps/api/");
+
+                var resource = string.Format(CultureInfo.InvariantCulture, "directions/json?origin={0},{1}&destination={2},{3}&sensor=false", lat, lng, lat + 0.008, lng + 0.008);
+
+                var directions = client.Get<DirectionResult>(resource);
+
+                var steps = new List<Location>();
+
+                foreach (var s in directions.Routes[0].Legs[0].Steps)
+                {
+                    steps.Add(s.Start_location);
+                    steps.Add(s.End_location);       
+                }
+                _fakeTaxiPositions.Add(guid, steps);
+                _fakeTaxiPositionsIndex.Add(guid, 0);
+            }
+
+            var index = _fakeTaxiPositionsIndex[guid];
+
+            _fakeTaxiPositionsIndex[guid] = index + 1;
+
+            if (_fakeTaxiPositionsIndex[guid] >= _fakeTaxiPositions[guid].Count)
+            {
+                return new Location { Lat = lat, Lng = lng };
+            }
+            return _fakeTaxiPositions[guid][_fakeTaxiPositions[guid].Count - index - 1];
+
         }
     }
 }
