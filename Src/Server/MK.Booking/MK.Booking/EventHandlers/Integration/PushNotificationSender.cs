02fcbb4ef1d45dc59ac2ee8f79e69d7b0bb3c3ee
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-01-14 16:55:23 -0500

    MKTAXI-725: Send push notifications for wosArrived and wosAssigned statuses only Changed message sent with push notifications

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/PushNotificationSender.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/PushNotificationSender.cs
@@ -14,6 +14,8 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
         : IIntegrationEventHandler,
           IEventHandler<OrderStatusChanged>
     {
+        private const string wosASSIGNED = "Taxi #{0} has been assigned to you";
+        private const string wosARRIVED = "Your Taxi (#{0}) has arrived";
         readonly Func<BookingDbContext> _contextFactory;
         readonly IPushNotificationService _pushNotificationService;
 
@@ -25,18 +27,37 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
 
         public void Handle(OrderStatusChanged @event)
         {
-            using (var context = _contextFactory.Invoke())
-            {
-                var order = context.Find<OrderDetail>(@event.SourceId);
-                var devices = context.Set<DeviceDetail>().Where(x => x.AccountId == order.AccountId);
-                var data = new Dictionary<string, object>
-                {
-                    { "orderId", order.Id },
-                };
+            var shouldSendPushNotification = @event.Status.IBSStatusId == "wosASSIGNED"
+                                                || @event.Status.IBSStatusId == "wosARRIVED";
 
-                foreach (var device in devices)
+            if (shouldSendPushNotification)
+            {
+                var alert = string.Empty;
+                switch (@event.Status.IBSStatusId)
                 {
-                    _pushNotificationService.Send("status changed to " + @event.Status.IBSStatusId, data, device.DeviceToken, device.Platform);
+                    case "wosASSIGNED":
+                        alert = string.Format(wosASSIGNED, @event.Status.VehicleNumber);
+                        break;
+                    case "wosARRIVED":
+                        alert = string.Format(wosARRIVED, @event.Status.VehicleNumber);
+                        break;
+                    default:
+                        throw new InvalidOperationException("No push notification for this order status");
+                }
+
+                using (var context = _contextFactory.Invoke())
+                {
+                    var order = context.Find<OrderDetail>(@event.SourceId);
+                    var devices = context.Set<DeviceDetail>().Where(x => x.AccountId == order.AccountId);
+                    var data = new Dictionary<string, object>
+                                   {
+                                       {"orderId", order.Id},
+                                   };
+
+                    foreach (var device in devices)
+                    {
+                        _pushNotificationService.Send(alert, data, device.DeviceToken, device.Platform);
+                    }
                 }
 
             }
