2334cedb65c7fc5e68bf21275708f0a08d395fc8
matthieugd <devnull@localhost>
2013-01-29 16:38:13 -0500

    MKTAXI-759 add confirm by admin - UI

---
+++ Src/Server/apcurium.MK.Web/admin/views/ConfirmEmailView.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    
+    var View = TaxiHail.ConfirmEmailView = TaxiHail.TemplatedView.extend({
+
+        tagName: 'form',
+        className: 'form-horizontal',
+        
+        events: {
+            'click [data-action=confirmemail]': 'confirmemail'
+        },
+
+        render: function () {
+            this.$el.html(this.renderTemplate());
+
+        
+        
+            return this;
+        },
+        
+        confirmemail: function (e) {
+            e.preventDefault();
+            var email = this.$('[name=email]').val();
+            return $.ajax({
+                type: 'PUT',
+                url: '../api/account/adminconfirm',
+                data: {
+                    accountEmail: email
+                },
+                dataType: 'json',
+                success : _.bind(function() {
+                    this.$('.errors').text(TaxiHail.localize('confirmEmailSuccess'));
+                },this)
+            }).fail(_.bind(function (e) {
+                this.$('.errors').text(TaxiHail.localize('confirmEmailError'));
+            }),this);
+        }
+
+   
+    });
+
+    
+
+}());
