8884d91f7f98a42a52fe69f292c9a59748278754
alex.proteau <alex.proteau@apcurium.com>
2013-02-13 10:09:09 -0500

    Fix cell text truncating issue

--- Src/Mobile/Android/Controls/ListViewCell2.cs
+++ Src/Mobile/Android/Controls/ListViewCell2.cs
@@ -18,36 +18,36 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
 {
     public class ListViewCell2 : TextView
     {
-		public ListViewCell2(Context context)
+        public ListViewCell2(Context context)
             : base(context)
         {
         }
-
-		public ListViewCell2(Context context, IAttributeSet attrs)
+        
+        public ListViewCell2(Context context, IAttributeSet attrs)
             : base(context, attrs)
         {
         }
-
-		public ListViewCell2(IntPtr ptr, Android.Runtime.JniHandleOwnership handle)
+        
+        public ListViewCell2(IntPtr ptr, Android.Runtime.JniHandleOwnership handle)
             : base(ptr, handle)
         {
         }
-
+        
         public string TextLine1 { get; set; }
-		public string TextLine2 { get; set; }
-
+        public string TextLine2 { get; set; }
+        
         private bool _isTop;
-
+        
         public bool IsTop { 
             get{ return _isTop;} 
             set
             { 
                 _isTop = value; 
                 Invalidate();
-            
+                
             } 
         }
-		
+        
         private bool _isBottom;
         public bool IsBottom { get{ return _isBottom;} 
             set
@@ -58,64 +58,64 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
                 {
                     Console.WriteLine ( "ISBOTTOM : " + value.ToString() );
                 }
-
+                
             } 
         }
-
+        
         public bool ShowRightArrow { get; set; }
         public bool ShowPlusSign { get; set; }
-
+        
         public string Icon { get; set; }
-
-
-
+        
+        
+        
         protected override void OnDraw(Android.Graphics.Canvas canvas)
         {
             base.OnDraw(canvas);
             var textX =  ShowPlusSign ? DrawHelper.GetPixels(65) : DrawHelper.GetPixels(8);
-
+            
             if ( !ShowPlusSign &&( Icon.HasValue() ))
             {
                 textX =   DrawHelper.GetPixels(40) ;
             }
-
-			if( !TextLine1.IsNullOrEmpty() )
-			{
+            
+            if( !TextLine1.IsNullOrEmpty() )
+            {
                 DrawText(canvas, TextLine1 ?? "", textX, DrawHelper.GetPixels( 21 ), DrawHelper.GetPixels( 16 ), AppFonts.Bold,new Color( 50,50,50,255 ));
                 DrawText(canvas, TextLine2 ?? "", textX,  DrawHelper.GetPixels( 41 ),  DrawHelper.GetPixels( 15 ), AppFonts.Regular,new Color( 86,86,86,255 ) );
-			}
-			else
-			{
+            }
+            else
+            {
                 DrawText(canvas, TextLine2 ?? "", textX, DrawHelper.GetPixels( 32 ), DrawHelper.GetPixels(16), AppFonts.Regular,new Color( 86,86,86,255 ) );
-			}
-
+            }
+            
             if (ShowRightArrow)
             {
                 canvas.DrawBitmap(BitmapFactory.DecodeResource(Resources, Resource.Drawable.right_arrow), this.Width-DrawHelper.GetPixels(20), DrawHelper.GetPixels(16), null);
             }
-
-			if (ShowPlusSign)
-			{
+            
+            if (ShowPlusSign)
+            {
                 canvas.DrawBitmap(BitmapFactory.DecodeResource(Resources, Resource.Drawable.add_btn), DrawHelper.GetPixels(6), DrawHelper.GetPixels(10), null);
-			}
+            }
             else if ( Icon.HasValue() )
             {
                 var identifier = Context.Resources.GetIdentifier(  Icon, "drawable", Context.PackageName);
                 //return _context.Resources.GetString(identifier);
-
+                
                 canvas.DrawBitmap(BitmapFactory.DecodeResource(Resources, identifier ), DrawHelper.GetPixels(6), DrawHelper.GetPixels(10), null);
             }
-
-
-
+            
+            
+            
             var d = IsTop && !IsBottom ? Resource.Drawable.cell_top_state : IsBottom && !IsTop ? Resource.Drawable.cell_bottom_state : IsTop && IsBottom ? Resource.Drawable.cell_full_state : Resource.Drawable.cell_middle_state;
-			SetBackgroundDrawable( Resources.GetDrawable( d ) );
-
-
-
-
+            SetBackgroundDrawable( Resources.GetDrawable( d ) );
+            
+            
+            
+            
         }
-
+        
         private void DrawText(Android.Graphics.Canvas canvas, string text, float x, float y, float textSize, Typeface typeface, Color color )
         {
             TextPaint paintText = new TextPaint(PaintFlags.AntiAlias | Android.Graphics.PaintFlags.LinearText);
@@ -123,19 +123,19 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
             paintText.TextSize = textSize;
             paintText.GetTextBounds(text, 0, text.Length, rect);
             paintText.SetARGB(color.A, color.R  , color.G , color.B );
-			paintText.SetTypeface(typeface);
+            paintText.SetTypeface(typeface);
             var p = new TextPaint();
             p.SetTypeface(typeface);
-
+            
             p.TextSize = textSize;
-            var ellipsizedText = TextUtils.Ellipsize(text, p, this.Width - 80, TextUtils.TruncateAt.End);
+            var ellipsizedText = TextUtils.Ellipsize(text, p, this.Width - DrawHelper.GetPixels(75), TextUtils.TruncateAt.End);
             if (ellipsizedText.IsNullOrEmpty())
             {
                 ellipsizedText = text;
             }
-
+            
             canvas.DrawText(ellipsizedText, x, y, paintText);
-
+            
         }
     }
 }
--- Src/Mobile/iOS/InfoTableView/TwoLinesCell.cs
+++ Src/Mobile/iOS/InfoTableView/TwoLinesCell.cs
@@ -10,43 +10,43 @@ using Cirrious.MvvmCross.Binding.Touch.Views;
 
 namespace apcurium.MK.Booking.Mobile.Client.InfoTableView
 {
-	public class TwoLinesCell : MvxBindableTableViewCell
-	{
-		private UIImageView _arrowImage;
-		//private UIImageView _plusSignImage;
-		private bool _showPlusSign;
-		private bool _showArrow;
-		private float _rowHeight = 44f;
+    public class TwoLinesCell : MvxBindableTableViewCell
+    {
+        private UIImageView _arrowImage;
+        //private UIImageView _plusSignImage;
+        private bool _showPlusSign;
+        private bool _showArrow;
+        private float _rowHeight = 44f;
         private string _icon;
-		public TwoLinesCell (IntPtr handle, string bindingText) : base(bindingText, handle)
-		{		
-		}
-
-		public TwoLinesCell (string cellIdentifier, string bindingText) : base( bindingText, UITableViewCellStyle.Subtitle, new NSString(cellIdentifier), UITableViewCellAccessory.None   )
-		{					
-			SelectionStyle = UITableViewCellSelectionStyle.None;
-			Accessory = UITableViewCellAccessory.None;
-			Initialize ();
-		}
-
-		public string FirstLine { get { return TextLabel.Text; }
-			set { TextLabel.Text = value; }
-		}
-
-		public string SecondLine { get { return DetailTextLabel.Text; }
-			set { DetailTextLabel.Text = value; }
-		}
-
-		public string test{get;set;}
-
-		public bool ShowPlusSign { 
-			get { return _showPlusSign; }
-			set { 
-				_showPlusSign = value;
+        public TwoLinesCell (IntPtr handle, string bindingText) : base(bindingText, handle)
+        {       
+        }
+        
+        public TwoLinesCell (string cellIdentifier, string bindingText) : base( bindingText, UITableViewCellStyle.Subtitle, new NSString(cellIdentifier), UITableViewCellAccessory.None   )
+        {                   
+            SelectionStyle = UITableViewCellSelectionStyle.None;
+            
+            Initialize ();
+        }
+        
+        public string FirstLine { get { return TextLabel.Text; }
+            set { TextLabel.Text = value; }
+        }
+        
+        public string SecondLine { get { return DetailTextLabel.Text; }
+            set { DetailTextLabel.Text = value;}
+        }
+        
+        public string test{get;set;}
+        
+        public bool ShowPlusSign { 
+            get { return _showPlusSign; }
+            set { 
+                _showPlusSign = value;
                 ImageView.Image = value ? UIImage.FromFile("Assets/Cells/add_btn.png") : null;
-			}
-		}
-
+            }
+        }
+        
         public string Icon { 
             get { return _icon; }
             set { 
@@ -58,77 +58,81 @@ namespace apcurium.MK.Booking.Mobile.Client.InfoTableView
                 ImageView.Image = value.HasValue () ? UIImage.FromFile(string.Format ("Assets/{0}.png", value)) : null;
             }
         }
-
-
-		public bool ShowRightArrow { 
-			get { return _showArrow; }
-			set { 
-				_showArrow = value;
-				_arrowImage.Hidden = !_showArrow;
-			}
-		}
-
-		private bool _isFirst, _isLast;
-		public bool IsFirst { get { return _isFirst;} 
-			set{ 
-				_isFirst = value;
-				((CustomCellBackgroundView)BackgroundView).IsTop = _isFirst;
-			}
-		}
-		public bool IsLast { get{ return _isLast;} 
-			set{ 
-				_isLast = value;
-				((CustomCellBackgroundView)BackgroundView).IsBottom = _isLast;
-			}
-		}
-
+        
+        
+        public bool ShowRightArrow { 
+            get { return _showArrow; }
+            set { 
+                _showArrow = value;
+                _arrowImage.Hidden = !_showArrow;
+            }
+        }
+        
+        private bool _isFirst, _isLast;
+        public bool IsFirst { get { return _isFirst;} 
+            set{ 
+                _isFirst = value;
+                ((CustomCellBackgroundView)BackgroundView).IsTop = _isFirst;
+            }
+        }
+        public bool IsLast { get{ return _isLast;} 
+            set{ 
+                _isLast = value;
+                ((CustomCellBackgroundView)BackgroundView).IsBottom = _isLast;
+            }
+        }
+        
         public bool IsAddNewCell {
             set {
                 ((CustomCellBackgroundView)BackgroundView).IsAddNewCell = value;
                 this.TextLabel.TextColor = this.DetailTextLabel.TextColor = value 
                     ? AppStyle.CellAddTextColor 
-                    : AppStyle.CellFirstLineTextColor;
+                        : AppStyle.CellFirstLineTextColor;
             }
         }
-
-		private void Initialize ()
-		{
-			BackgroundView = new CustomCellBackgroundView( IsFirst, IsLast, Frame, ShowPlusSign );
-			TextLabel.TextColor = AppStyle.CellFirstLineTextColor;
-			TextLabel.BackgroundColor = UIColor.Clear;
-			TextLabel.Font = AppStyle.CellFont;
-
-			DetailTextLabel.TextColor = AppStyle.CellSecondLineTextColor;
-			DetailTextLabel.BackgroundColor = UIColor.Clear;
-			DetailTextLabel.Font = AppStyle.NormalTextFont;
-			_arrowImage = new UIImageView (new RectangleF (290, _rowHeight/2 - 15/2, 14, 15 ) ); 
-			_arrowImage.BackgroundColor = UIColor.Clear;
-			_arrowImage.ContentMode = UIViewContentMode.ScaleAspectFit;
-			_arrowImage.Image = UIImage.FromFile("Assets/Cells/rightArrow.png");
-			_arrowImage.Hidden = true;
-			AddSubview ( _arrowImage );	
-	
-		}
-	
-		public override void TouchesBegan ( NSSet touches, UIEvent evt )
-		{	
-			BackgroundView.SetNeedsDisplay();
-			base.TouchesBegan ( touches, evt );	
-		}
-		public override void TouchesEnded (NSSet touches, UIEvent evt)
-		{
-			SetHighlighted( false, true );
-			BackgroundView.SetNeedsDisplay();
-			base.TouchesEnded (touches, evt);
-		}
-		public override void TouchesCancelled (NSSet touches, UIEvent evt)
-		{
-			SetHighlighted( false, true );
-			BackgroundView.SetNeedsDisplay();
-			base.TouchesCancelled (touches, evt);
-		}
-
-
-	}
-
+        
+        private void Initialize ()
+        {
+            BackgroundView = new CustomCellBackgroundView( IsFirst, IsLast, Frame, ShowPlusSign );
+            TextLabel.TextColor = AppStyle.CellFirstLineTextColor;
+            TextLabel.BackgroundColor = UIColor.Clear;
+            TextLabel.Font = AppStyle.CellFont;
+            
+            DetailTextLabel.TextColor = AppStyle.CellSecondLineTextColor;
+            DetailTextLabel.BackgroundColor = UIColor.Clear;
+            DetailTextLabel.Font = AppStyle.NormalTextFont;
+            
+            //_arrowImage = new UIImageView (new RectangleF (290, _rowHeight/2 - 15/2, 14, 15 ) ); 
+            _arrowImage = new UIImageView (new RectangleF (0,0, 14, 15 )); 
+            _arrowImage.BackgroundColor = UIColor.Clear;
+            _arrowImage.ContentMode = UIViewContentMode.ScaleAspectFit;
+            _arrowImage.Image = UIImage.FromFile("Assets/Cells/rightArrow.png");
+            _arrowImage.Hidden = true;
+            
+            AccessoryView = _arrowImage;
+            //AddSubview ( _arrowImage );   
+            
+        }
+        
+        public override void TouchesBegan ( NSSet touches, UIEvent evt )
+        {   
+            BackgroundView.SetNeedsDisplay();
+            base.TouchesBegan ( touches, evt ); 
+        }
+        public override void TouchesEnded (NSSet touches, UIEvent evt)
+        {
+            SetHighlighted( false, true );
+            BackgroundView.SetNeedsDisplay();
+            base.TouchesEnded (touches, evt);
+        }
+        public override void TouchesCancelled (NSSet touches, UIEvent evt)
+        {
+            SetHighlighted( false, true );
+            BackgroundView.SetNeedsDisplay();
+            base.TouchesCancelled (touches, evt);
+        }
+        
+        
+    }
+    
 }
