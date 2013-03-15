4fbc76d7b13398cbf41da2da19b6985fbe9e0ae6
alex p <alex.proteau@apcurium.com>
2013-03-15 10:41:19 -0400

    Add version to config tool

--- Src/Server/MK.ConfigurationManager/ConfigurationManagerDbContext.cs
+++ Src/Server/MK.ConfigurationManager/ConfigurationManagerDbContext.cs
@@ -25,6 +25,8 @@ namespace MK.ConfigurationManager
             modelBuilder.Entity<IBSServer>().ToTable("IBSServer", SchemaName);
             modelBuilder.Entity<TaxiHailEnvironment>().ToTable("TaxiHailEnvironment", SchemaName);
             modelBuilder.Entity<DeploymentJob>().ToTable("DeploymentJob", SchemaName);
+            modelBuilder.Entity<AppVersion>().ToTable("AppVersion", SchemaName);
+
         }
     }
 }
