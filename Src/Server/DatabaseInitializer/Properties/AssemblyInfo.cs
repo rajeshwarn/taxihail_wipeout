8cf00b0473d99257e0735a93028043aa22252a7c
alex p <alex.proteau@apcurium.com>
2013-05-16 14:46:37 -0400

    Change version number to 1.3.14 ( Server )

--- Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
+++ Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.13")]
-[assembly: AssemblyVersion("1.3.13")]
-[assembly: AssemblyFileVersion("1.3.13")]
+// [assembly: AssemblyVersion("1.3.14")]
+[assembly: AssemblyVersion("1.3.14")]
+[assembly: AssemblyFileVersion("1.3.14")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
