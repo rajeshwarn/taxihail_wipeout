3793b1fbfc28da5442810211283ad5a91e181fcb
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-05-23 18:24:13 -0400

    MKTAXI-814: Created AvailableVehiclesService stub

---
+++ Src/Server/MK.Booking.Api.Contract/Requests/AvailableVehicles.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ServiceStack.ServiceHost;
+using apcurium.MK.Booking.Api.Contract.Resources;
+
+namespace apcurium.MK.Booking.Api.Contract.Requests
+{
+    [RestService("/vehicles/", "GET")]
+    public class AvailableVehicles : IReturn<AvailableVehiclesResponse>
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class AvailableVehiclesResponse : List<AvailableVehicle>
+    {
+
+    }
+}
---
+++ Src/Server/MK.Booking.Api/Services/AvailableVehiclesService.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack.ServiceInterface;
+using apcurium.MK.Booking.Api.Contract.Requests;
+
+namespace apcurium.MK.Booking.Api.Services
+{
+    public class AvailableVehiclesService : Service
+    {
+        public AvailableVehiclesResponse Get(AvailableVehicles request)
+        {
+
+            return new AvailableVehiclesResponse();
+        }
+    }
+}
