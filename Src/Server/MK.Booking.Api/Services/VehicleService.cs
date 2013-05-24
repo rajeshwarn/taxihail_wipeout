caad540ba992d005f4b8980d90fa0b5352b96ab2
Chris-PC <devnull@localhost>
2013-05-24 11:40:58 -0400

    826 - Send message to driver restored

---
+++ Src/Server/MK.Booking.Api/Services/VehicleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Infrastructure.Messaging;
+using MK.Common.Android;
+using ServiceStack.ServiceInterface;
+using apcurium.MK.Booking.IBS;
+using apcurium.MK.Booking.IBS.Impl;
+
+namespace apcurium.MK.Booking.Api.Services
+{
+    public class VehicleService: Service
+    {
+        private IBookingWebServiceClient _bookingWebServiceClient;
+        public VehicleService(IBookingWebServiceClient bookingWebServiceClient)
+        {
+            _bookingWebServiceClient = bookingWebServiceClient;
+        }
+
+        public SendMessageToDriverResponse Post(SendMessageToDriverRequest request)
+        {
+            return new SendMessageToDriverResponse()
+                {
+                    Success = _bookingWebServiceClient.SendMessageToDriver(request.CarNumber, request.Message)
+                };
+        }
+
+
+    }
+}
