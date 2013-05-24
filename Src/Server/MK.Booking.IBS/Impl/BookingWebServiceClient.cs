7fa65b93e8de2f9fc9eddda823c528ffa513e794
Chris-PC <devnull@localhost>
2013-05-24 15:03:06 -0400

    826 - Send message to driver fixed in confirmation

--- Src/Server/MK.Booking.IBS/Impl/BookingWebServiceClient.cs
+++ Src/Server/MK.Booking.IBS/Impl/BookingWebServiceClient.cs
@@ -96,7 +96,7 @@ namespace apcurium.MK.Booking.IBS.Impl
             var success = false;
             UseService(service =>
             {
-                var resultat = service.SendDriverMsg(UserNameApp, PasswordApp, message, carId);
+                var resultat = service.SendDriverMsg(UserNameApp, PasswordApp, carId,message);
                 success = resultat == 1;
             });
             return success;
