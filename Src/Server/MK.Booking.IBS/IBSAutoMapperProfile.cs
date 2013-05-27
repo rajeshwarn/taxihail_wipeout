76e78968c5766a751abd5140b4113fda51f09ae4
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-05-27 15:42:05 -0400

    MKTAXI-814: Temporary fox for ibs latitude/longitude mixup

--- Src/Server/MK.Booking.IBS/IBSAutoMapperProfile.cs
+++ Src/Server/MK.Booking.IBS/IBSAutoMapperProfile.cs
@@ -7,6 +7,8 @@ namespace apcurium.MK.Booking.IBS
         protected override void Configure()
         {
             this.CreateMap<TVehiclePosition, IBSVehiclePosition>()
+                .ForMember(x => x.Longitude, opt => opt.MapFrom(x => x.Latitude))
+                .ForMember(x => x.Latitude, opt => opt.MapFrom(x => x.Longitude))
                 .ForMember(x => x.PositionDate, opt => opt
                     .MapFrom(x => x.GPSLastUpdated.ToDateTime().GetValueOrDefault()));
         }
