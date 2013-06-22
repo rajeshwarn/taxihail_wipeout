eb61b929acb5eb3278822b868a4511068f6cfdfa
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-06-22 15:02:12 -0400

    added a few tests

--- Src/Server/MK.Booking.IBS.Test/StaticDataWebServiceClientFixture.cs
+++ Src/Server/MK.Booking.IBS.Test/StaticDataWebServiceClientFixture.cs
@@ -2,6 +2,7 @@
 using apcurium.MK.Booking.IBS.Impl;
 using apcurium.MK.Common.Configuration.Impl;
 using apcurium.MK.Common.Diagnostic;
+using apcurium.MK.Common.Entity;
 
 namespace MK.Booking.IBS.Test
 {
@@ -29,6 +30,57 @@ namespace MK.Booking.IBS.Test
             var zone = sut.GetZoneByCoordinate(MobileKnowledgeProviderId, 43.566900, -79.574300);
 
             Assert.AreEqual("701", zone);
+        }
+
+        [Test]
+        public void get_vehicles_test()
+        {
+            var sut = new StaticDataWebServiceClient(new FakeConfigurationManager(), new Logger());
+
+            var vehicles = sut.GetVehiclesList(new ListItem { Id = TheChauffeurGroupProviderId });
+
+            Assert.Inconclusive();
+        }
+        
+        [Test]
+        public void get_pickup_cities_test()
+        {
+            var sut = new StaticDataWebServiceClient(new FakeConfigurationManager(), new Logger());
+
+            var cities = sut.GetPickupCity(new ListItem { Id = MobileKnowledgeProviderId });
+
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void get_payments_test()
+        {
+            var sut = new StaticDataWebServiceClient(new FakeConfigurationManager(), new Logger());
+
+            var payments = sut.GetPaymentsList(new ListItem { Id = MobileKnowledgeProviderId });
+
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void StaticDataservice_test()
+        {
+            var service = new StaticDataservice();
+            service.Url = "http://72.38.252.190:6928/XDS_IASPI.DLL/soap/IStaticData";
+
+            var cities = service.GetDropoffCityList("taxi", "test", 17);
+
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void get_dropoff_cities_test()
+        {
+            var sut = new StaticDataWebServiceClient(new FakeConfigurationManager(), new Logger());
+
+            var cities = sut.GetDropoffCity(new ListItem { Id = MobileKnowledgeProviderId });
+
+            Assert.Inconclusive();
         }
        
     }
