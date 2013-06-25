46628f787f9aab9ec31c62d5097b41a7f2300030
Chris-Pc <devnull@localhost>
2013-06-25 12:07:54 -0400

    Removing dead code

--- Src/Server/apcurium.CMT.Web.Tests/CmtPaymentServiceClientFixture.cs
+++ Src/Server/apcurium.CMT.Web.Tests/CmtPaymentServiceClientFixture.cs
@@ -25,22 +25,17 @@ namespace apcurium.CMT.Web.Tests
         [SetUp]
         public void Setup()
         {
-
-            
-
-
         }
-
-
 
         private CmtPaymentClient GetCmtPaymentClient()
         {
 
-            string MERCHANT_TOKEN = "E4AFE87B0E864228200FA947C4A5A5F98E02AA7A3CFE907B0AD33B56D61D2D13E0A75F51641AB031500BD3C5BDACC114";
-            string CONSUMER_KEY = "vmAoqWEY3zIvUCM4";
-            string CONSUMER_SECRET_KEY = "DUWzh0jAldPc7C5I";
-            string SANDBOX_BASE_URL = "https://payment-sandbox.cmtapi.com/v2/merchants/" + MERCHANT_TOKEN + "/";
-            string BASE_URL = SANDBOX_BASE_URL; // for now will will not use production		
+            const string MERCHANT_TOKEN = "E4AFE87B0E864228200FA947C4A5A5F98E02AA7A3CFE907B0AD33B56D61D2D13E0A75F51641AB031500BD3C5BDACC114";
+            const string CONSUMER_KEY = "vmAoqWEY3zIvUCM4";
+            const string CONSUMER_SECRET_KEY = "DUWzh0jAldPc7C5I";
+            const string SANDBOX_BASE_URL = "https://payment-sandbox.cmtapi.com/v2/merchants/" + MERCHANT_TOKEN + "/";
+            const string BASE_URL = SANDBOX_BASE_URL; // for now will will not use production	
+	
             return new CmtPaymentClient(BASE_URL, CONSUMER_KEY, CONSUMER_SECRET_KEY, AuthorizationRequest.CurrencyCodes.Main.UnitedStatesDollar, true);
         }
 
@@ -89,98 +84,6 @@ namespace apcurium.CMT.Web.Tests
 
             var response = client.ForgetTokenizedCard(token);
             Assert.AreEqual(1, response.ResponseCode, response.ResponseMessage);
-        }
-        //[Test]
-        //public void when_preauthorizing_a_credit_card_payment_usd()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString,
-        //                           AuthorizationRequest.CurrencyCodes.Main.UnitedStatesDollar);
-
-        //    when_preauthorizing_a_credit_card_payment();
-        //}
-
-        //[Test]
-        //public void when_preauthorizing_a_credit_card_payment_cad()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString,
-        //                           AuthorizationRequest.CurrencyCodes.Main.CanadaDollar);
-
-        //    when_preauthorizing_a_credit_card_payment();
-
-        //}
-
-        //[Test]
-        //public void when_preauthorizing_a_credit_card_payment_eurp()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString,
-        //                           AuthorizationRequest.CurrencyCodes.Main.EuroMemberCountries);
-
-        //    when_preauthorizing_a_credit_card_payment();
-        //}
-
-        private void when_preauthorizing_a_credit_card_payment()
-        {
-            var client = GetCmtPaymentClient();
-
-            var token = client.Tokenize(TestCreditCards.Mastercard.Number, TestCreditCards.Mastercard.ExpirationDate).CardOnFileToken;
-
-            const double amount = 21.56;
-            var response = client.PreAuthorizeTransaction(token, amount, "orderNumber");
-
-            Assert.AreEqual(1, response.ResponseCode, response.ResponseMessage);
-
-            Assert.AreEqual(amount, response.Amount, .001);
-            //if (response.CurrencyCode !=
-            //    DummyConfigManager.GetSetting(AuthorizationRequest.CurrencyCodes.CurrencyCodeString))
-            //{
-            //    Assert.Inconclusive("Currency Codes dont match");
-            //}
-        }
-
-        //[Test]
-        //public void when_capturing_a_preauthorized_a_credit_card_payment_euro()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString,
-        //   AuthorizationRequest.CurrencyCodes.Main.EuroMemberCountries);
-
-        //    when_capturing_a_preauthorized_a_credit_card_payment();
-        //}
-        //[Test]
-        //public void when_capturing_a_preauthorized_a_credit_card_payment_usd()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString,
-        //   AuthorizationRequest.CurrencyCodes.Main.UnitedStatesDollar);
-
-        //    when_capturing_a_preauthorized_a_credit_card_payment();
-        //}
-        //[Test]
-        //public void when_capturing_a_preauthorized_a_credit_card_payment_cad()
-        //{
-        //    DummyConfigManager.AddOrSet(AuthorizationRequest.CurrencyCodes.CurrencyCodeString, AuthorizationRequest.CurrencyCodes.Main.CanadaDollar);
-
-        //    when_capturing_a_preauthorized_a_credit_card_payment();
-        //}
-
-        private void when_capturing_a_preauthorized_a_credit_card_payment()
-        {
-            
-            var client = GetCmtPaymentClient();
-
-
-            var token = client.Tokenize(TestCreditCards.Discover.Number, TestCreditCards.Discover.ExpirationDate).CardOnFileToken;
-
-            const double amount = 99.50;
-            const string orderNumber = "12345";
-            var authorization = client.PreAuthorizeTransaction(token, amount, orderNumber);
-
-            var response = client.CapturePreAuthorized(authorization.TransactionId, orderNumber);
-
-            Assert.AreEqual(1, response.ResponseCode, response.ResponseMessage);
-            Assert.AreEqual(amount, response.Amount, .001);
-            //if (response.CurrencyCode != DummyConfigManager.GetSetting(AuthorizationRequest.CurrencyCodes.CurrencyCodeString))
-            //{
-            //    Assert.Inconclusive("Currency Codes dont match");
-            //}
         }
 
 
