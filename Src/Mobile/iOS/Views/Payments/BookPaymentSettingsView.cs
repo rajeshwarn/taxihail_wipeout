66d87ef0bf13e968ee983af32cab0b37536b4a12
Vincent Costel <vincent.costel@gmail.com>
2013-06-26 11:46:51 -0400

    fixed Label placeholder value on iOS in Payment view

--- Src/Mobile/iOS/Views/Payments/BookPaymentSettingsView.cs
+++ Src/Mobile/iOS/Views/Payments/BookPaymentSettingsView.cs
@@ -24,29 +24,29 @@ namespace apcurium.MK.Booking.Mobile.Client.Views
 
         double MeterAmount{
             get{
-                return MeterAmountLabel.Text.FromDollars();
+                return CultureProvider.ParseCurrency(MeterAmountLabel.Text);
             }
             set{
-                MeterAmountLabel.Text = value.ToDollars();
+                MeterAmountLabel.Text = CultureProvider.FormatCurrency(value);
             }
         }
 
         double TipAmount{
             get{
-                return TipAmountLabel.Text.FromDollars();
+                return CultureProvider.ParseCurrency(TipAmountLabel.Text);
             }
             set{
-                TipAmountLabel.Text = value.ToDollars();
+                TipAmountLabel.Text = CultureProvider.FormatCurrency(value);
             }
         }
 
         double TotalAmount{
             get{
-                return TotalAmountLabel.Text.FromDollars();
+                return CultureProvider.ParseCurrency(TotalAmountLabel.Text);
             }
             set{
-                TotalAmountLabel.Text = value.ToDollars();
-                ViewModel.Amount = TotalAmountLabel.Text;//Todo ugly binding done in code behind
+                TotalAmountLabel.Text = CultureProvider.FormatCurrency(value);
+                ViewModel.TextAmount = TotalAmountLabel.Text;//Todo ugly binding done in code behind
             }
         }
 
@@ -112,6 +112,8 @@ namespace apcurium.MK.Booking.Mobile.Client.Views
                 { btConfirm, "{'TouchUpInside':{'Path':'ConfirmOrderCommand'}}"},   
                 { TipSlider, new B("Value","PaymentPreferences.Tip",B.Mode.TwoWay) },
                 //{ TotalAmountLabel, new B("Text","Amount")},//See above
+                { MeterAmountLabel, new B("Placeholder", "PlaceholderAmount") },
+                { TipAmountLabel, new B("Placeholder", "PlaceholderAmount") },
                 { btCreditCard, 
                     new B("Text","PaymentPreferences.SelectedCreditCard.FriendlyName")
                         .Add("Last4Digits","PaymentPreferences.SelectedCreditCard.Last4Digits")
