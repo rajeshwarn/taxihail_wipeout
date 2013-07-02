7f223325b20c2e14fc03df4f75ef7687cda025c2
Alex Proteau <alex.proteau@apcurium.com>
2013-07-02 10:38:37 -0400

    Beginning of the splash screen resize

---
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/ConfigCropImage.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace apcurium.MK.Booking.ConfigTool
+{
+	public class ConfigCropImage : Config
+    {
+        public string Source { get; set; }
+        public string Destination{ get; set; }
+
+		public ConfigCropImage(AppConfig parent)
+            : base(parent)
+        {
+        }
+
+        public override void Apply()
+        {
+            var destPath = Path.Combine(Parent.SrcDirectoryPath, PathConverter.Convert(  Destination ) );
+            var sourcePath = Path.Combine(Parent.ConfigDirectoryPath, PathConverter.Convert(  Source ) );
+            if (!File.Exists(sourcePath))
+            {
+                sourcePath = Path.Combine(Parent.CommonDirectoryPath, PathConverter.Convert(Source));
+            }
+            File.Copy(sourcePath, destPath, true);
+        }
+
+		public override string ToString ()
+		{
+			return string.Format ("[ConfigCropImage: Source={0}, Destination={1}]", Source, Destination);
+		}
+        
+    }
+
+}
