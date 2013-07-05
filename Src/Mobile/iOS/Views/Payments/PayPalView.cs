da909d9b77b20ba082e6537ab40407a808f3b052
Vincent Costel <vincent.costel@gmail.com>
2013-07-05 11:56:01 -0400

    MKTAXI-800: show message when payment cancelled

--- Src/Mobile/iOS/Views/Payments/PayPalView.cs
+++ Src/Mobile/iOS/Views/Payments/PayPalView.cs
@@ -6,6 +6,7 @@ using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using apcurium.MK.Booking.Mobile.ViewModels.Payment;
 using Cirrious.MvvmCross.Views;
+using apcurium.MK.Booking.Mobile.Client.PlatformIntegration;
 
 namespace apcurium.MK.Booking.Mobile.Client.Views.Payments
 {
@@ -29,15 +30,27 @@ namespace apcurium.MK.Booking.Mobile.Client.Views.Payments
             base.ViewDidLoad ();
             ViewModel.Load();
 
+			// Hide navigation bar, full screen height is required for displaying paypal page
+			this.NavigationController.NavigationBarHidden = true;
+
             webView.ShouldStartLoad = LoadHook;
-            webView.LoadRequest(new NSUrlRequest(new NSUrl(ViewModel.Url)));
+			var request = new NSUrlRequest (new NSUrl (ViewModel.Url));
+            
+			webView.LoadRequest(request);
+			webView.LoadFinished += (s,e) => {
+				ViewModel.WebViewLoadFinished();
+			}; 
         }
 
         bool LoadHook (UIWebView sender, NSUrlRequest request, UIWebViewNavigationType navType)
         {
 			if(request.Url.Scheme.StartsWith("taxihail"))
 			{
-				ViewModel.Finish.Execute();
+				if (request.Url.Host == "success") {
+					ViewModel.Finish.Execute (true);
+				} else {
+					ViewModel.Finish.Execute (false);
+				}
 				return false;
 			}
 			return true;
