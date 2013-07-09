83083a006b71c3a2b054275de9e304b80927ca39
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-07-09 10:52:34 -0400

    MKTAXI-893 [Android] Launch app from browser

--- Src/Mobile/Android/Activities/SplashActivity.cs
+++ Src/Mobile/Android/Activities/SplashActivity.cs
@@ -26,7 +26,8 @@ using apcurium.MK.Booking.Mobile.AppServices.Impl;
 namespace apcurium.MK.Booking.Mobile.Client.Activities
 {
     [Activity(Label = "@string/ApplicationName", MainLauncher = true, Theme = "@style/Theme.Splash", NoHistory = true, Icon = "@drawable/icon", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
-    public class SplashActivity : MvxBaseSplashScreenActivity
+	[IntentFilter(new[] { "android.intent.action.VIEW" }, Categories = new[] { "android.intent.category.DEFAULT", "android.intent.category.BROWSABLE" }, DataScheme = "@string/ApplicationName")]
+	public class SplashActivity : MvxBaseSplashScreenActivity
     {
         public SplashActivity()
         {
