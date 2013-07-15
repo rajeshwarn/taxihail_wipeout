5ba8a15ab1b4c195428f4fab4a216992fad975a4
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-07-15 12:10:32 -0400

    Added IBS test project

---
+++ Src/Server/MK.Booking.IBS.Test/AccountFixture/given_no_account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MK.Booking.IBS.Test.AccountFixture
+{
+    [TestFixture]
+    public class given_no_account
+    {
+        protected WebAccount3Service Sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.Sut = new WebAccount3Service
+            {
+                Url = "http://72.38.252.190:6928/XDS_IASPI.DLL/soap/IWebAccount3"
+            };
+        }
+
+
+        [Test]
+        public void when_creating_an_account()
+        {
+            // Arrange
+            var accountId = Guid.NewGuid().ToString().Substring(0, 5);
+            var email = "vincent.costel@apcurium.com";
+            var account = new TBookAccount3
+            {
+                WEBID = accountId,
+                Address = new TWEBAddress() { },
+                Email2 = email,
+                Title = "",
+                FirstName = "Apcurium",
+                LastName = "Test",
+                Phone = "5141234569",
+                MobilePhone = "5141234569",
+                WEBPassword = "123456"
+            };
+
+            // Act
+            var ibsAcccountId = Sut.SaveAccount3("taxi", "test", account);
+
+            // Assert
+            Assert.Greater(ibsAcccountId, 0);
+        }
+    }
+}
