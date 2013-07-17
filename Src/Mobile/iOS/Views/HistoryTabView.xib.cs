5730d1c02b2e85b535e3282aa8ccef413bf48d91
apcurium <matthieu.duluc@apcurium.com>
2013-07-17 16:17:17 -0400

    Use DisplayAddress instead of BookAddress where relevant (iOS)

--- Src/Mobile/iOS/Views/HistoryTabView.xib.cs
+++ Src/Mobile/iOS/Views/HistoryTabView.xib.cs
@@ -29,7 +29,7 @@ namespace apcurium.MK.Booking.Mobile.Client
 		const string CellBindingText = @"
                 {
                    'FirstLine':{'Path':'Title'},
-                   'SecondLine':{'Path':'PickupAddress.BookAddress'},
+                   'SecondLine':{'Path':'PickupAddress.DisplayAddress'},
                    'ShowRightArrow':{'Path':'ShowRightArrow'},
                    'ShowPlusSign':{'Path':'ShowPlusSign'},
                    'IsFirst':{'Path':'IsFirst'},
