53c9814b423afde305258895ad14953e6762903e
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-07-18 11:55:59 -0400

    MKTAXI-937 Remove Card on File Type

--- Src/Server/MK.Booking.Api.Contract/Resources/ReferenceData.cs
+++ Src/Server/MK.Booking.Api.Contract/Resources/ReferenceData.cs
@@ -5,8 +5,6 @@ namespace apcurium.MK.Booking.Api.Contract.Resources
 {
     public class ReferenceData : BaseDTO
     {
-        public const int CreditCardOnFileType = 666;
-
         public IList<ListItem> CompaniesList { get; set; }
         public IList<ListItem> VehiclesList { get; set; }
         public IList<ListItem> PaymentsList { get; set; }
