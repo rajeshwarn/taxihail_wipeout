5b6cc19c57d3b579077f93a5287738f4013c4090
Alex Proteau <alex.proteau@apcurium.com>
2013-08-09 15:37:56 -0400

    Fix issue with configuration

--- Src/Server/MK.Booking.Api.Client/TaxiHail/ConfigurationClientService.cs
+++ Src/Server/MK.Booking.Api.Client/TaxiHail/ConfigurationClientService.cs
@@ -8,58 +8,60 @@ namespace apcurium.MK.Booking.Api.Client.TaxiHail
     public class ConfigurationClientService : BaseServiceClient, IConfigurationManager
     {
         private static Dictionary<string, string> _settings = null;
+        private static object lockObject = new object ();
 
-        public ConfigurationClientService(string url, string sessionId)
+        public ConfigurationClientService (string url, string sessionId)
             : base(url, sessionId)
         {
         }
 
-        public void Reset()
+        public void Reset ()
         {
             _settings = null;
         }
 
-        public string GetSetting(string key)
+        public string GetSetting (string key)
         {
-            if ( (_settings == null) || ( !_settings.ContainsKey( key ) ) )
-            {
-                Load();
+            if ((_settings == null) || (!_settings.ContainsKey (key))) {
+                Load ();
             }
 
-            if (  !_settings.ContainsKey( key )  )
-            {
+            if (!_settings.ContainsKey (key)) {
                 return null;
             }
 
-            return _settings[key];
+            return _settings [key];
         }
 
-        private void Load()
+        private void Load ()
         {            
 
-            var settings = Client.Get<Dictionary<string,string>>("/settings");
-            _settings = new Dictionary<string, string>();
-            settings.ForEach( s=> _settings.Add( s.Key, s.Value ));
-            _settings.ToString();
+            lock (lockObject) {
+                if (_settings == null) {
+                    var settings = Client.Get<Dictionary<string,string>> ("/settings");
+                    _settings = new Dictionary<string, string> ();
+                    settings.ForEach (s => _settings.Add (s.Key, s.Value));
+                    _settings.ToString ();
+                }
+            }
         }
 
-        public void SetSetting(string key, string value)
+        public void SetSetting (string key, string value)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException ();
         }
 
-        public IDictionary<string, string> GetSettings()
+        public IDictionary<string, string> GetSettings ()
         {
-            if (_settings == null)
-            {
-                Load();
+            if (_settings == null) {
+                Load ();
             }
             return _settings;            
         }
 
-        public void SetSettings(IDictionary<string, string> appSettings)
+        public void SetSettings (IDictionary<string, string> appSettings)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException ();
         }
     }
 }
