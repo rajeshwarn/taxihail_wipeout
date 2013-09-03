9d18f522dc5eb5127b31c5e697b2a6d9c8f5916a
vincent costel <vincent.costel@VINCENT-PC.apcurium.local>
2013-09-03 11:19:08 -0400

    Handle new IBS wosTIMEOUT status in web app

--- Src/Server/apcurium.MK.Web/models/OrderStatus.js
+++ Src/Server/apcurium.MK.Web/models/OrderStatus.js
@@ -2,13 +2,21 @@
 
     TaxiHail.OrderStatus = Backbone.Model.extend({
 
+        initialize: function() {
+            this.on('change:ibsStatusId', function(model, value, options) {
+                if (value === 'wosTIMEOUT') {
+                    model.trigger('ibs:timeout');
+                }
+            });
+        },
+
         url: function() {
             return 'api/account/orders/' + this.id + '/status/';
         },
 
         isActive: function() {
             var status = this.get('ibsStatusId');
-            return _.indexOf(['wosCANCELLED', 'wosCANCELLED_DONE', 'wosDONE', 'wosLOADED'], status) == -1;
+            return _.indexOf(['wosCANCELLED', 'wosCANCELLED_DONE', 'wosDONE', 'wosLOADED', 'wosTIMEOUT'], status) == -1;
         },
 
         isCompleted: function() {
--- Src/Server/apcurium.MK.Web/views/BookingStatusView.js
+++ Src/Server/apcurium.MK.Web/views/BookingStatusView.js
@@ -14,8 +14,15 @@
             this.interval = window.setInterval(_.bind(function() {
                 this.fetch();
             }, status), 5000);
+            
+            // Variable use to store the reference to window.timeout
+            // after an order has timed out
+            this.redirectTimeout = null;
+
+
             status.on('change:ibsStatusId', this.render, this);
             status.on('change:ibsStatusId', this.onStatusChanged, this);
+            status.on('ibs:timeout', this.ontimeout, this);
         },
 
         render: function() {
@@ -65,6 +72,8 @@
 
             // Stop polling for Order Status updates
             window.clearInterval(this.interval);
+
+            window.clearTimeout(this.redirectTimeout);
 
         },
 
@@ -133,8 +142,13 @@
 
                     }, this);
             }
-        }
+        },
 
+        ontimeout: function(model, status) {
+            this.redirectTimeout = window.timeout(10 * 1000, function() {
+                TaxiHail.app.navigate('', { trigger: true });
+            });
+        }
 
     });
 }());
