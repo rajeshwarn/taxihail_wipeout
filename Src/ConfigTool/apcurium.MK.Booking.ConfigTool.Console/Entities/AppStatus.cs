daad43e263b7353be59ca1700e5602a51c8b7e77
Alex Proteau <alex.proteau@apcurium.com>
2013-10-22 12:00:45 -0400

    Added the entities

---
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/Entities/AppStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerPortal.Web.Entities
+{
+    public enum AppStatus
+    {
+        Open,
+        LayoutCompleted,
+        LayoutRejected,
+        Test,
+        Production
+    }
+}
---
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/Entities/Questionnaire.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CustomerPortal.Web.Properties;
+
+namespace CustomerPortal.Web.Entities
+{
+    public class Questionnaire
+    {
+        [Display(Name = "AppName", Description = "AppNameHelp", ResourceType = typeof(Resources))]
+        [StringLength(30)]
+        public string AppName { get; set; }
+
+            
+        [Display(Name = "CompanyWebsiteUrl", Description = "CompanyWebsiteUrlHelp", ResourceType = typeof(Resources))]
+        [Url]
+        public string CompanyWebsiteUrl { get; set; }
+
+        [Display(Name = "CompanyPhoneNumber", Description = "CompanyPhoneNumberHelp", ResourceType = typeof(Resources))]
+        public string CompanyPhoneNumber { get; set; }
+
+        [Display(Name = "AboutUsLink", Description = "AboutUsLinkHelp", ResourceType = typeof(Resources))]
+        [Url]
+        public string AboutUsLink { get; set; }
+
+        [Display(Name = "SupportContactEmail", Description = "SupportContactEmailHelp", ResourceType = typeof(Resources))]
+        [EmailAddress]
+        public string SupportContactEmail { get; set; }
+
+        [Display(Name = "FlagDropRate", Description = "FlagDropRateHelp", ResourceType = typeof(Resources))]
+        [Range(0d, 100d)]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false)]
+        public decimal? FlagDropRate { get; set; }
+
+        [Display(Name = "MileageRate", Description = "MileageRateHelp", ResourceType = typeof(Resources))]
+        [Range(0d, 100d)]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false)]
+        public decimal? MileageRate { get; set; }
+
+        [Display(Name = "UnitOfLength", Description = "UnitOfLengthHelp", ResourceType = typeof(Resources))]
+        public UnitOfLength UnitOfLength { get; set; }
+
+        [Display(Name = "CorporateColor", Description = "CorporateColorHelp", ResourceType = typeof(Resources))]
+        [UIHint("Color")]
+        public string CorporateColor { get; set; }
+    }
+}
