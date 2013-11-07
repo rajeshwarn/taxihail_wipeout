c42ff6cf391224c6a5abeef8e8b92a26fa77a56b
Alex Proteau <alex.proteau@apcurium.com>
2013-11-06 21:58:05 -0500

    Version bump 1.4.26

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.25")]
-[assembly: AssemblyVersion("1.4.25")]
-[assembly: AssemblyFileVersion("1.4.25")]
+// [assembly: AssemblyVersion("1.4.26")]
+[assembly: AssemblyVersion("1.4.26")]
+[assembly: AssemblyFileVersion("1.4.26")]
 
