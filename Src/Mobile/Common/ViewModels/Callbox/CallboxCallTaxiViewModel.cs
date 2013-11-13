6490ede2acbc731898530d6b6ba9c1db02839de5
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-11-13 13:30:12 -0500

    MKTAXI-1005 Fix red screen flashing when passengers were picked up and no more cars were on location

--- Src/Mobile/Common/ViewModels/Callbox/CallboxCallTaxiViewModel.cs
+++ Src/Mobile/Common/ViewModels/Callbox/CallboxCallTaxiViewModel.cs
@@ -4,36 +4,36 @@ using System;
 
 namespace apcurium.MK.Booking.Mobile.ViewModels.Callbox
 {
-    public class CallboxCallTaxiViewModel : BaseCallboxViewModel
-    {
-        public override void Load()
-        {
-            base.Load();
-        }
+	public class CallboxCallTaxiViewModel : BaseCallboxViewModel
+	{
+		public override void Load()
+		{
+			base.Load();
+		}
 
-        public IMvxCommand CallTaxi
-        {
-            get
-            {
-                return this.GetCommand(() => 
-                                       InvokeOnMainThread ( ()=>
-                                        {
-                                            this.MessageService.ShowEditTextDialog(Resources.GetString("BookTaxiTitle"), 
-                                            Resources.GetString("BookTaxiPassengerName"), Resources.GetString("Ok"), 
-                                            s =>
-                                            { 
-                                                  try
-                                                    {
-                            RequestClose( this );
-                                                        RequestNavigate<CallboxOrderListViewModel>(new { passengerName = s },true, MvxRequestedBy.UserAction);                                                        
-                                                    }
-                                                   catch( Exception e )
-                                                    {
-                                                        Logger.LogError( e );
-                                                    }
-                                            });
-                                        }));
-            }
-        }
-    }
+		public IMvxCommand CallTaxi
+		{
+			get
+			{
+				return this.GetCommand(() => 
+					InvokeOnMainThread ( ()=>
+						{
+							this.MessageService.ShowEditTextDialog(Resources.GetString("BookTaxiTitle"), 
+								Resources.GetString("BookTaxiPassengerName"), Resources.GetString("Ok"), 
+								s =>
+								{ 
+									try
+									{
+										RequestClose( this );
+										RequestNavigate<CallboxOrderListViewModel>(new { passengerName = s },true, MvxRequestedBy.UserAction);                                                        
+									}
+									catch( Exception e )
+									{
+										Logger.LogError( e );
+									}
+								});
+						}));
+			}
+		}
+	}
 }
