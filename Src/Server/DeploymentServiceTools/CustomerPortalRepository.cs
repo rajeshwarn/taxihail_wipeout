0f3e12eef78bb7fb60f7ad39d2b6d91d2acbb4e0
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-11-15 16:25:14 -0500

    TAXIOPS-56 Modify build tool to create new version in customer portal

---
+++ Src/Server/DeploymentServiceTools/CustomerPortalRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace DeploymentServiceTools
+{
+    public class CustomerPortalRepository
+    {
+        public bool CreateNewVersion(string companyId, string versionNumber, string websiteUrl, string ipaFileName, FileStream ipaFile, string apkFileName, FileStream apkFile)
+         {
+             var data = new
+             {
+                 CompanyId = companyId,
+                 VersionNumber = versionNumber,
+                 WebsiteUrl = websiteUrl
+             };
+
+             using (var client = new HttpClient())
+             {
+                 using (var multipartFormDataContent = new MultipartFormDataContent())
+                 {
+                     multipartFormDataContent.Add(new StringContent(JsonConvert.SerializeObject(data)), "data");
+
+                     var ipaContent = new StreamContent(ipaFile);
+                     ipaContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                     ipaContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = ipaFileName };
+                     multipartFormDataContent.Add(ipaContent);
+
+                     var apkContent = new StreamContent(apkFile);
+                     apkContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                     apkContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = apkFileName };
+                     multipartFormDataContent.Add(apkContent);
+
+                     var result = client.PostAsync(Path.Combine(ConfigurationManager.AppSettings["CustomerPortalAPIUrl"], "/admin/version"), multipartFormDataContent).Result;
+                     return result.StatusCode.Equals(HttpStatusCode.OK);
+                 }
+             }
+         }
+    }
+}
