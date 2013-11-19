a131807257cc9410997d99cdc889d3ff414c7a88
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-11-19 17:12:53 -0500

    TAXIOPS-62 Won’t work because of a bug in Mono (cookies with version 1 and having a domain not starting by “.” are invalid on Mono, based on the cookie standard) but some websites use them (developer.apple.com) and .net ignores this standard.

--- Src/Server/MK.DeploymentService.Mobile/Main.cs
+++ Src/Server/MK.DeploymentService.Mobile/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using DeploymentServiceTools;
+using Cupertino;
 
 namespace MK.DeploymentService.Mobile
 {
@@ -6,8 +8,12 @@ namespace MK.DeploymentService.Mobile
 	{
 		public static void Main (string[] args)
 		{
+//			var test = new Agent ();
+//			test.Login ("https://daw.apple.com/cgi-bin/WebObjects/DSAuthWeb.woa/wa/login?&appIdKey=891bd3417a7776362562d2197f89480a8547b108fd934911bcbea0110d07f757&path=%2F%2Fdevcenter%2Fios%2Findex.action");
+//			test.Get ("https://developer.apple.com/account/ios/profile/profileList.action?type=limited");
+
             var service = new DeploymentJobService();
-            service.Start();
+			service.Start();
             while (true) { }
 		}
 	}
