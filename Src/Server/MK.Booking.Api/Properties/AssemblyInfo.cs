b9cd23da12e570aa8c0bc9861acf83077a921af6
Alex Proteau <alex.proteau@apcurium.com>
2013-11-28 15:58:16 -0500

    Version bump to 1.4.30

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using PostSharp.Toolkit.Diagnostics;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4.29")]
-[assembly: AssemblyVersion("1.4.29")]
-[assembly: AssemblyFileVersion("1.4.29")]
+// [assembly: AssemblyVersion("1.4.30")]
+[assembly: AssemblyVersion("1.4.30")]
+[assembly: AssemblyFileVersion("1.4.30")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
