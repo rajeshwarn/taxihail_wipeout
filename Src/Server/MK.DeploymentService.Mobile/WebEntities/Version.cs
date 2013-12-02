5bbfed0db22d2178e6f0958215270ec5921fb2b2
Alex Proteau <alex.proteau@apcurium.com>
2013-12-02 13:51:25 -0500

    Migration of mobile build service to use portal

---
+++ Src/Server/MK.DeploymentService.Mobile/WebEntities/Version.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerPortal.Web.Entities
+{
+    public class Version
+    {
+        public string VersionId { get; set; }
+
+        [Required]
+        public string Number { get; set; }
+
+        [Display(Name = "iOS App Store Package File (.ipa)")]
+        public string IpaFilename { get; set; }
+        [Display(Name = "Android Application Package File (.apk)")]
+        public string ApkFilename { get; set; }
+
+        [Display(Name = "Website Url")]
+        [UIHint("Url")]
+        public string WebsiteUrl { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+    }
+}
