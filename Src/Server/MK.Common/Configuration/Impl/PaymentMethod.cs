e582b4d46b13408c60974ec2bdf1ce67ddf7f0d5
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2013-12-19 15:07:45 -0500

    MKTAXI-1154 Add ridelinq cmt payment method

--- Src/Server/MK.Common/Configuration/Impl/PaymentMethod.cs
+++ Src/Server/MK.Common/Configuration/Impl/PaymentMethod.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace apcurium.MK.Common.Configuration.Impl
+﻿namespace apcurium.MK.Common.Configuration.Impl
 {
-
     public enum PaymentMethod
     {
         None,
         Cmt,
+        RideLinqCmt,
         Braintree,
         Fake
     }
