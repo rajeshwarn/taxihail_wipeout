7f1bb297b32d615c157a1863d01d95f7e9d78fa5
Vincent Costel <vincent.costel@apcurium.com>
2014-01-09 09:36:10 +0100

    Fixed bug in VehicleService. Use special UseServiceClient overload for async operations

--- Src/Mobile/Common/AppServices/Impl/VehicleService.cs
+++ Src/Mobile/Common/AppServices/Impl/VehicleService.cs
@@ -7,17 +7,10 @@ namespace apcurium.MK.Booking.Mobile.AppServices.Impl
 	public class VehicleService : BaseService, IVehicleService
     {
 		
-		public async Task<AvailableVehicle[]> GetAvailableVehiclesAsync(double latitude, double longitude)
+		public Task<AvailableVehicle[]> GetAvailableVehiclesAsync(double latitude, double longitude)
         {
-			AvailableVehicle[] vehicles = null;
-
-            //todo potential bug here with the async in front of the lambda
-			UseServiceClient<IVehicleClient>(async service =>
-				{
-					vehicles = await service.GetAvailableVehiclesAsync( latitude, longitude);
-				});
-
-			return vehicles;
+			return UseServiceClient<IVehicleClient, AvailableVehicle[]>(service => service
+				.GetAvailableVehiclesAsync( latitude, longitude));
         }
     }
 }
