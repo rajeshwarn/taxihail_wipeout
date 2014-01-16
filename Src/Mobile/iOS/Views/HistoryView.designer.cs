86260b67266438cf2e14df0dc4e643fdf7eab61f
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-01-16 11:25:01 -0500

    MKTAXI-1256 [iOS] Update Order History list

---
+++ Src/Mobile/iOS/Views/HistoryView.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace apcurium.MK.Booking.Mobile.Client.Views
+{
+	[Register ("HistoryView")]
+	partial class HistoryView
+	{
+		[Outlet]
+		MonoTouch.UIKit.UILabel lblNoHistory { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITableView tableOrders { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (tableOrders != null) {
+				tableOrders.Dispose ();
+				tableOrders = null;
+			}
+
+			if (lblNoHistory != null) {
+				lblNoHistory.Dispose ();
+				lblNoHistory = null;
+			}
+		}
+	}
+}
