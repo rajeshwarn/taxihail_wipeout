95283e9fcb3f4eb581408a75ae99f727a67edf21
Leon Pelletier <leon.pelletier@gmail.com>
2014-01-21 17:29:01 -0500

    MKTAXI-1330 - Update Confirm Car Number screen for Android

--- Src/Mobile/Android/Activities/Book/ConfirmCarNumberActivity.cs
+++ Src/Mobile/Android/Activities/Book/ConfirmCarNumberActivity.cs
@@ -5,7 +5,7 @@ using apcurium.MK.Booking.Mobile.ViewModels.Payment;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 {
-    [Activity(Label = "ConfirmCarNumberActivity", Theme = "@android:style/Theme.NoTitleBar",
+    [Activity(Label = "ConfirmCarNumberActivity", Theme = "@style/MainTheme",
         ScreenOrientation = ScreenOrientation.Portrait)]
     public class ConfirmCarNumberActivity : BaseBindingActivity<ConfirmCarNumberViewModel>
     {
