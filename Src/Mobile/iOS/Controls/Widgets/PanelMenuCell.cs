ef750fa3321d1ae11b5e78666c3b993db604c3b9
lexe <aurelie.valton@apcurium.com>
2014-01-21 14:41:03 -0500

    MKTAXI-1295 : panelMenuView changed for IOS7 and code behind cleaned up for building the table view with new way to bind a view and to add it like a subview

---
+++ Src/Mobile/iOS/Controls/Widgets/PanelMenuCell.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Drawing;
+using System.Windows.Input;
+using Cirrious.MvvmCross.Binding.Touch.Views;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace apcurium.MK.Booking.Mobile.Client.Controls.Widgets
+{
+	public class PanelMenuCell : MvxStandardTableViewCell
+	{
+		public PanelMenuCell (string cellIdentifier, string bindingText)
+			: base (bindingText, UITableViewCellStyle.Default, new NSString (cellIdentifier), UITableViewCellAccessory.None)
+		{
+			SelectionStyle = UITableViewCellSelectionStyle.None;
+			Accessory = UITableViewCellAccessory.None;
+
+			BackgroundColor = UIColor.Clear;
+
+			TextLabel.Font = UIFont.FromName (FontName.HelveticaNeueLight, 34 / 2);
+			TextLabel.TextColor = UIColor.FromRGB (79, 76, 71);
+			TextLabel.BackgroundColor = UIColor.Clear;
+		}
+
+		public override void LayoutSubviews ()
+		{
+			base.LayoutSubviews ();
+		}
+
+	}
+}
+
