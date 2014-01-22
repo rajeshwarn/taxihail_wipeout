e26db15c0faf90fb06ff9fdc631e2a5db67c3419
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-01-22 11:15:03 -0500

    Add Style class

---
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/Entities/Style.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerPortal.Web.Entities
+{
+	public class Style
+	{
+
+		public string NavigationBarColor { get; set; }
+		public string TitleColor { get; set; }
+		public string WebAccentColor { get; set; }
+
+	}
+}
