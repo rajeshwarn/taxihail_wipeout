718ea40c4b2c61bdc495f2c3a60b756c7d60f290
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-01-22 15:21:01 -0500

    add alpha on main button

--- Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/AppConfig.cs
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/AppConfig.cs
@@ -179,7 +179,7 @@ namespace apcurium.MK.Booking.ConfigTool
 				{  
 					Destination=@"Mobile\Android\Resources\Values\Themes.xml", 
 					NodeSelector=@"//resources/color[@name=""button_main_background_color""]", 
-					SetterEle = (app,ele) => ele.InnerText = GetHexaColorCode(Company.Style.NavigationBarColor) + "4B" 
+					SetterEle = (app,ele) => ele.InnerText = GetHexaColorCode(Company.Style.NavigationBarColor) + "4b" 
 				},
 				new ConfigXML(this)
 				{  
