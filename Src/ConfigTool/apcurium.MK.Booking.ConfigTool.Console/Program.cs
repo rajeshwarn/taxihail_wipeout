bd90aeb3b0e0bf5583b1b023cf698c4313497a5e
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-01-22 15:17:38 -0500

    change company test

--- Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/Program.cs
+++ Src/ConfigTool/apcurium.MK.Booking.ConfigTool.Console/Program.cs
@@ -60,7 +60,7 @@ namespace apcurium.MK.Booking.ConfigTool
                     Console.WriteLine("");
                     Console.WriteLine("Enter the config number:");
                     var selectedText = Console.ReadLine();
-					int selected = 0;//int.Parse(selectedText);
+					int selected = 1;//int.Parse(selectedText);
 
 					Console.WriteLine("Enter the server url:");
 					var url = "http://test.taxihail.biz:8181/taxihaildemo/api/";//Console.ReadLine();
