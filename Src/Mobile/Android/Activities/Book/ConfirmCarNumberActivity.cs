68a3f1e5de91caef33032dc874f1a66270a79e66
Leon Pelletier <leon.pelletier@gmail.com>
2014-01-22 14:49:23 -0500

    MKTAX-1294 - Reverted ConfirmCarNumber Activity to old content

--- Src/Mobile/Android/Activities/Book/ConfirmCarNumberActivity.cs
+++ Src/Mobile/Android/Activities/Book/ConfirmCarNumberActivity.cs
@@ -1,27 +1,17 @@
-using System;
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.OS;
-using Android.Text;
-using Android.Views;
-using Android.Widget;
-using Cirrious.MvvmCross.ViewModels;
-using apcurium.MK.Booking.Mobile.Extensions;
-using apcurium.MK.Booking.Mobile.Infrastructure;
+
 using apcurium.MK.Booking.Mobile.ViewModels.Payment;
-using TinyIoC;
-using apcurium.MK.Booking.Mobile.Client.Activities;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 {
-    [Activity(Label = "ConfirmCarNumberActivity", Theme = "@style/MainTheme",
+    [Activity(Label = "ConfirmPaymentActivity", Theme = "@style/MainTheme",
         ScreenOrientation = ScreenOrientation.Portrait)]
-    public class ConfirmCarNumberActivity : BaseBindingActivity<ConfirmCarNumberViewModel>
+    public class ConfirmPaymentActivity : BaseBindingActivity<ConfirmCarNumberViewModel>
     {
         protected override int ViewTitleResourceId
         {
-            get { return Resource.String.PaymentView; }
+            get { return Resource.String.View_PaymentCreditCardsOnFile; }
         }
 
         protected override void OnViewModelSet()
