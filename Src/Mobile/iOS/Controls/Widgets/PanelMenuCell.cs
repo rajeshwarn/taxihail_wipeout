10e7273dfe461e68a1b711b4276c106c4a152ecb
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-01-28 14:33:52 -0500

    MKTAXI-1399 Fix selected color in iOS6

--- Src/Mobile/iOS/Controls/Widgets/PanelMenuCell.cs
+++ Src/Mobile/iOS/Controls/Widgets/PanelMenuCell.cs
@@ -15,8 +15,9 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls.Widgets
 		public PanelMenuCell (string cellIdentifier, string bindingText)
 			: base (bindingText, UITableViewCellStyle.Default, new NSString (cellIdentifier), UITableViewCellAccessory.None)
 		{
-			BackgroundView = new CustomCellBackgroundView(Frame, 0, UIColor.FromRGB (242, 242, 242), UIColor.White);
+            BackgroundView = new CustomCellBackgroundView(Frame, 0, UIColor.FromRGB (242, 242, 242), UIColor.White);
 
+            SelectionStyle = UITableViewCellSelectionStyle.None;
 			Accessory = UITableViewCellAccessory.None;
 
 			TextLabel.Font = UIFont.FromName (FontName.HelveticaNeueLight, 36 / 2);
@@ -43,6 +44,26 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls.Widgets
 			}
 		}
 
+        public override void TouchesBegan (NSSet touches, UIEvent evt)
+        {   
+            ((CustomCellBackgroundView)BackgroundView).Highlighted = true;
+            SetNeedsDisplay();
+            base.TouchesBegan ( touches, evt ); 
+        }
+
+        public override void TouchesEnded (NSSet touches, UIEvent evt)
+        {
+            ((CustomCellBackgroundView) BackgroundView).Highlighted = false;
+            SetNeedsDisplay();
+            base.TouchesEnded (touches, evt);
+        }
+
+        public override void TouchesCancelled (NSSet touches, UIEvent evt)
+        {
+            ((CustomCellBackgroundView) BackgroundView).Highlighted = false;
+            SetNeedsDisplay();
+            base.TouchesCancelled (touches, evt);
+        }
 	}
 }
 
