c95004a75c0771cbfd0c3d7b671087d781e2d023
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-01-29 18:05:47 -0500

    MKTAXI-1408 [iOS] Add shadow layer to OverlayView

--- Src/Mobile/iOS/Controls/Widgets/OverlayView.cs
+++ Src/Mobile/iOS/Controls/Widgets/OverlayView.cs
@@ -3,12 +3,19 @@ using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using System.Drawing;
 using System.Linq;
+using MonoTouch.CoreGraphics;
+using System.Collections.Generic;
+using apcurium.MK.Booking.Mobile.Client.Extensions;
+using MonoTouch.CoreAnimation;
 
 namespace apcurium.MK.Booking.Mobile.Client.Controls.Widgets
 {
     [Register("OverlayView")]
     public class OverlayView : UIView
     {
+        private float _radiusCorner = 2f;
+        private UIView _shadowView = null;
+
         public OverlayView(IntPtr handle) : base(handle)
         {
             Initialize();
@@ -26,7 +33,55 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls.Widgets
 
         private void Initialize()
         {
-            BackgroundColor = UIColor.White.ColorWithAlpha(0.9f);
+            BackgroundColor = UIColor.Clear;
+        }
+
+        public override void Draw (RectangleF rect)
+        {           
+            var context = UIGraphics.GetCurrentContext ();
+
+            var fillColor = UIColor.White.ColorWithAlpha(0.8f);
+            var textColor = UIColor.Black;
+
+            var roundedRectanglePath = UIBezierPath.FromRoundedRect (rect, _radiusCorner);
+
+            DrawBackground(context, rect, roundedRectanglePath, fillColor.CGColor);
+            DrawStroke(fillColor.CGColor);
+
+            SetNeedsDisplay();
+        }
+
+        void DrawBackground (CGContext context, RectangleF rect, UIBezierPath roundedRectanglePath, CGColor fillColor)
+        {
+            context.SaveState ();
+            context.BeginTransparencyLayer (null);
+            roundedRectanglePath.AddClip ();
+            context.SetFillColorWithColor(fillColor);
+            context.FillRect(rect);
+            context.EndTransparencyLayer ();
+            context.RestoreState ();
+        }
+
+        void DrawStroke(CGColor fillColor)
+        {
+            Layer.BorderWidth = 1.0f;
+            Layer.BorderColor = fillColor;
+            Layer.CornerRadius = _radiusCorner;
+
+            if (_shadowView == null)
+            {
+                _shadowView = new UIView(Frame);
+                _shadowView.BackgroundColor = UIColor.White.ColorWithAlpha(0.7f);
+                _shadowView.Layer.MasksToBounds = false;
+                _shadowView.Layer.ShadowColor = UIColor.FromRGBA(0, 0, 0, 127).CGColor;
+                _shadowView.Layer.ShadowOpacity = 1.0f;
+                _shadowView.Layer.ShadowRadius = _radiusCorner+1;
+                _shadowView.Layer.ShadowOffset = new SizeF(0.3f, 0.3f);
+                _shadowView.Layer.ShouldRasterize = true;        
+
+                this.Superview.InsertSubviewBelow(_shadowView, this);
+            }
+            _shadowView.Frame = Frame.Copy().Shrink(1);
         }
     }
 }
--- Src/Mobile/iOS/Extensions/RectangleFExtensions.cs
+++ Src/Mobile/iOS/Extensions/RectangleFExtensions.cs
@@ -9,21 +9,25 @@ namespace apcurium.MK.Booking.Mobile.Client.Extensions
             thisRectangleF.X += delta;
             return thisRectangleF;
         }
+
         public static RectangleF IncrementY(this RectangleF thisRectangleF, float delta)
         {
             thisRectangleF.Y += delta;
             return thisRectangleF;
         }
+
         public static RectangleF SetX(this RectangleF thisRectangleF, float value)
         {
             thisRectangleF.X = value;
             return thisRectangleF;
         }
+
         public static RectangleF SetY(this RectangleF thisRectangleF, float value)
         {
             thisRectangleF.Y = value;
             return thisRectangleF;
         }
+
         public static RectangleF SetBottom(this RectangleF thisRectangleF, float value)
         {
             thisRectangleF.Height = value - thisRectangleF.Y;
@@ -35,20 +39,27 @@ namespace apcurium.MK.Booking.Mobile.Client.Extensions
             thisRectangleF.Y += numberOfPixels;
             thisRectangleF.X += numberOfPixels;
             thisRectangleF.Width -= (numberOfPixels*2);
-            thisRectangleF.Height -=(numberOfPixels*2);
+            thisRectangleF.Height -= (numberOfPixels*2);
             return thisRectangleF;
         }
-        
+
+        public static RectangleF Copy(this RectangleF thisRectangleF)
+        {
+            return new RectangleF(thisRectangleF.X, thisRectangleF.Y, thisRectangleF.Width, thisRectangleF.Height);
+        }
+
         public static RectangleF IncrementHeight(this RectangleF thisRectangleF, float delta)
         {
             thisRectangleF.Height += delta;
             return thisRectangleF;
         }
+
         public static RectangleF IncrementWidth(this RectangleF thisRectangleF, float delta)
         {
             thisRectangleF.Width += delta;
             return thisRectangleF;
         }
+
         public static RectangleF SetWidth(this RectangleF thisRectangleF, float value)
         {
             thisRectangleF.Width = value;
