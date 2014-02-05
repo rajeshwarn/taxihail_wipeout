413b221a4ca8fc1abcbcfb7ab6bdb3d0ad4533f8
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-02-05 16:58:08 -0500

    small refactoring, extract hint management in interface to support a more generic way for subviews management

---
+++ Src/Mobile/Common/PresentationHints/IChangePresentation.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace apcurium.MK.Booking.Mobile.PresentationHints
+{
+    public interface IChangePresentation
+    {
+		void ChangeState(ChangeStatePresentationHint hint);
+    }
+}
+
