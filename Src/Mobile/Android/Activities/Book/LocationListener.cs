d35f7999ff9c7edeb149cd1ffce3d3867975db23
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-02-12 08:29:17 -0500

    Fixed compilation android

--- Src/Mobile/Android/Activities/Book/LocationListener.cs
+++ Src/Mobile/Android/Activities/Book/LocationListener.cs
@@ -6,6 +6,7 @@ using apcurium.MK.Booking.Mobile.Client.Extensions;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using MK.Common.iOS.Patterns;
 using Object = Java.Lang.Object;
+using System.Linq;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 {
