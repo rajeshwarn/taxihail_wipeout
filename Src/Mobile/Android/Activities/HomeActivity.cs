5a271a0e62d5773b37bb5ff7b2c3bc65096838df
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-02-14 14:10:38 -0500

    MKTAXI-1489 [Android] HomeActivity as a SingleInstance

--- Src/Mobile/Android/Activities/HomeActivity.cs
+++ Src/Mobile/Android/Activities/HomeActivity.cs
@@ -38,10 +38,13 @@ using apcurium.MK.Booking.Mobile.Client.Helpers;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 {
-	[Activity(Label = "Home", Theme = "@style/MainTheme",
-        ScreenOrientation = ScreenOrientation.Portrait, ClearTaskOnLaunch = true,
-        WindowSoftInputMode = SoftInput.AdjustPan,
-        FinishOnTaskLaunch = true)]
+    [Activity(Label = "Home", 
+        Theme = "@style/MainTheme", 
+        ScreenOrientation = ScreenOrientation.Portrait, 
+        ClearTaskOnLaunch = true, 
+        WindowSoftInputMode = SoftInput.AdjustPan, 
+        FinishOnTaskLaunch = true, 
+        LaunchMode = LaunchMode.SingleInstance)]
    
     public class HomeActivity : BaseBindingFragmentActivity<HomeViewModel>, IChangePresentation
     {
