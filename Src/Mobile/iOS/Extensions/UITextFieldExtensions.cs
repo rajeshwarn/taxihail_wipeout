bad1c83b08f8411875df1a2c3d41c3187ca4d667
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-02-20 20:36:10 -0500

    MKTAXI-1473 [iOS] Change "OK" button style to FlatButton

--- Src/Mobile/iOS/Extensions/UITextFieldExtensions.cs
+++ Src/Mobile/iOS/Extensions/UITextFieldExtensions.cs
@@ -56,11 +56,16 @@ namespace apcurium.MK.Booking.Mobile.Client.Extensions
         public static void ShowCloseButtonOnKeyboard(this UITextField text, Action onClosePressed = null)
         {
             text.InputAccessoryView = new UIView { Frame = new RectangleF(0, 0, 320, 44), BackgroundColor = UIColor.FromRGB(251, 253, 253) };
-            var closeButton = new AppBarLabelButton("");
-            text.InputAccessoryView.AddSubview(closeButton);
-
+            var closeButton = new FlatButton();
             closeButton.SetTitle(Localize.GetValue("OkButtonText"), UIControlState.Normal);           
             closeButton.TranslatesAutoresizingMaskIntoConstraints = false;
+            FlatButtonStyle.Silver.ApplyTo(closeButton);
+            text.InputAccessoryView.AddSubview(closeButton);
+
+            closeButton.AddConstraints(new [] {
+                NSLayoutConstraint.Create(closeButton, NSLayoutAttribute.Height, NSLayoutRelation.Equal, null, NSLayoutAttribute.NoAttribute, 1, 36f),
+                NSLayoutConstraint.Create(closeButton, NSLayoutAttribute.Width, NSLayoutRelation.Equal, null, NSLayoutAttribute.NoAttribute, 1, 60f)
+            });
 
             text.InputAccessoryView.AddConstraints(new [] {
                 NSLayoutConstraint.Create(closeButton, NSLayoutAttribute.Trailing, NSLayoutRelation.Equal, text.InputAccessoryView, NSLayoutAttribute.Trailing, 1, -8f),
