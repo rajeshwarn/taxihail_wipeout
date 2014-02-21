e0a9d5284c0edd262223ac50cb75a1818c0e9bfc
Vincent Costel <vincent.costel@apcurium.com>
2014-02-21 09:48:26 -0500

    Loading overlay refactoring

--- Src/Mobile/iOS/Controls/Message/LoadingOverlay.cs
+++ Src/Mobile/iOS/Controls/Message/LoadingOverlay.cs
@@ -9,126 +9,23 @@ using apcurium.MK.Booking.Mobile.Client.Localization;
 
 namespace apcurium.MK.Booking.Mobile.Client.Controls.Message
 {
-    public class LoadingOverlay
+    public static class LoadingOverlay
     {
-        private static UIView _modalBackgroundView;
-        private static UIView _dialogView;
-        private static CircularProgressView _progressView;
-        private static UIImageView _imageView;
-
-        private static float _dialogWidth = UIScreen.MainScreen.Bounds.Width;
-        private static float _dialogHeight = 95;
-        private static bool _isLoading;
-
         private static readonly object _lock = new object();
+        private static int count = 0;
+        private static UIView _modalBackgroundView;
 
         public static void StartAnimatingLoading()
         {
             lock (_lock)
             {
-                UIApplication.SharedApplication.InvokeOnMainThread(() =>
+                count++;
+
+                if (count == 1)
                 {
-                    if (_modalBackgroundView == null)
-                    {
-                        _modalBackgroundView = new UIView();
-                        _modalBackgroundView.BackgroundColor = UIColor.Black.ColorWithAlpha(0.75f);
-
-                        _dialogView = new UIView();
-                        _dialogView.BackgroundColor = UIColor.White;
-
-                        var colorOfCarAndWheel = UIColor.FromRGB(0, 125, 249);
-
-                        if(_imageView == null)
-                        {
-                            _imageView = new UIImageView(UIImage.FromFile("taxi_progress.png")); // TODO MKTAXI-1519 Apply color to image with ImageHelper
-                            _imageView.SizeToFit();
-                            _imageView.Hidden = true;
-                        }
-
-                        _progressView = new CircularProgressView(new RectangleF(0, 0, 67, 67), colorOfCarAndWheel);
-                        _progressView.OnCompleted = () => CloseOverlay();
-                        _progressView.LineWidth = 1.5f;
-                        _progressView.Hidden = true;
-                    }
-
-                    _modalBackgroundView.Frame = new RectangleF(0, 0, UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
-
-                    _dialogView.Frame  = new RectangleF(0, UIScreen.MainScreen.Bounds.Height / 2, UIScreen.MainScreen.Bounds.Width, 0);
-                    _modalBackgroundView.AddSubview(_dialogView);
-
-                    _progressView.SetHorizontalCenter(UIScreen.MainScreen.Bounds.Width / 2);
-                    _progressView.SetVerticalCenter(UIScreen.MainScreen.Bounds.Height / 2);
-                    _modalBackgroundView.AddSubview(_progressView);
-
-                    _imageView.SetHorizontalCenter(UIScreen.MainScreen.Bounds.Width / 2);
-                    _imageView.SetVerticalCenter(UIScreen.MainScreen.Bounds.Height / 2);
-                    _modalBackgroundView.AddSubview(_imageView);
-
+                    _modalBackgroundView = new LoadingOverlayView();
                     UIApplication.SharedApplication.KeyWindow.AddSubview(_modalBackgroundView);
-
-                    _isLoading = true;
-                    Animate();
-                });
-            }
-        }
-
-        private static void Animate()
-        {
-            var options = UIViewAnimationOptions.CurveEaseIn;
-
-            if (_dialogView != null)
-            {
-                UIView.Animate(
-                    0.2, 0, options, 
-                    () =>
-                    {
-                        _dialogView.Frame = new RectangleF(0, (UIScreen.MainScreen.Bounds.Height - _dialogHeight) / 2, _dialogWidth, _dialogHeight);
-                    },
-                    () => 
-                    {
-                        _progressView.Hidden = false;
-                        _imageView.Hidden = false;
-
-                        if(_isLoading)
-                        {
-                            IncreaseProgress();
-                            Animate();
-                        }
-                        else
-                        {
-                            _progressView.Progress = 1f;
-                        }
-                    });
-            }
-        }
-
-        private static void IncreaseProgress()
-        {
-            var currentProgress = _progressView.Progress;
-
-            var slowestSpeed = 0.00001f;
-
-            // multistage progress speed
-            if (currentProgress <= 0.2f)
-            {
-                _progressView.Progress += slowestSpeed * 50f;
-                return;
-            }
-
-            if (currentProgress < 0.8f)
-            {
-                _progressView.Progress += slowestSpeed * 5.5f;
-                return;
-            }
-
-            if (currentProgress > 0.8f)
-            {
-                var nextProgress = currentProgress + slowestSpeed;
-                if (nextProgress >= 1.0f)
-                {
-                    nextProgress = 0.95f;
                 }
-                _progressView.Progress = nextProgress;
             }
         }
 
@@ -136,29 +33,14 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls.Message
         {
             lock (_lock)
             {
-                if (_modalBackgroundView != null)
+                if (count == 1)
                 {
-                    _isLoading = false;
+                    _modalBackgroundView.Dispose();
+                    _modalBackgroundView = null;
                 }
+
+                count = Math.Max(0, --count);
             }
-        }
-
-        private static void CloseOverlay()
-        {
-            UIApplication.SharedApplication.InvokeOnMainThread(() => 
-            {
-                _dialogView.RemoveFromSuperview();
-                _dialogView = null;
-
-                _modalBackgroundView.RemoveFromSuperview();
-                _modalBackgroundView = null;
-
-                _progressView.RemoveFromSuperview();
-                _progressView = null;
-
-                _imageView.RemoveFromSuperview();
-                _imageView = null;
-            });
         }
     }
 }
---
+++ Src/Mobile/iOS/Controls/Message/LoadingOverlayView.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using apcurium.MK.Booking.Mobile.Client.Extensions;
+
+namespace apcurium.MK.Booking.Mobile.Client.Controls.Message
+{
+    public class LoadingOverlayView: UIView
+    {
+        private UIView _dialogView;
+        private UIImageView _imageView;
+        private CircularProgressView _progressView;
+        private static float _dialogWidth = UIScreen.MainScreen.Bounds.Width;
+        private static float _dialogHeight = 95;
+        private static bool _isLoading;
+
+        public LoadingOverlayView()
+        {
+            BackgroundColor = UIColor.Black.ColorWithAlpha(0.75f);
+            Frame = UIScreen.MainScreen.Bounds;
+
+            _dialogView = new UIView();
+            _dialogView.BackgroundColor = UIColor.White;
+
+            var colorOfCarAndWheel = UIColor.FromRGB(0, 125, 249);
+
+            _imageView = new UIImageView(UIImage.FromFile("taxi_progress.png")); // TODO MKTAXI-1519 Apply color to image with ImageHelper
+            _imageView.SizeToFit();
+            _imageView.Hidden = true;
+
+
+            _progressView = new CircularProgressView(new RectangleF(0, 0, 67, 67), colorOfCarAndWheel);
+            _progressView.OnCompleted = () => this.RemoveFromSuperview();
+            _progressView.LineWidth = 1.5f;
+            _progressView.Hidden = true;
+
+
+            _dialogView.Frame  = new RectangleF(0, UIScreen.MainScreen.Bounds.Height / 2, UIScreen.MainScreen.Bounds.Width, 0);
+
+            _progressView.SetHorizontalCenter(UIScreen.MainScreen.Bounds.Width / 2);
+            _progressView.SetVerticalCenter(UIScreen.MainScreen.Bounds.Height / 2);
+
+            _imageView.SetHorizontalCenter(UIScreen.MainScreen.Bounds.Width / 2);
+            _imageView.SetVerticalCenter(UIScreen.MainScreen.Bounds.Height / 2);
+
+            AddSubviews(_dialogView, _progressView, _imageView);
+
+            _isLoading = true;
+            Animate();
+        }
+
+        private void Animate()
+        {
+            var options = UIViewAnimationOptions.CurveEaseIn;
+
+            UIView.Animate(
+                0.2, 0, options, 
+                () =>
+            {
+                _dialogView.Frame = new RectangleF(0, (UIScreen.MainScreen.Bounds.Height - _dialogHeight) / 2, _dialogWidth, _dialogHeight);
+            },
+                () => 
+            {
+                _progressView.Hidden = false;
+                _imageView.Hidden = false;
+
+                if(_isLoading)
+                {
+                    IncreaseProgress();
+                    Animate();
+                }
+                else
+                {
+                    _progressView.Progress = 1f;
+                }
+            });
+        }
+        private void IncreaseProgress()
+        {
+            var currentProgress = _progressView.Progress;
+
+            var slowestSpeed = 0.00001f;
+
+            // multistage progress speed
+            if (currentProgress <= 0.2f)
+            {
+                _progressView.Progress += slowestSpeed * 50f;
+                return;
+            }
+
+            if (currentProgress < 0.8f)
+            {
+                _progressView.Progress += slowestSpeed * 5.5f;
+                return;
+            }
+
+            if (currentProgress > 0.8f)
+            {
+                var nextProgress = currentProgress + slowestSpeed;
+                if (nextProgress >= 1.0f)
+                {
+                    nextProgress = 0.95f;
+                }
+                _progressView.Progress = nextProgress;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                _isLoading = false;
+            }
+        }
+    }
+
+
+}
+
