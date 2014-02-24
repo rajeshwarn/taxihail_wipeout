3491a6b3813855a57d7c62d5902fc27b2021ec8d
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-02-24 09:59:48 -0500

    Forgot a revert on a file, fix build

--- Src/Mobile/Android/Activities/Account/LoginActivity.cs
+++ Src/Mobile/Android/Activities/Account/LoginActivity.cs
@@ -98,7 +98,7 @@ namespace apcurium.MK.Booking.Mobile.Client.Activities.Account
 
         public bool ShouldUseClipboardManager()
         {
-            return DeviceHelper.IsAndroid23();
+            return (int) Build.VERSION.SdkInt <= 8;
         }
 
         public void ApplyKeyboardEnabler(EditText view)
