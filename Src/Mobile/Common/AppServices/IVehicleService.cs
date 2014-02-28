5112d3bbf19838edd5b28940b531237b0e874828
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-02-28 13:57:36 -0500

    MKTAXI-1606 Get available vehicles using pickup address

--- Src/Mobile/Common/AppServices/IVehicleService.cs
+++ Src/Mobile/Common/AppServices/IVehicleService.cs
@@ -7,8 +7,6 @@ namespace apcurium.MK.Booking.Mobile.AppServices
 {
 	public interface IVehicleService
 	{
-		Task<AvailableVehicle[]> GetAvailableVehiclesAsync(double latitude, double longitude);
-
 		void Start();
 		void Stop();
 		IObservable<AvailableVehicle[]> GetAndObserveAvailableVehicles();
