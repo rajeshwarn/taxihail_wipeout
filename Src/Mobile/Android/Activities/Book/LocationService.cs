8cdea9751254642865e53588b929b71050a804be
Alex Proteau <alex.proteau@apcurium.com>
2014-03-04 09:48:08 -0500

    MKTAXI-1621 : Gps postion is not being updated properly.

--- Src/Mobile/Android/Activities/Book/LocationService.cs
+++ Src/Mobile/Android/Activities/Book/LocationService.cs
@@ -6,21 +6,23 @@ using Android.OS;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using TinyIoC;
 using apcurium.MK.Common.Diagnostic;
+using Cirrious.CrossCore.Droid;
+using apcurium.MK.Booking.Mobile.Client.Extensions;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 {
     public class LocationService : BaseLocationService
     {
-        private readonly LocationListener _locationListener;
+        private readonly LocationListenerManager _locationListeners;
         private readonly LocationManager _locationManager;
         private bool _isStarted;
 
         public LocationService()
         {
             _locationManager = (LocationManager) Application.Context.GetSystemService(Context.LocationService);
-            _locationListener = new LocationListener();
+            _locationListeners = new LocationListenerManager();
 
-            Positions = _locationListener;
+            Positions = _locationListeners;
         }
 
         public bool IsNetworkProviderEnabled
@@ -30,7 +32,13 @@ namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 
         public bool IsGpsProviderEnabled
         {
-            get { return _locationManager.IsProviderEnabled(LocationManager.GpsProvider); }
+            get { 
+                //_locationManager.IsProviderEnabled not working for gps
+                //http://stackoverflow.com/questions/10117587/gps-is-not-enabled-but-isproviderenabled-is-returning-true
+                var context = TinyIoCContainer.Current.Resolve<IMvxAndroidGlobals> ().ApplicationContext;
+                var gps = Android.Provider.Settings.Secure.GetString (context.ContentResolver,Android.Provider.Settings.Secure.LocationProvidersAllowed).Contains( "gps" ); 
+                return gps;
+            }
         }
 
         public override bool IsLocationServicesEnabled
@@ -40,20 +48,19 @@ namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
 
         public override Position BestPosition
         {
-            get { return _locationListener.BestPosition; }
+            get { return _locationListeners.BestPosition; }
         }
 
         public override Position LastKnownPosition
         {
-            get { return _locationListener.LastKnownPosition; }
+            get { return _locationListeners.LastKnownPosition; }
         }
 
         public bool IsLocationServiceEnabled
         {
             get
-            {
-                return _locationManager.IsProviderEnabled(LocationManager.NetworkProvider)
-                       || _locationManager.IsProviderEnabled(LocationManager.GpsProvider);
+            {            
+                return IsNetworkProviderEnabled || IsGpsProviderEnabled;
             }
         }
 
@@ -67,34 +74,59 @@ namespace apcurium.MK.Booking.Mobile.Client.Activities.Book
             if (IsStarted)
             {
                 _isStarted = false;
-                _locationManager.RemoveUpdates(_locationListener);
+                _locationManager.RemoveUpdates(_locationListeners.GpsListener);
+                _locationManager.RemoveUpdates(_locationListeners.NetworkListener);
             }
         }
 
         public override void Start()
         {
+
             if (IsStarted)
             {
                 return;
             }
 
+
+
             if (!IsLocationServiceEnabled)
-            {
+            { 
                 // TODO: MKTAXI-1111 - Handle Location Service Disabled
             }
 
+
+
+      
             if (IsNetworkProviderEnabled)
             {
-                _locationManager.RequestLocationUpdates(LocationManager.NetworkProvider, 0, 0, _locationListener,
+                _locationManager.RequestLocationUpdates(LocationManager.NetworkProvider, 0, 0, _locationListeners.NetworkListener,
                     Looper.MainLooper);
+
+                _locationListeners.OnLocationChanged(_locationManager.GetLastKnownLocation(LocationManager.NetworkProvider));
             }
 
             if (IsGpsProviderEnabled)
             {
-                _locationManager.RequestLocationUpdates(LocationManager.GpsProvider, 0, 0, _locationListener,
+                _locationManager.RequestLocationUpdates(LocationManager.GpsProvider, 0, 0, _locationListeners.GpsListener,
                     Looper.MainLooper);
+                _locationListeners.OnLocationChanged(_locationManager.GetLastKnownLocation(LocationManager.GpsProvider));
             }
             _isStarted = true;
+        }
+
+        private Position ToPosition( Location location )
+        {
+            if (location != null)
+            {
+                return new Position()
+                {
+                    Time = location.Time.ToDateTime(),
+                    Error = location.Accuracy,
+                    Latitude = location.Latitude,
+                    Longitude = location.Longitude
+                };
+            }
+            return null;
         }
     }
 }
