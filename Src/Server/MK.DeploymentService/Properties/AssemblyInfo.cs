928ea0a638658123782d57e6ff433a7a976f7e85
Alex Proteau <alex.proteau@apcurium.com>
2014-03-04 14:27:12 -0500

    Version bump 1.5.8

--- Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
+++ Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.7")]
-[assembly: AssemblyVersion("1.5.7")]
-[assembly: AssemblyFileVersion("1.5.7")]
+// [assembly: AssemblyVersion("1.5.8")]
+[assembly: AssemblyVersion("1.5.8")]
+[assembly: AssemblyFileVersion("1.5.8")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
