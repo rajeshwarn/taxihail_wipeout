ec9f38b019217429d18fa0cd069b12f216d7132b
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-03-10 15:55:07 -0400

    MKTAXI-1657 Check for latest Terms and Conditions when Home appears

--- Src/Mobile/Common/AppServices/ITermsAndConditionsService.cs
+++ Src/Mobile/Common/AppServices/ITermsAndConditionsService.cs
@@ -1,11 +1,15 @@
 using System.Threading.Tasks;
 using apcurium.MK.Booking.Api.Contract.Resources;
+using System;
+using System.Collections.Generic;
 
 namespace apcurium.MK.Booking.Mobile.AppServices
 {
 	public interface ITermsAndConditionsService
 	{
         Task<TermsAndConditions> GetTerms();
+
+		Task CheckIfNeedsToShowTerms (Action<object, Action<bool>> actionToDoIfTrue);
 	}
 }
 
--- Src/Mobile/Common/ViewModels/HomeViewModel.cs
+++ Src/Mobile/Common/ViewModels/HomeViewModel.cs
@@ -61,7 +61,10 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 		public override void OnViewStarted(bool firstTime)
 		{
 			base.OnViewStarted(firstTime);
+
 			_locationService.Start();
+			CheckTermsAsync();
+
 			if (firstTime)
 			{
 				Map = AddChild<MapViewModel>();
@@ -75,8 +78,6 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 				BottomBar.CancelEdit = OrderEdit.Cancel;
 
 				this.Services().ApplicationInfo.CheckVersionAsync();
-
-				CheckTermsAsync();
 
 				_tutorialService.DisplayTutorialToNewUser();
 				_pushNotificationService.RegisterDeviceForPushNotifications(force: true);
@@ -99,31 +100,7 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 
 		public async void CheckTermsAsync()
 		{
-			if (!Settings.ShowTermsAndConditions) 
-			{
-				return;
-			}
-
-			var response = await _termsService.GetTerms();
-			var termsAckKey = string.Format("TermsAck{0}", _accountService.CurrentAccount.Email);
-			var termsAcknowledged = this.Services().Cache.Get<string>(termsAckKey);
-
-			if (response.Updated || !(termsAcknowledged == "yes"))
-			{				 
-				this.Services().Cache.Clear(termsAckKey);
-				ShowSubViewModel<UpdatedTermsAndConditionsViewModel, bool>(new 
-					{														
-						content = response.Content
-					}.ToStringDictionary(), 
-					async acknowledged =>
-					{
-						this.Services().Cache.Set<string>(termsAckKey, acknowledged ? "yes" : "no");
-						if (!acknowledged)
-						{
-							_accountService.SignOut();
-						}
-					});
-			}
+			await _termsService.CheckIfNeedsToShowTerms ((content, actionOnResult) => ShowSubViewModel<UpdatedTermsAndConditionsViewModel, bool> (content, actionOnResult));
 		}
 
 		public override void OnViewStopped()
