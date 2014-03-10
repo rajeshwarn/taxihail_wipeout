cac50750f93b144fb07590f4a857948130481749
Vincent Costel <vincent.costel@apcurium.com>
2014-03-10 15:02:35 -0400

    Moved code in separate files

---
+++ Src/Mobile/Common/Extensions/EventHandlerExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System
+{
+    public static class EventHandlerExtensions
+    {
+        public static void Raise<T>(this EventHandler<T> eh, object sender, T args) where T:EventArgs
+        {
+            if (eh != null)
+            {
+                eh.Invoke(sender, args);
+            }
+        }
+
+        public static void Raise(this EventHandler eh, object sender, EventArgs args)
+        {
+            if (eh != null)
+            {
+                eh.Invoke(sender, args);
+            }
+        }
+    }
+}
+
--- Src/Mobile/Common/ViewModels/IRequestPresentationState.cs
+++ Src/Mobile/Common/ViewModels/IRequestPresentationState.cs
@@ -7,52 +7,5 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 	{
 		event EventHandler<T> PresentationStateRequested;
 	}
-
-	public static class IRequestPresentationStateExtensions
-	{
-        public static void Raise<T>(this EventHandler<T> eh, object sender, T args) where T:EventArgs
-		{
-            if (eh != null)
-			{
-                eh.Invoke(sender, args);
-			}
-		}
-
-        public static void Raise(this EventHandler eh, object sender, EventArgs args)
-        {
-            if (eh != null)
-            {
-                eh.Invoke(sender, args);
-            }
-        }
-
-	}
-
-	public class HomeViewModelStateRequestedEventArgs: EventArgs
-	{
-        public HomeViewModelStateRequestedEventArgs(HomeViewModelState state, bool isNewOrder)
-			:this(state)
-		{
-			this.IsNewOrder = isNewOrder;
-			
-		}
-
-		public HomeViewModelStateRequestedEventArgs(HomeViewModelState state)
-		{
-			this.State = state;
-		}
-
-		public bool IsNewOrder
-		{
-			get;
-			private set;
-		}
-
-		public HomeViewModelState State
-		{
-			get;
-			private set;
-		}
-	}
 }
 
