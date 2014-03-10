c954182ca201b79fabb66ca49475817a25011223
Vincent Costel <vincent.costel@apcurium.com>
2014-03-10 14:54:42 -0400

    Fixed bug in HardwareInfo class. Removed requirement to be called on UI thread

--- Src/Mobile/iOS/Helper/DeviceHardware.cs
+++ Src/Mobile/iOS/Helper/DeviceHardware.cs
@@ -7,48 +7,46 @@ namespace apcurium.MK.Booking.Mobile.Client.Helper
 {
     public static class HardwareInfo
     {
-        public const string HardwareProperty = "hw.machine";
+        private const string HardwareProperty = "hw.machine";
 
         [DllImport(MonoTouch.Constants.SystemLibrary)]
         static internal extern int sysctlbyname([MarshalAs(UnmanagedType.LPStr)] string property, IntPtr output, IntPtr oldLen, IntPtr newp, uint newlen);
 
-        public static string Version
+        static HardwareInfo()
         {
-            get
+            Version = GetVersion();
+        }
+
+        public static readonly string Version;
+
+        private static string GetVersion()
+        {
+            var pLen = Marshal.AllocHGlobal(sizeof(int));
+            sysctlbyname(HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
+
+            var length = Marshal.ReadInt32(pLen);
+
+            if (length == 0)
             {
-                var pLen = Marshal.AllocHGlobal(sizeof(int));
-                sysctlbyname(HardwareProperty, IntPtr.Zero, pLen, IntPtr.Zero, 0);
-
-                var length = Marshal.ReadInt32(pLen);
-
-                if (length == 0)
-                {
-                    Marshal.FreeHGlobal(pLen);
-
-                    return "Unknown";
-                }
-
-                var pStr = Marshal.AllocHGlobal(length);
-                sysctlbyname(HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
-
-                var hardwareStr = Marshal.PtrToStringAnsi(pStr);
-
-                if (hardwareStr == "i386" || hardwareStr == "x86_64")
-                {
-                    if (UIDevice.CurrentDevice.Model.Contains("iPhone"))
-                        return UIScreen.MainScreen.Bounds.Height * UIScreen.MainScreen.Scale == 960 
-                            || UIScreen.MainScreen.Bounds.Width * UIScreen.MainScreen.Scale == 960 
-                            ? "iPhone4Simulator"
-                            : "iPhoneSimulator";
-                    else
-                        return "iPadSimulator";
-                }
-
                 Marshal.FreeHGlobal(pLen);
-                Marshal.FreeHGlobal(pStr);
 
-                return hardwareStr;
+                return "Unknown";
             }
+
+            var pStr = Marshal.AllocHGlobal(length);
+            sysctlbyname(HardwareProperty, pStr, pLen, IntPtr.Zero, 0);
+
+            var hardwareStr = Marshal.PtrToStringAnsi(pStr);
+
+            if (hardwareStr == "i386" || hardwareStr == "x86_64")
+            {
+                return "Simulator";
+            }
+
+            Marshal.FreeHGlobal(pLen);
+            Marshal.FreeHGlobal(pStr);
+
+            return hardwareStr;
         }
     }
 }
