a5f1dd2b88a49b091bb5b6aba4fde3b7aca1f055
Leon Pelletier <leon.pelletier@gmail.com>
2014-03-11 09:18:52 -0400

    MKTAXI-1662 - CPU/Mem Profiling method - Removed from Setup.cs, added to the platform helper

--- Src/Mobile/Android/Setup.cs
+++ Src/Mobile/Android/Setup.cs
@@ -23,12 +23,6 @@ using apcurium.MK.Booking.Mobile.Client.Services.Social;
 using Cirrious.MvvmCross.ViewModels;
 using apcurium.MK.Booking.Mobile.IoC;
 using Cirrious.CrossCore.Droid.Platform;
-using Java.IO;
-using System.Threading;
-using System.Collections.Concurrent;
-using Cirrious.CrossCore;
-using System.IO;
-using System.Linq;
 
 namespace apcurium.MK.Booking.Mobile.Client
 {
@@ -74,7 +68,6 @@ namespace apcurium.MK.Booking.Mobile.Client
 
             _container.Register<IAppSettings>(new AppSettingsService(_container.Resolve<ICacheService>(), _container.Resolve<ILogger>()));
 
-			CpuBenchmark ();
 			InitializeSocialNetwork();
         }
 
@@ -125,77 +118,5 @@ namespace apcurium.MK.Booking.Mobile.Client
         {
             return new PhonePresenter();
         }
-
-
-
-		// Debug CPU
-		private void CpuBenchmark()
-		{
-			System.Timers.Timer timer = new System.Timers.Timer ();
-			timer.Interval = 250;
-			timer.AutoReset = true;
-			timer.Elapsed += (object sender, System.Timers.ElapsedEventArgs e) => {
-				readUsage();
-			};
-			timer.Start ();
-		}
-
-		private float[] cpuStack = new float[] { 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f };
-		private int cpuStackPointer = 0;
-
-		private void readUsage() {
-			try {
-				var topActivity = TinyIoC.TinyIoCContainer.Current.Resolve<IMvxAndroidCurrentTopActivity> (); 
-				RandomAccessFile reader = new RandomAccessFile ("/proc/stat", "r");
-				string load = reader.ReadLine ();
-
-				string[] toks = load.Split (' ');
-
-				long idle1 = long.Parse (toks [5]);
-				long cpu1 = long.Parse (toks [2]) + long.Parse (toks [3]) + long.Parse (toks [4])
-				            + long.Parse (toks [6]) + long.Parse (toks [7]) + long.Parse (toks [8]);
-
-				try {
-					Thread.Sleep (360);
-				} catch (Exception) {
-				}
-
-				reader.Seek (0);
-				load = reader.ReadLine ();
-				reader.Close ();
-
-				toks = load.Split (' ');
-
-				float idle2 = float.Parse (toks [5]);
-				float cpu2 = float.Parse (toks [2]) + float.Parse (toks [3]) + float.Parse (toks [4])
-				             + float.Parse (toks [6]) + float.Parse (toks [7]) + float.Parse (toks [8]);
-
-				float cpuValue = ((cpu2 - cpu1) * 100f / ((cpu2 + idle2) - (cpu1 + idle1)));
-				cpuStack [cpuStackPointer++] = cpuValue;
-
-				if (cpuStackPointer == 10) {
-					cpuStackPointer = 0;
-				}
-				var averageTxt = ((int)cpuStack.Take(10).Average()).ToString ().PadLeft(2,'0');
-
-				TimeSpan t = (DateTime.UtcNow - new DateTime(1970, 1, 1));
-
-				//Get VM Heap Size by calling:
-				var heapSize = (Java.Lang.Runtime.GetRuntime().TotalMemory() / 1000).ToString().PadLeft(6,'0');
-				//Get Allocated VM Memory by calling:
-				var allocated = ((Java.Lang.Runtime.GetRuntime().TotalMemory() - Java.Lang.Runtime.GetRuntime().FreeMemory()) / 1000).ToString().PadLeft(6,'0');
-				// Free
-				var free = (Java.Lang.Runtime.GetRuntime().FreeMemory() / 1000).ToString().PadLeft(6,'0');
-				//Get VM Heap Size Limit by calling:
-				var heapSizeLimit = (Java.Lang.Runtime.GetRuntime().MaxMemory() / 1000).ToString().PadLeft(6,'0');
-				//
-				var mem = heapSize+" " + allocated + " " + free + " " + heapSizeLimit;
-				System.Console.WriteLine ("-|-cpu " + (((long)t.TotalSeconds * 1000L) + (long)DateTime.Now.Millisecond).ToString() + " " + averageTxt + " " + mem + " " + topActivity.Activity.Title);
-
-
-			} catch (System.IO.IOException) {
-				//ex.printStackTrace();
-			}
-		}
 	}
 }
