01827139c537e3860649770f28e478389acbafe4
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-03-12 09:43:12 -0400

    MKTAXI-1546 Done

--- Src/Mobile/Android/Extensions/ViewServicesExtensions.cs
+++ Src/Mobile/Android/Extensions/ViewServicesExtensions.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Android.App;
+using Android.Widget;
 using apcurium.MK.Booking.Mobile.Extensions;
-using Cirrious.MvvmCross.Views;
 
 namespace apcurium.MK.Booking.Mobile.Client
 {
 	public static class ViewServicesExtensions
 	{
-		public static ServicesExtensionPoint Services(this IMvxView activity)
+		public static ServicesExtensionPoint Services(this Activity activity)
+		{
+			return new ServicesExtensionPoint();
+		}
+			
+		public static ServicesExtensionPoint Services(this FrameLayout view)
 		{
 			return new ServicesExtensionPoint();
 		}
