537a7bb637bb92e3c3a5c126a71d8fd71b823d42
Alex Proteau <alex.proteau@apcurium.com>
2014-03-18 12:58:43 -0400

    Version  bump to 1.5.13

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.12")]
-[assembly: AssemblyVersion("1.5.12")]
-[assembly: AssemblyFileVersion("1.5.12")]
+// [assembly: AssemblyVersion("1.5.13")]
+[assembly: AssemblyVersion("1.5.13")]
+[assembly: AssemblyFileVersion("1.5.13")]
 
