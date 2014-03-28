08f59ad7c578245235aead5875cf5ccec1b4df21
Alex Proteau <alex.proteau@apcurium.com>
2014-03-28 09:30:39 -0400

    Version bump to 1.5.14

--- Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
+++ Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.13")]
-[assembly: AssemblyVersion("1.5.13")]
-[assembly: AssemblyFileVersion("1.5.13")]
+// [assembly: AssemblyVersion("1.5.14")]
+[assembly: AssemblyVersion("1.5.14")]
+[assembly: AssemblyFileVersion("1.5.14")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
