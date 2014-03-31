0f4326b3c4d861da066392f312eea66dd6895a59
Alex Proteau <alex.proteau@apcurium.com>
2014-03-31 13:26:32 -0400

    Fix issue with fare estimate

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.14")]
-[assembly: AssemblyVersion("1.5.14")]
-[assembly: AssemblyFileVersion("1.5.14")]
+// [assembly: AssemblyVersion("1.5.15")]
+[assembly: AssemblyVersion("1.5.15")]
+[assembly: AssemblyFileVersion("1.5.15")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.14")]
-[assembly: AssemblyVersion("1.5.14")]
-[assembly: AssemblyFileVersion("1.5.14")]
+// [assembly: AssemblyVersion("1.5.15")]
+[assembly: AssemblyVersion("1.5.15")]
+[assembly: AssemblyFileVersion("1.5.15")]
 
