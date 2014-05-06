33e437fc4778e7870ebdb94989e839cf99fe85e6
Alex Proteau <alex.proteau@apcurium.com>
2014-05-06 15:28:03 -0400

    Fix issue with tutorial

--- Src/Mobile/Common/AppServices/Impl/TutorialService.cs
+++ Src/Mobile/Common/AppServices/Impl/TutorialService.cs
@@ -60,7 +60,7 @@ namespace apcurium.MK.Booking.Mobile.AppServices.Impl
             
             foreach (var name in typeof(TutorialService).Assembly.GetManifestResourceNames()) 
 			{ 
-				if (name.ToLower ().EndsWith (string.Format(".tutorial.{0}.json", TinyIoCContainer.Current.Resolve<ILocalization>()["LanguageCode"])))
+				if (name.ToLower ().EndsWith (string.Format(".tutorial-{0}.json", TinyIoCContainer.Current.Resolve<ILocalization>()["LanguageCode"])))
 				{
                     resourceName = name;
                     break;
