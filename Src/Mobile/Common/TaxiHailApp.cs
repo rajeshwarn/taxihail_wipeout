9670086e03a3fa4c8cbae7974401c1174f0cd87a
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-05-16 16:21:44 -0400

    Register AccountPaymentService

--- Src/Mobile/Common/TaxiHailApp.cs
+++ Src/Mobile/Common/TaxiHailApp.cs
@@ -73,6 +73,7 @@ namespace apcurium.MK.Booking.Mobile
 			_container.Register<IVehicleService, VehicleService>();
 			_container.Register<ITutorialService, TutorialService>();
 			_container.Register<ITermsAndConditionsService, TermsAndConditionsService>();
+			_container.Register<IAccountPaymentService, AccountPaymentService>();
 
 			_container.Register<IGeolocService, GeolocService>();
 			_container.Register<IGoogleService, GoogleService>();
