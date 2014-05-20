8e4717bbe6fe6acb24a58508487d6c565e88fa9f
Alex Proteau <alex.proteau@apcurium.com>
2014-05-20 15:02:02 -0400

    Update google analytics references to use component Remove reference to xamarin mobile

--- Src/Mobile/iOS/PlatformIntegration/GoogleAnalyticsService.cs
+++ Src/Mobile/iOS/PlatformIntegration/GoogleAnalyticsService.cs
@@ -2,47 +2,49 @@ using System;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using apcurium.MK.Common.Configuration;
 using GoogleAnalytics;
+using GoogleAnalytics.iOS;
 
 namespace apcurium.MK.Booking.Mobile.Client.PlatformIntegration
 {
-    public class GoogleAnalyticsService: IAnalyticsService
-    {
+	public class GoogleAnalyticsService: IAnalyticsService
+	{
 
-        GAITracker Tracker {
-            get;
-            set;
-        }
+		IGAITracker Tracker {
+			get;
+			set;
+		}
 
-        public GoogleAnalyticsService (IAppSettings settings,IPackageInfo packageInfo)
-        {
-            GAI.SharedInstance.TrackUncaughtExceptions = true;
-            GAI.SharedInstance.DispatchInterval = 20;
-            GAI.SharedInstance.Debug = true;// Optional: set debug to YES for extra debugging information.
-
-            Tracker = GAI.SharedInstance.GetTracker (  "UA-44714416-1" );
-            Tracker.AppName =  settings.Data.ApplicationName.Replace( ' ' , '_' );
-            Tracker.AppVersion = packageInfo.Version;
+		public GoogleAnalyticsService (IAppSettings settings,IPackageInfo packageInfo)
+		{
+			GAI.SharedInstance.TrackUncaughtExceptions = true;
+			GAI.SharedInstance.DispatchInterval = 20;
 
 
-        }
+			Tracker = GAI.SharedInstance.GetTracker (  "UA-44714416-1" );
+			Tracker.Set( GAIConstants.AppName,   settings.Data.ApplicationName.Replace( ' ' , '_' ));
+			Tracker.Set(GAIConstants.AppVersion, packageInfo.Version);
 
-        public void LogViewModel (string viewModelName){
-            Tracker.TrackView (viewModelName);
-        }
 
-        public void LogCommand(string commandName, string parameter)
-        {
-            Tracker.TrackEvent ("Interaction","Command Issued",commandName+"("+parameter +")", 0);
-        }
+		}
 
-        public void LogNavigation(string source, string destination)
-        {
-            Tracker.TrackEvent ("Interaction","NavigationRequested",source +" to "+ destination, 0);
-        }
-        public void LogException(string className, string methodName, Exception e, bool isFatal = false)
-        {
-            Tracker.TrackException (isFatal,className+":"+methodName+": "+e.Message);
-        }
-    }
+		public void LogViewModel (string viewModelName){
+			var appView = GAIDictionaryBuilder.CreateAppView ();
+			appView.Set (viewModelName, GAIConstants.ScreenName);
+			Tracker.Send(  appView.Build() );
+		}
+
+		public void LogCommand(string commandName, string parameter)
+		{
+		}
+
+		public void LogNavigation(string source, string destination)
+		{
+
+		}
+		public void LogException(string className, string methodName, Exception e, bool isFatal = false)
+		{
+
+		}
+	}
 }
 
