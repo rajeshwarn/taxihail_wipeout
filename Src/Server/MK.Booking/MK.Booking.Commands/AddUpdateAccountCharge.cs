6be014b9ae983eb29baafa8967b87c72d8eeb03b
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-05-20 14:18:27 -0400

    MKTAXI-1706 add command, events and handler + DAO

---
+++ Src/Server/MK.Booking/MK.Booking.Commands/AddUpdateAccountCharge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using apcurium.MK.Common.Entity;
+using Infrastructure.Messaging;
+
+namespace apcurium.MK.Booking.Commands
+{
+    public class AddUpdateAccountCharge : ICommand
+    {
+        public AddUpdateAccountCharge()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public Guid AccountChargeId { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public AccountChargeQuestion[] Questions { get; set; }
+        public Guid CompanyId { get; set; }
+    }
+}
