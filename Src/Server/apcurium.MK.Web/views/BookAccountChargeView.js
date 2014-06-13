8803dd06a4f4f5f5ad11c9bb3f8f5f9797f63f8c
Matthieu <matthieu@live.com>
2014-06-13 09:08:16 -0400

    add validation of the answers

--- Src/Server/apcurium.MK.Web/views/BookAccountChargeView.js
+++ Src/Server/apcurium.MK.Web/views/BookAccountChargeView.js
@@ -1,11 +1,17 @@
 ﻿(function () {
 
-    TaxiHail.BookAccountChargeView = TaxiHail.TemplatedView.extend({
+    var View = TaxiHail.BookAccountChargeView = TaxiHail.TemplatedView.extend({
 
         tagName: 'form',
         events: {
             'click [data-action=loadPrompts]': 'loadPrompts',
+            'click [data-action=cancel]': 'cancel',
         },
+
+        initialize: function() {
+            _.bindAll(this, 'book', 'showErrors');
+        },
+
         render: function () {
             var html = this.renderTemplate(this.model.toJSON());
             this.$el.html(html);
@@ -27,8 +33,6 @@
             this.model.fetchQuestions(accountNumber)
                 .done(_.bind(function (data) {
 
-                    //this.refreshValidation(data.questions);
-
                     this.$('#btloadPrompts').button('reset');
 
                     var $ul = this.$('ul');
@@ -45,6 +49,7 @@
 
                     $ul.first().append(items);
 
+                    this.refreshValidationRules(data.questions);
 
                 }, this))
                 .fail(_.bind(function (response) {
@@ -68,42 +73,76 @@
                 this.refreshPrompts(accountNumber);
             }
         },
-        refreshValidation: function(questions)
-        {
-            
+        refreshValidationRules: function (questions) {
+
+            var questionRules = new Object();
+            var questionMessages = new Object();
+
+            for (var i = 0; i < questions.length; i++) {
+
+                var question = questions[i];
+                if (question.isEnabled &&
+                    (question.isRequired || question.maxLength > 0)) {
+                    var name = 'answer' + i;
+
+                    var rule = new Object();
+                    var message = new Object();
+                    if (question.isRequired) {
+                        rule.required = true;
+                        message.required = 'the answer is required' ;
+                    }
+                    if (question.maxLength > 0) {
+                        rule.maxlength = question.maxLength;
+                    }
+
+                    questionRules[name] = rule;
+                    questionMessages[name] = message;
+                }
+
+            }
+
             this.validate({
-                rules: {
-                    'settings.name': "required",
-                    'settings.phone': {
-                        tenOrMoreDigits: true,
-                        minlength: 10
-                    },
-                    'settings.passengers': {
-                        required: true,
-                        number: true
-                    },
-                    'settings.largeBags': {
-                        number: true
-                    }
-                },
-                messages: {
-                    'settings.name': {
-                        required: TaxiHail.localize('error.NameRequired')
-                    },
-                    'settings.phone': {
-                        required: TaxiHail.localize('error.PhoneRequired'),
-                        tenOrMoreDigits: TaxiHail.localize('error.PhoneBadFormat')
-                    },
-                    'settings.passengers': {
-                        required: TaxiHail.localize('error.PassengersRequired'),
-                        number: TaxiHail.localize('error.NotANumber')
-                    },
-                    'settings.largeBags': {
-                        number: TaxiHail.localize('error.NotANumber')
-                    }
-                },
+                rules: questionRules,
+                messages: questionMessages,
                 submitHandler: this.book
             });
+        },
+
+        book: function (form) {
+
+            this.model.save({}, {
+                success: TaxiHail.postpone(function (model) {
+                    // Wait for order to be created before redirecting to status
+                    TaxiHail.app.navigate('status/' + model.id, { trigger: true, replace: true /* Prevent user from coming back to this screen */ });
+                }, this),
+                error: this.showErrors
+            });
+        },
+
+        cancel: function (e) {
+            e.preventDefault();
+            this.model.destroyLocal();
+            TaxiHail.app.navigate('', { trigger: true, replace: true /* Prevent user from coming back to this screen */ });
+        },
+
+        showErrors: function (model, result) {
+            this.$(':submit').button('reset');
+
+            if (result.responseText) {
+                result = JSON.parse(result.responseText).responseStatus;
+            }
+
+            var $alert = $('<div class="alert alert-error" />');
+            if (result.errorCode == "CreateOrder_RuleDisable") {
+                $alert.append($('<div />').text(result.message));
+            }
+            else if (result.statusText) {
+                $alert.append($('<div />').text(this.localize(result.statusText)));
+            }
+            _.each(result.errors, function (error) {
+                $alert.append($('<div />').text(this.localize(error.statusText)));
+            }, this);
+            this.$('.errors').html($alert);
         },
     });
 
