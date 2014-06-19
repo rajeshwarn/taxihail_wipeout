ecf834538172c922eb45db03066bb0004022258f
Alex Proteau <alex.proteau@apcurium.com>
2014-06-19 15:44:51 -0400

    MKTAXI-1759 : Ensure that the order status service is running only on one iis server MKTAXI-1760 : Fix in a previous commit but tag here to include in release notes

--- Src/Server/MK.Booking.Api/Jobs/UpdateOrderStatusJob.cs
+++ Src/Server/MK.Booking.Api/Jobs/UpdateOrderStatusJob.cs
@@ -9,7 +9,11 @@ using apcurium.MK.Common;
 using System.Threading;
 using log4net;
 using apcurium.MK.Common.Entity;
-
+using apcurium.MK.Common.Extensions;
+using apcurium.MK.Booking.ReadModel;
+using apcurium.MK.Common.Configuration;
+using System.Reactive.Linq;
+using System.Diagnostics;
 #endregion
 
 namespace apcurium.MK.Booking.Api.Jobs
@@ -19,10 +23,15 @@ namespace apcurium.MK.Booking.Api.Jobs
         private readonly IOrderDao _orderDao;
         private readonly IAccountDao _accountDao;
         private readonly IBookingWebServiceClient _bookingWebServiceClient;
+        private readonly IOrderStatusUpdateDao _orderStatusUpdateDao;
         private readonly OrderStatusUpdater _orderStatusUpdater;
+        private readonly IConfigurationManager _configManager;
 
-        public UpdateOrderStatusJob(IOrderDao orderDao, IAccountDao accountDao, IBookingWebServiceClient bookingWebServiceClient, OrderStatusUpdater orderStatusUpdater)
+
+        public UpdateOrderStatusJob(IOrderDao orderDao, IAccountDao accountDao, IBookingWebServiceClient bookingWebServiceClient, IOrderStatusUpdateDao orderStatusUpdateDao, IConfigurationManager configManager, OrderStatusUpdater orderStatusUpdater)
         {
+            _configManager = configManager;
+            _orderStatusUpdateDao = orderStatusUpdateDao;
             _accountDao = accountDao;
             _orderDao = orderDao;
             _bookingWebServiceClient = bookingWebServiceClient;
@@ -32,40 +41,80 @@ namespace apcurium.MK.Booking.Api.Jobs
         public void CheckStatus(Guid orderId)
         {
 
-            var order = _orderDao.FindById(orderId);
+            Func<OrderDetail> getOrderDetails = () =>
+                {
+                    var o = _orderDao.FindById(orderId);
+                    if (o == null)
+                    {
+                        throw new Exception();
+                    }
+                    return o;
+                };
 
-            while (order == null)
+            OrderDetail order = null;
+
+            try
             {
-                order = _orderDao.FindById(orderId);
-                Thread.Sleep(100);
+                order = getOrderDetails.Retry(TimeSpan.FromMilliseconds(300), 7);
+                if (order != null)
+                {
+                    var orderStatus = _orderDao.FindOrderStatusById(orderId);
+                    var account = _accountDao.FindById(order.AccountId);
+                    var status = _bookingWebServiceClient.GetOrderStatus(order.IBSOrderId.Value, account.IBSAccountId, order.Settings.Phone);
+
+                    var ibsStatus = new IBSOrderInformation
+                    {
+                        Status = status.Status,
+                        IBSOrderId = order.IBSOrderId.Value,
+                    };
+
+
+                    _orderStatusUpdater.Update(ibsStatus, orderStatus);
+                }
+            }
+            catch
+            {
+
             }
 
-            var orderStatus = _orderDao.FindOrderStatusById(orderId);
-            var account = _accountDao.FindById(order.AccountId);
-            var status = _bookingWebServiceClient.GetOrderStatus(order.IBSOrderId.Value, account.IBSAccountId, order.Settings.Phone);
-
-            var ibsStatus = new IBSOrderInformation
-            {
-                Status = status.Status,
-                IBSOrderId = order.IBSOrderId.Value,
-            };
 
 
-            _orderStatusUpdater.Update(ibsStatus, orderStatus);
+
         }
 
 
-        public void CheckStatus()
+        public void CheckStatus(string updaterUniqueId)
         {
+            var lastUpdate = _orderStatusUpdateDao.GetLastUpdate();
 
-            var orders = _orderDao.GetOrdersInProgress().ToArray();
+            int pollingValue = _configManager.GetSetting<int>("OrderStatus.ServerPollingInterval", 10);
 
-            BatchUpdateStatus(orders.Where(o => o.Status == OrderStatus.Pending));
 
-            BatchUpdateStatus(orders.Where(o => o.Status == OrderStatus.Created));
+            if ((lastUpdate == null) ||
+                (lastUpdate.UpdaterUniqueId == updaterUniqueId) ||
+                (DateTime.UtcNow.Subtract(lastUpdate.LastUpdateDate).TotalSeconds > 20))
+            {
 
-            BatchUpdateStatus(orders.Where(o => (o.Status != OrderStatus.Pending) && (o.Status != OrderStatus.Created)));
+                var timer = Observable.Timer(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(pollingValue))
+                                .Subscribe(_ =>_orderStatusUpdateDao.UpdateLastUpdate(updaterUniqueId, DateTime.UtcNow));                                    
 
+                try
+                {
+                    var orders = _orderDao.GetOrdersInProgress().ToArray();
+
+                    BatchUpdateStatus(orders.Where(o => o.Status == OrderStatus.Pending));
+
+                    BatchUpdateStatus(orders.Where(o => o.Status == OrderStatus.Created));
+
+                    BatchUpdateStatus(orders.Where(o => (o.Status != OrderStatus.Pending) && (o.Status != OrderStatus.Created)));
+
+                    Thread.Sleep(new Random().Next(1, 15) * 1000);
+                }
+                finally
+                {
+                    timer.Dispose();
+                }
+            }
         }
 
         public void BatchUpdateStatus(IEnumerable<OrderStatusDetail> orders)
@@ -90,6 +139,8 @@ namespace apcurium.MK.Booking.Api.Jobs
                 }
 
             }
+
+
         }
     }
 }
