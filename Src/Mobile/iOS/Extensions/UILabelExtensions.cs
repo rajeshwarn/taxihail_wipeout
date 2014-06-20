fe042b67ae14425d84f077140ff8b5f378970a4b
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-06-20 11:11:13 -0400

    fixed ridesettings iOS

---
+++ Src/Mobile/iOS/Extensions/UILabelExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MonoTouch.UIKit;
+using apcurium.MK.Booking.Mobile.Client.Extensions.Helpers;
+
+namespace apcurium.MK.Booking.Mobile.Client.Extensions
+{
+	public static class UILabelExtensions
+	{
+		public static void TextAlignment(this UILabel label)
+		{
+			if (UIHelper.IsOS7orHigher) 
+			{
+				label.TextAlignment = UITextAlignment.Natural;
+
+			} 
+			else 
+			{
+				if (label.Services ().Localize.IsRightToLeft)
+				{
+					label.TextAlignment = UITextAlignment.Right;
+				}
+			}
+		}
+	}
+}
+
