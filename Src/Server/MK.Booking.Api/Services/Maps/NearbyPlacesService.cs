1938f6ddc337e45efe02aeff020e0f5c090645a4
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-07-11 15:04:07 -0400

    MKTAXI-1758 add language info on services server-side

--- Src/Server/MK.Booking.Api/Services/Maps/NearbyPlacesService.cs
+++ Src/Server/MK.Booking.Api/Services/Maps/NearbyPlacesService.cs
@@ -1,9 +1,12 @@
 ﻿#region
 
+using System;
+using System.Globalization;
 using System.Net;
 using apcurium.MK.Booking.Api.Contract.Requests;
 using apcurium.MK.Booking.Api.Contract.Resources;
 using apcurium.MK.Booking.Maps;
+using apcurium.MK.Booking.ReadModel.Query.Contract;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceInterface;
 
@@ -14,10 +17,12 @@ namespace apcurium.MK.Booking.Api.Services.Maps
     public class NearbyPlacesService : Service
     {
         private readonly IPlaces _client;
+        private readonly IAccountDao _accountDao;
 
-        public NearbyPlacesService(IPlaces client)
+        public NearbyPlacesService(IPlaces client, IAccountDao accountDao)
         {
             _client = client;
+            _accountDao = accountDao;
         }
 
         public object Get(NearbyPlacesRequest request)
@@ -28,7 +33,17 @@ namespace apcurium.MK.Booking.Api.Services.Maps
                 throw new HttpError(HttpStatusCode.BadRequest, ErrorCode.NearbyPlaces_LocationRequired.ToString());
             }
 
-            return _client.SearchPlaces(request.Name, request.Lat, request.Lng, request.Radius);
+            var language = CultureInfo.CurrentUICulture.Name;
+            if (this.GetSession() != null)
+            {
+                var account = _accountDao.FindById(new Guid(this.GetSession().UserAuthId));
+                if (account != null)
+                {
+                    language = account.Language;
+                }
+            }
+
+            return _client.SearchPlaces(request.Name, request.Lat, request.Lng, request.Radius, language);
         }
     }
 }
