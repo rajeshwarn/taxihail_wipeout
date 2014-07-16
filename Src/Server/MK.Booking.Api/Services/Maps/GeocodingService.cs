20f7bb8154fbf42b9b38e99aa612e37635233ce3
Mathieu Savaria <mathieu.savaria@apcurium.com>
2014-07-16 11:32:50 -0400

    Fix: Geocoding search method needed the account language as a parameter

--- Src/Server/MK.Booking.Api/Services/Maps/GeocodingService.cs
+++ Src/Server/MK.Booking.Api/Services/Maps/GeocodingService.cs
@@ -1,8 +1,11 @@
 ﻿#region
 
+using System;
+using System.Globalization;
 using System.Net;
 using apcurium.MK.Booking.Api.Contract.Requests;
 using apcurium.MK.Booking.Maps;
+using apcurium.MK.Booking.ReadModel.Query.Contract;
 using apcurium.MK.Common.Extensions;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceInterface;
@@ -17,10 +20,12 @@ namespace apcurium.MK.Booking.Api.Services.Maps
     public class GeocodingService :Service
     {
         private readonly IGeocoding _geocoding;
+        private readonly IAccountDao _accountDao;
 
-        public GeocodingService(IGeocoding geocoding)
+        public GeocodingService(IGeocoding geocoding, IAccountDao accountDao)
         {
             _geocoding = geocoding;
+            _accountDao = accountDao;
         }
 
         public object Post(GeocodingRequest request)
@@ -31,12 +36,25 @@ namespace apcurium.MK.Booking.Api.Services.Maps
                 throw new HttpError(HttpStatusCode.BadRequest, "400", "You must specify the name or the coordinate");
             }
 
+            // Get current language
+            var language = CultureInfo.CurrentUICulture.Name;
+
+            if (this.GetSession() != null
+                && this.GetSession().UserAuthId != null)
+            {
+                var account = _accountDao.FindById(new Guid(this.GetSession().UserAuthId));
+                if (account != null)
+                {
+                    language = account.Language;
+                }
+            }
+
             if (request.Name.HasValue())
             {
-                return _geocoding.Search(request.Name, request.GeoResult);
+                return _geocoding.Search(request.Name, language, request.GeoResult);
             }
 // ReSharper disable PossibleInvalidOperationException
-            return _geocoding.Search(request.Lat.Value, request.Lng.Value, request.GeoResult);
+            return _geocoding.Search(request.Lat.Value, request.Lng.Value, language, request.GeoResult);
 // ReSharper restore PossibleInvalidOperationException
         }
     }
