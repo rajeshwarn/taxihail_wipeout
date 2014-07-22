4078ffcb4606fee9cc9247068dc5e08f54a18875
Mathieu Savaria <mathieu.savaria@apcurium.com>
2014-07-22 15:03:21 -0400

    MKTAXI-1777: Unit tests

--- Src/Server/MK.Booking/MK.Booking.Test/Maps/PriceCalculatorFixture/given_a_date.cs
+++ Src/Server/MK.Booking/MK.Booking.Test/Maps/PriceCalculatorFixture/given_a_date.cs
@@ -29,9 +29,18 @@ namespace apcurium.MK.Booking.Test.Maps.PriceCalculatorFixture
                 VehicleTypeId = 10
             };
 
+            var tariff2 = new Tariff
+            {
+                Name = "Default Day tariff ending the same day for all vehicles",
+                StartTime = new DateTime(2012, 12, 17, 8, 0, 0),
+                EndTime = new DateTime(2012, 12, 17, 20, 0, 0),
+                Type = (int)TariffType.Day, // Monday
+                VehicleTypeId = null
+            };
+
             _tariffProvider = new FakeTariffProvider(new[]
             {
-                tariff1
+                tariff1, tariff2
             });
             _sut = new PriceCalculator(new TestConfigurationManager(), _tariffProvider, new Logger());
         }
@@ -87,6 +96,14 @@ namespace apcurium.MK.Booking.Test.Maps.PriceCalculatorFixture
             var tariff = _sut.GetTariffFor(new DateTime(2012, 12, 17, 6, 0, 0), 10);
 
             Assert.Null(tariff);
+        }
+
+        [Test]
+        public void when_date_match_day_and_start_time_exactly_for_all_vehicles()
+        {
+            var tariff = _sut.GetTariffFor(new DateTime(2012, 12, 17, 8, 0, 0));
+            Assert.NotNull(tariff);
+            Assert.AreEqual("Default Day tariff ending the same day for all vehicles", tariff.Name);
         }
     }
 
@@ -192,9 +209,19 @@ namespace apcurium.MK.Booking.Test.Maps.PriceCalculatorFixture
                 VehicleTypeId = 10
             };
 
+            var tariff2 = new Tariff
+            {
+                Name = "Default Recurring tariff for all vehicle types",
+                StartTime = new DateTime(1900, 1, 1, 8, 0, 0),
+                EndTime = new DateTime(1900, 1, 1, 20, 0, 0),
+                DaysOfTheWeek = (int)(DayOfTheWeek.Thursday | DayOfTheWeek.Friday | DayOfTheWeek.Saturday),
+                Type = (int)TariffType.Recurring,
+                VehicleTypeId = null
+            };
+
             _tariffProvider = new FakeTariffProvider(new[]
             {
-                tariff1
+                tariff1, tariff2
             });
             _sut = new PriceCalculator(new TestConfigurationManager(), _tariffProvider, new Logger());
         }
@@ -250,6 +277,14 @@ namespace apcurium.MK.Booking.Test.Maps.PriceCalculatorFixture
             var tariff = _sut.GetTariffFor(new DateTime(2012, 12, 21, 7, 0, 0));
 
             Assert.Null(tariff);
+        }
+
+        [Test]
+        public void when_date_match_day_and_start_time_exactly_for_all_vehicle()
+        {
+            var tariff = _sut.GetTariffFor(new DateTime(2012, 12, 21, 9, 0, 0) /* Friday */);
+            Assert.NotNull(tariff);
+            Assert.AreEqual("Default Recurring tariff for all vehicle types", tariff.Name);
         }
     }
 
@@ -372,6 +407,41 @@ namespace apcurium.MK.Booking.Test.Maps.PriceCalculatorFixture
 
             Assert.NotNull(tariff);
             Assert.AreEqual("Recurring tariff", tariff.Name);
+        }
+    }
+
+    [TestFixture]
+    public class given_a_defaultVehicle_tariff_ending_the_next_day
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var tariff1 = new Tariff
+            {
+                Name = "Vehicle default tariff ending the next day",
+                StartTime = new DateTime(1900, 1, 1, 20, 0, 0),
+                EndTime = new DateTime(1900, 1, 2, 8, 0, 0),
+                Type = (int) TariffType.VehicleDefault,
+                VehicleTypeId = 10
+            };
+
+            _tariffProvider = new FakeTariffProvider(new[]
+            {
+                tariff1
+            });
+            _sut = new PriceCalculator(new TestConfigurationManager(), _tariffProvider, new Logger());
+        }
+
+        private PriceCalculator _sut;
+        private FakeTariffProvider _tariffProvider;
+
+        [Test]
+        public void when_date_match_day_and_inside_time_period()
+        {
+            var tariff = _sut.GetTariffFor(new DateTime(1900, 1, 1, 23, 0, 0), 10);
+
+            Assert.NotNull(tariff);
+            Assert.AreEqual("Vehicle default tariff ending the next day", tariff.Name);
         }
     }
 
