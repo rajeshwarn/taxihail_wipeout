4470eb07c7d250040f24e6f74bbdbe949f683a70
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-07-31 07:44:03 -0400

    Cleanup event

--- Src/Server/MK.Booking/MK.Booking.Events/CreditCardAdded.cs
+++ Src/Server/MK.Booking/MK.Booking.Events/CreditCardAdded.cs
@@ -10,15 +10,11 @@ namespace apcurium.MK.Booking.Events
     public class CreditCardAdded : VersionedEvent
     {
         public Guid CreditCardId { get; set; }
-        public Guid AccountId { get; set; }
         public string NameOnCard { get; set; }
         public string Token { get; set; }
         public string Last4Digits { get; set; }
         public string ExpirationMonth { get; set; }
         public string ExpirationYear { get; set; }
         public string CreditCardCompany { get; set; }
-
-        // not used anymore but to prevent incompatible event deserialization in dbinit
-        public string FriendlyName { get; set; } 
     }
 }
