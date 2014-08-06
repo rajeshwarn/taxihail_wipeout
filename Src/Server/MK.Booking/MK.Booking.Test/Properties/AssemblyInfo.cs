cb989758e7c79a9acbb50bf5a5c245c7003988dc
Alex Proteau <alex.proteau@apcurium.com>
2014-08-05 22:09:29 -0400

    Version bump 1.5.40

--- Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.28")]
-[assembly: AssemblyVersion("1.5.28")]
-[assembly: AssemblyFileVersion("1.5.28")]
+// [assembly: AssemblyVersion("1.5.40")]
+[assembly: AssemblyVersion("1.5.40")]
+[assembly: AssemblyFileVersion("1.5.40")]
 
