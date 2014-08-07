8e1ba3919dfb1994d466f82baaf8a3e8ad97501a
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-08-07 10:56:52 -0400

    MKTAXI-1849 [Android] Better zoom behavior

--- Src/Mobile/Android/Controls/Fragments/TouchableMap.cs
+++ Src/Mobile/Android/Controls/Fragments/TouchableMap.cs
@@ -9,6 +9,8 @@ using Android.Widget;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using apcurium.MK.Common.Configuration;
 using TinyIoC;
+using Android.App;
+using Android.Support.V4.View;
 
 namespace apcurium.MK.Booking.Mobile.Client.Controls
 {
@@ -29,9 +31,15 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
 			var longitude = bestPosition != null ? bestPosition.Longitude : settings.DefaultLongitude;
 
             Map.MapType = GoogleMap.MapTypeNormal;
+            Map.MoveCamera(CameraUpdateFactory.NewLatLngZoom(new LatLng(latitude, longitude) , 12f));
+
+            // disable gestures on the map since we're handling them ourselves
             Map.UiSettings.CompassEnabled = false;
             Map.UiSettings.ZoomControlsEnabled = false;
-            Map.MoveCamera(CameraUpdateFactory.NewLatLngZoom(new LatLng(latitude, longitude) , 12f));
+            Map.UiSettings.ZoomGesturesEnabled = false;
+            Map.UiSettings.RotateGesturesEnabled = false;
+            Map.UiSettings.TiltGesturesEnabled = false;
+            Map.UiSettings.ScrollGesturesEnabled = false;
 
             Surface = new TouchableWrapper(Activity);
             Surface.AddView(mOriginalContentView);
@@ -47,23 +55,37 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
         }
     }
 
-    public class TouchableWrapper: FrameLayout {
+    public class TouchableWrapper : FrameLayout 
+    {
+        private GestureDetector _gestureDetector;
+        private ScaleGestureDetector _scaleDetector;
 
         public event EventHandler<MotionEvent> Touched;
+        public Action<bool, float> ZoomBy;
+        public Action<float, float> MoveBy;
 
         public TouchableWrapper(Context context) :
         base(context)
         {
+            Initialize ();
         }
 
         public TouchableWrapper(Context context, IAttributeSet attrs) :
         base(context, attrs)
         {
+            Initialize ();
         }
 
         public TouchableWrapper(Context context, IAttributeSet attrs, int defStyle) :
         base(context, attrs, defStyle)
         {
+            Initialize ();
+        }
+
+        private void Initialize()
+        {
+            _gestureDetector = new GestureDetector (Context, new GestureListener (this));
+            _scaleDetector = new ScaleGestureDetector (Context, new ScaleListener (this));
         }
 
         public override bool DispatchTouchEvent(MotionEvent e)
@@ -73,7 +95,62 @@ namespace apcurium.MK.Booking.Mobile.Client.Controls
                 this.Touched(this, e);
             }
 
-            return base.DispatchTouchEvent(e);
+            _gestureDetector.OnTouchEvent (e);
+            _scaleDetector.OnTouchEvent (e);
+
+            return true;
+        }
+
+        private class GestureListener : GestureDetector.SimpleOnGestureListener
+        {
+            private readonly TouchableWrapper _view;
+            public GestureListener (TouchableWrapper view)
+            {   
+                _view = view;
+            }
+
+            public override bool OnScroll (MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+            {
+                if (e1.PointerCount > 1 || e2.PointerCount > 1)
+                {
+                    // don't scroll if we have more than one finger
+                    return false;
+                }
+
+                _view.MoveBy.Invoke (distanceX, distanceY);
+                return true;
+            }
+
+            public override bool OnDoubleTap (MotionEvent e)
+            {
+                if (e.PointerCount > 1)
+                {
+                    // Zooming out on double tap with multitouch
+                    _view.ZoomBy.Invoke (true, -1f);
+                }
+                else
+                {
+                    // Zooming in on double tap with one finger
+                    _view.ZoomBy.Invoke (true, 1f);
+                }
+
+                return true;
+            }
+        }
+
+        private class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener 
+        {
+            private readonly TouchableWrapper _view;
+            public ScaleListener (TouchableWrapper view)
+            {   
+                _view = view;
+            }
+
+            public override bool OnScale (ScaleGestureDetector detector)
+            {
+                _view.ZoomBy.Invoke (false, (detector.ScaleFactor - 1f) * 4);
+                return true;
+            }
         }
     }
 }
