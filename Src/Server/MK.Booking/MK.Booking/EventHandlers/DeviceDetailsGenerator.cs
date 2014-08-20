346206859c6b7644faaa7d41435ad4bb7514c6c7
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-08-20 08:41:40 -0400

    Backout changeset e439673a551a1f59d766528669c187749753c9b4

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/DeviceDetailsGenerator.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/DeviceDetailsGenerator.cs
@@ -1,12 +1,9 @@
 ﻿#region
 
 using System;
-using System.Linq;
 using apcurium.MK.Booking.Database;
 using apcurium.MK.Booking.Events;
 using apcurium.MK.Booking.ReadModel;
-using apcurium.MK.Booking.ReadModel.Query.Contract;
-using apcurium.MK.Common.Extensions;
 using Infrastructure.Messaging.Handling;
 
 #endregion
@@ -18,22 +15,16 @@ namespace apcurium.MK.Booking.EventHandlers
         IEventHandler<DeviceUnregisteredForPushNotifications>
     {
         private readonly Func<BookingDbContext> _contextFactory;
-        private readonly IDeviceDao _deviceDao;
 
-        public DeviceDetailsGenerator(Func<BookingDbContext> contextFactory, IDeviceDao deviceDao)
+        public DeviceDetailsGenerator(Func<BookingDbContext> contextFactory)
         {
             _contextFactory = contextFactory;
-            _deviceDao = deviceDao;
         }
 
         public void Handle(DeviceRegisteredForPushNotifications @event)
         {
             using (var context = _contextFactory.Invoke())
             {
-                // Unassign this device to all other accounts it might have been registered to before
-                context.RemoveWhere<DeviceDetail>(x => x.DeviceToken == @event.DeviceToken && x.AccountId != @event.SourceId);
-                context.SaveChanges();
-
                 var device = context.Set<DeviceDetail>().Find(@event.SourceId, @event.DeviceToken);
                 if (device == null)
                 {
