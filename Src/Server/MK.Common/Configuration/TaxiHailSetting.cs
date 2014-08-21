8616d979adef7b2254934f96ffb0fbedd357430a
Matthieu Guyonnet-Duluc <matthieu@live.com>
2014-08-21 17:05:10 -0400

    add default values for settings

--- Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
+++ Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
@@ -28,7 +28,7 @@ namespace MK.Common.Configuration
             CompanySettings = "Client.ShowEstimateWarning,Client.DestinationIsRequired,IBS.TimeDifference,IBS.PickupZoneToExclude,IBS.DestinationZoneToExclude,IBS.ValidateDestinationZone,IBS.ValidatePickupZone,Booking.ConfirmationEmail,Client.HideCallDispatchButton,DefaultBookingSettings.ChargeTypeId,DefaultBookingSettings.NbPassenger,DefaultBookingSettings.ProviderId,DefaultBookingSettings.VehicleTypeId,Receipt.Note,Client.HideReportProblem,OrderStatus.ServerPollingInterval,IBS.NoteTemplate,AccountActivationDisabled,AvailableVehicles.Enabled,AvailableVehicles.Radius,AvailableVehicles.Count,Store.AppleLink,Store.PlayLink,Client.AccountChargeTypeId";
 			DefaultTipPercentage = 15;
             DirectionDataProvider = MapProvider.Google;
-
+		    EtaPaddingRatio = 1;
 		}
 
 		[Display(Name = "Application Name", Description="Application name as displayed in message")]
