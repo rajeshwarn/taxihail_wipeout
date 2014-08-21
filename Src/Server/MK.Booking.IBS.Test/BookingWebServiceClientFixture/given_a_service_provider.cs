c495d8868dabd53b526e90dcee52d69da8a0fbac
Mathieu Savaria <mathieu.savaria@apcurium.com>
2014-08-21 09:25:29 -0400

    Fixed getAvailableVehicle unit test

--- Src/Server/MK.Booking.IBS.Test/BookingWebServiceClientFixture/given_a_service_provider.cs
+++ Src/Server/MK.Booking.IBS.Test/BookingWebServiceClientFixture/given_a_service_provider.cs
@@ -4,6 +4,7 @@ using System;
 using System.Linq;
 using apcurium.MK.Booking.IBS;
 using apcurium.MK.Booking.IBS.Impl;
+using apcurium.MK.Common.Configuration;
 using apcurium.MK.Common.Diagnostic;
 using AutoMapper;
 using NUnit.Framework;
@@ -17,10 +18,14 @@ namespace MK.Booking.IBS.Test.BookingWebServiceClientFixture
         [Test]
         public void StaticDataservice_test()
         {
+            IConfigurationManager config = new FakeConfigurationManager();
+            ILogger logger = new Logger();
+            IStaticDataWebServiceClient webServiceClient = new StaticDataWebServiceClient(config, logger);
+
             var profile = new IbsAutoMapperProfile();
             Mapper.AddProfile(profile);
 
-            var service = new BookingWebServiceClient(new FakeConfigurationManager(), new Logger(), null);
+            var service = new BookingWebServiceClient(config, logger, webServiceClient);
 
             var test = service.GetAvailableVehicles(45.498247, -73.656673, 1).ToList();
 
