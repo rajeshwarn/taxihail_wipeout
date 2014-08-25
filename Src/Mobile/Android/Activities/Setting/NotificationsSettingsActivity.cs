5cee3654d3e83a7a15d87bb0f14e98a7caf78d93
Mathieu Savaria <mathieu.savaria@apcurium.com>
2014-08-25 13:58:34 -0400

    MKTAXI-1859: [Android] Notifications settings view skeleton

---
+++ Src/Mobile/Android/Activities/Setting/NotificationsSettingsActivity.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using apcurium.MK.Booking.Mobile.ViewModels;
+
+namespace apcurium.MK.Booking.Mobile.Client.Activities.Setting
+{
+    [Activity(Label = "PushSettingsActivity",
+        Theme = "@style/MainTheme",
+        ScreenOrientation = ScreenOrientation.Portrait)]
+    public class NotificationsSettingsActivity : BaseBindingActivity<NotificationsSettingsViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.View_NotificationsSettings);
+        }
+    }
+}
