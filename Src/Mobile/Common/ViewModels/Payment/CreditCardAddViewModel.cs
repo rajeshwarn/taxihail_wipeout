3722af9d5d3f34e5035e2dd5d9fb2cb03e88995f
Pierre Garcia <pierre.garcia@apcurium.com>
2014-09-11 14:00:54 -0400

    MKTAXI-1915 When adding a credit card for the first time, the month and year should be empty

--- Src/Mobile/Common/ViewModels/Payment/CreditCardAddViewModel.cs
+++ Src/Mobile/Common/ViewModels/Payment/CreditCardAddViewModel.cs
@@ -133,8 +133,8 @@ namespace apcurium.MK.Booking.Mobile.ViewModels.Payment
 				Data.NameOnCard = creditCard.NameOnCard;
 				Data.CreditCardCompany = creditCard.CreditCardCompany;
 
-				ExpirationMonth = string.IsNullOrWhiteSpace(creditCard.ExpirationMonth) ? null : int.Parse(creditCard.ExpirationMonth);
-				ExpirationYear = string.IsNullOrWhiteSpace(creditCard.ExpirationYear) ? null : int.Parse(creditCard.ExpirationYear);
+				ExpirationMonth = string.IsNullOrWhiteSpace(creditCard.ExpirationMonth) ? (int?)null : int.Parse(creditCard.ExpirationMonth);
+				ExpirationYear = string.IsNullOrWhiteSpace(creditCard.ExpirationYear) ? (int?)null : int.Parse(creditCard.ExpirationYear);
 
 				var id = CreditCardCompanies.Find(x => x.Display == creditCard.CreditCardCompany).Id;
 				if (id != null)
