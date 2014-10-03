7ff74952fcd9092cf0c143bf98ffbae6ed2f2957
Alex Proteau <alex.proteau@apcurium.com>
2014-10-03 15:56:01 -0400

    Version bump 1.5.42

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.40")]
-[assembly: AssemblyVersion("1.5.40")]
-[assembly: AssemblyFileVersion("1.5.40")]
+// [assembly: AssemblyVersion("1.5.42")]
+[assembly: AssemblyVersion("1.5.42")]
+[assembly: AssemblyFileVersion("1.5.42")]
 
--- Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
+++ Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.40")]
-[assembly: AssemblyVersion("1.5.40")]
-[assembly: AssemblyFileVersion("1.5.40")]
+// [assembly: AssemblyVersion("1.5.42")]
+[assembly: AssemblyVersion("1.5.42")]
+[assembly: AssemblyFileVersion("1.5.42")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
