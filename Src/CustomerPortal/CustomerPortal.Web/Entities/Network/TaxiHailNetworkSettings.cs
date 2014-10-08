06878563380ae252314193099c91b88e173123ab
MBP-Pierre\pierre.garcia <pierre.garcia@apcurium.com>
2014-10-08 09:43:12 -0400

    MKTAXI-2167-[Backend] Bind isInNetwork and Location controls Added view and realtion do mongo

--- Src/CustomerPortal/CustomerPortal.Web/Entities/Network/TaxiHailNetworkSettings.cs
+++ Src/CustomerPortal/CustomerPortal.Web/Entities/Network/TaxiHailNetworkSettings.cs
@@ -2,14 +2,18 @@ using System.ComponentModel.DataAnnotations;
 using CustomerPortal.Web.Properties;
 
 namespace CustomerPortal.Web.Entities.Network
+
 {
+    
     public class TaxiHailNetworkSettings
     {
+        [Required]
         [Display(Name = "IncludeInTaxiHailNetwork", Description = "IncludeInTaxiHailNetworkHelp",
             ResourceType = typeof(Resources))]
         public bool IsInNetwork { get; set; }
 
          [Display(Name = "RegionTaxhiHailNetworkLabel", ResourceType = typeof(Resources))]
+         [Required]
         public MapRegion Region {get; set; }
     }
 }
