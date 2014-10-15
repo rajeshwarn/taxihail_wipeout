4a1615851373b07795fb4c42b22b965a1911719d
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-10-15 17:54:39 -0400

    MKTAXI-2201 Endpoint to retrieve latest logins

---
+++ Src/Server/MK.Booking.Api.Contract/Requests/AppStartUpLogRequest.cs
@@ -0,0 +1,17 @@
+﻿using apcurium.MK.Booking.Api.Contract.Security;
+using apcurium.MK.Booking.Security;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+
+namespace apcurium.MK.Booking.Api.Contract.Requests
+{
+    [Authenticate]
+    #if !CLIENT
+    [AuthorizationRequired(ApplyTo.Get, RoleName.Admin)]
+    #endif
+    [Route("/app/starts/{LastMinutes}")]
+    public class AppStartUpLogRequest
+    {
+        public long LastMinutes { get; set; }
+    }
+}
