2bfa5bce1bc3b1376d221912f5fc9cafcf2e1d49
Brian Ricard <brian.ricard@apcurium.com>
2014-10-29 16:30:18 -0400

    Version bump

--- Src/Server/MK.Booking.MapDataProvider/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.MapDataProvider/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.271")]
-[assembly: AssemblyVersion("1.5.271")]
-[assembly: AssemblyFileVersion("1.5.271")]
+// [assembly: AssemblyVersion("1.5.272")]
+[assembly: AssemblyVersion("1.5.272")]
+[assembly: AssemblyFileVersion("1.5.272")]
 
