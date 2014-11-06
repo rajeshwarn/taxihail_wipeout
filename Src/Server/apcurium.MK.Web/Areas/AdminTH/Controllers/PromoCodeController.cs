b5a0446db97abfb30e0b55fb74d9312031aee1d2
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-11-06 16:52:13 -0500

    Start temp

---
+++ Src/Server/apcurium.MK.Web/Areas/AdminTH/Controllers/PromoCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace apcurium.MK.Web.Areas.AdminTH.Controllers
+{
+    public class PromoCodeController : Controller
+    {
+        // GET: AdminTH/PromoCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
