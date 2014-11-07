95ed1ca814c97706f1dcb9553059ee4152c0824c
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-11-07 10:33:05 -0500

    Start

--- Src/Server/apcurium.MK.Web/Areas/AdminTH/Controllers/PromoCodeController.cs
+++ Src/Server/apcurium.MK.Web/Areas/AdminTH/Controllers/PromoCodeController.cs
@@ -6,12 +6,91 @@ using System.Web.Mvc;
 
 namespace apcurium.MK.Web.Areas.AdminTH.Controllers
 {
+    public class PromoCode
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
     public class PromoCodeController : Controller
     {
         // GET: AdminTH/PromoCode
         public ActionResult Index()
         {
+            var promoCodes = new List<PromoCode> {new PromoCode {Id = Guid.NewGuid(), Name = "test"}};
+            return View(promoCodes);
+        }
+
+        // GET: AdminTH/PromoCode/Details/5
+        public ActionResult Details(Guid id)
+        {
             return View();
+        }
+
+        // GET: AdminTH/PromoCode/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminTH/PromoCode/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminTH/PromoCode/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            return View();
+        }
+
+        // POST: AdminTH/PromoCode/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminTH/PromoCode/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            return View();
+        }
+
+        // POST: AdminTH/PromoCode/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Guid id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
