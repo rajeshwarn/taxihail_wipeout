4823bfa1ea92743a2cd51a618ffffc077cec2ad8
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-11-19 15:03:22 -0500

    MKTAXI-2306 Admin page to modify privacy policy

---
+++ Src/Server/apcurium.MK.Web/admin/models/PrivacyPolicy.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    TaxiHail.PrivacyPolicy = Backbone.Model.extend({
+        urlRoot: TaxiHail.parameters.apiRoot + "/privacypolicy",
+
+        save: function (privacypolicy) {
+            return $.ajax({
+                type: 'POST',
+                url: this.urlRoot,
+                data: JSON.stringify({
+                    policy: privacypolicy
+                }),
+                contentType: 'application/json'
+            });
+        }
+    });
+
+}());
