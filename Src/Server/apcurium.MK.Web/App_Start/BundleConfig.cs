48f8a27dabf44611936619c32219da8e5976cab7
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-11-20 14:03:26 -0500

    Remove privacy policy js admin page

--- Src/Server/apcurium.MK.Web/App_Start/BundleConfig.cs
+++ Src/Server/apcurium.MK.Web/App_Start/BundleConfig.cs
@@ -167,7 +167,6 @@ namespace apcurium.MK.Web
                 "~/admin/models/VehicleType.js",
                 "~/admin/models/UnassignedReferenceDataVehicles.js",
                 "~/admin/models/RideRatings.js",
-                "~/admin/models/PrivacyPolicy.js",
                 /* Collections */
                 "~/common/collections/AddressCollection.js",
                 "~/common/collections/AvailableVehicleCollection.js",
@@ -193,7 +192,6 @@ namespace apcurium.MK.Web
                 "~/admin/controllers/AccountsChargeController.js",
                 "~/admin/controllers/VehicleTypesController.js",
                 "~/admin/controllers/RideRatingsController.js",
-                "~/admin/controllers/PrivacyPolicyController.js",
                 /* Views */
                 "~/admin/views/AddPopularAddressView.js",
                 "~/common/views/AlertView.js",
@@ -232,7 +230,6 @@ namespace apcurium.MK.Web
                 "~/admin/views/AddVehicleTypeView.js",
                 "~/admin/views/ManageVehicleTypesView.js",
                 "~/admin/views/VehicleTypeItemView.js",
-                "~/admin/views/UpdatePrivacyPolicyView.js",
                 /* Services */
                 "~/common/services/AuthService.js",
                 "~/common/services/GeocodingService.js",
