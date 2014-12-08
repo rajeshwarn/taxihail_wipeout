ac720ef8a61e03e6349e25709422ed7b3ac9f5b3
Mathieu Savaria <mathieu.savaria@apcurium.com>
2014-12-08 14:13:29 -0500

    Cleanup

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/OrderDispatchCompanyManager.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/OrderDispatchCompanyManager.cs
@@ -123,7 +123,7 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
                 ? networkFleet.First()
                 : FindNextAvailableCompanyInIbsZone(currentCompanyKey, pickupPosition, networkFleet, market);
 
-            return nextDispatchCompany;
+            return nextDispatchCompany; 
         }
 
         private NetworkFleetResponse FindNextAvailableCompanyInIbsZone(string currentCompanyKey, MapCoordinate pickupPosition, IList<NetworkFleetResponse> networkFleet, string market = null)
@@ -176,13 +176,13 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
                     var currentFleet = allMarketFleets.First(f => f.CompanyKey == currentCompanyKey);
 
                     // Return only those from the ordered fleet
-                    var test = orderedFleets
+                    var nonNullOrdered = orderedFleets
                         .Select(v => marketFleets.FirstOrDefault(f => f.FleetId == v.FleetId))
                         .Where(f => f != null)
                         .ToList();
 
-                    test.Insert(0, currentFleet);
-                    return test;
+                    nonNullOrdered.Insert(0, currentFleet);
+                    return nonNullOrdered;
                 }
 
                 // Nothing found, extend search radius (total radius after 10 iterations: 3375m)
