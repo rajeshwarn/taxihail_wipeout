b70fe50d243ac393ada4fc725f2dfac042d86b90
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-12-09 15:51:30 -0500

    React

--- Src/Server/MK.Booking/MK.Booking.Events/CreditCardPaymentCaptured_V2.cs
+++ Src/Server/MK.Booking/MK.Booking.Events/CreditCardPaymentCaptured_V2.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using apcurium.MK.Common.Enumeration;
-using Infrastructure.EventSourcing;
-
-namespace apcurium.MK.Booking.Events
+﻿namespace apcurium.MK.Booking.Events
 {
-    public class CreditCardPaymentCaptured_V2 : VersionedEvent
+    public class CreditCardPaymentCaptured_V2 : CreditCardPaymentCaptured
     {
-        public string TransactionId { get; set; }
-        public string AuthorizationCode { get; set; }
-        public decimal Amount { get; set; }
-        public decimal Meter { get; set; }
-        public decimal Tip { get; set; }
         public decimal Tax { get; set; }
-        public PaymentProvider Provider { get; set; }
-        public Guid OrderId { get; set; }
-        public bool IsNoShowFee { get; set; }
-        public Guid? PromotionUsed { get; set; }
-        public decimal AmountSavedByPromotion { get; set; }
     }
 }
