091d527524786219f863b6eb5cc21254735f400b
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2014-12-12 16:18:03 -0500

    MKTAXI-2336: Keep track of promotion unlock progression + handing of ride count trigger and amount spent trigger

---
+++ Src/Server/MK.Booking/MK.Booking/ReadModel/PromotionProgressDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace apcurium.MK.Booking.ReadModel
+{
+    public class PromotionProgressDetail
+    {
+        [Key, Column(Order = 1)]
+        public Guid AccountId { get; set; }
+
+        [Key, Column(Order = 2)]
+        public Guid PromoId { get; set; }
+
+        public int? RideCount { get; set; }
+
+        public double? AmountSpent { get; set; }
+    }
+}
--- Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IPromotionDao.cs
+++ Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IPromotionDao.cs
@@ -6,9 +6,13 @@ namespace apcurium.MK.Booking.ReadModel.Query.Contract
     public interface IPromotionDao
     {
         IEnumerable<PromotionDetail> GetAll();
+
         IEnumerable<PromotionDetail> GetAllCurrentlyActive();
+
         PromotionDetail FindById(Guid id);
+
         PromotionDetail FindByPromoCode(string promoCode);
+
         PromotionUsageDetail FindByOrderId(Guid orderId);
     }
 }
