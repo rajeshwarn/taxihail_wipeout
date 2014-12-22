18343dc2d32854e9ce4c4afacc5595d7f8fef10c
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2014-12-22 11:07:45 -0500

    MKTAXI-2363 Use display attribute for trigger types enum list

--- Src/Server/MK.Common/Enumeration/PromotionTriggerTypes.cs
+++ Src/Server/MK.Common/Enumeration/PromotionTriggerTypes.cs
@@ -1,21 +1,7 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using apcurium.MK.Common.Entity;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace apcurium.MK.Common.Enumeration
 {
-    //public class PromotionTriggerTypes
-    //{
-    //    public static readonly ListItem NoTrigger = new ListItem { Id = 0, Display = "No Trigger" };
-    //    public static readonly ListItem AccountCreated = new ListItem { Id = 1, Display = "Account Created" };
-    //    public static readonly ListItem RideCount = new ListItem { Id = 2, Display = "Ride Count" };
-    //    public static readonly ListItem AmountSpent = new ListItem { Id = 3, Display = "Amount Spent" };
-
-    //    public static IEnumerable<ListItem> GetList()
-    //    {
-    //        return new[] { NoTrigger, AccountCreated, RideCount, AmountSpent };
-    //    }
-    //}
     public enum PromotionTriggerTypes
     {
         [Display(Name = "No Trigger")]
