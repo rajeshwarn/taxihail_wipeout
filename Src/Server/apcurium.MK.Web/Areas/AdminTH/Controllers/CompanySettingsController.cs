ec4a5e43473c1b895afd836173dd67b89e696649
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-01-12 14:09:20 -0500

    MKTAXI-2375 & MKTAXI-2378: MVC Company settings page and menu + settings validation + dropdown for enums

---
+++ Src/Server/apcurium.MK.Web/Areas/AdminTH/Controllers/CompanySettingsController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using apcurium.MK.Booking.Commands;
+using apcurium.MK.Booking.Security;
+using apcurium.MK.Common;
+using apcurium.MK.Common.Configuration;
+using apcurium.MK.Web.Attributes;
+using Infrastructure.Messaging;
+using ServiceStack.CacheAccess;
+using ServiceStack.Configuration;
+using ServiceStack.ServiceModel.Extensions;
+
+namespace apcurium.MK.Web.Areas.AdminTH.Controllers
+{
+    [AuthorizationRequired(RoleName.Admin)]
+    public class CompanySettingsController : ServiceStackController
+    {
+        private readonly IServerSettings _serverSettings;
+        private readonly ICommandBus _commandBus;
+
+
+        // GET: AdminTH/CompanySettings
+        public CompanySettingsController(ICacheClient cache, IServerSettings serverSettings, ICommandBus commandBus)
+            : base(cache, serverSettings)
+        {
+            _serverSettings = serverSettings;
+            _commandBus = commandBus;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_serverSettings.ServerData);
+        }
+
+        // POST: AdminTH/CompanySettings/Update
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Update(FormCollection form)
+        {
+            var appSettings = form.ToDictionary();
+            appSettings.Remove("__RequestVerificationToken");
+
+            if (appSettings.Any())
+            {
+                var command = new AddOrUpdateAppSettings
+                {
+                    AppSettings = appSettings,
+                    CompanyId = AppConstants.CompanyId
+                };
+                _commandBus.Send(command);
+            }
+
+            TempData["Info"] = "Settings updated";
+            TempData["CompanySetting"] = form;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
