41459005f131e59ed6b4472c49bab832102707d7
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-01-28 16:58:13 -0500

    Added  missing file

---
+++ Src/Server/MK.Booking/MK.Booking.Commands/UnlinkAllPayPalAccounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure.Messaging;
+
+namespace apcurium.MK.Booking.Commands
+{
+    public class UnlinkAllPayPalAccounts : ICommand
+    {
+        public UnlinkAllPayPalAccounts()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid[] AccountIds { get; set; }
+
+        public Guid Id { get; private set; }
+    }
+}
