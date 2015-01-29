ca671667e7fa9ea9630d9efa7b951035c7f24fdb
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-01-29 14:44:25 -0500

    Enum for PayPal values

---
+++ Src/Server/MK.Common/Enumeration/PayPal/PaymentStates.cs
@@ -0,0 +1,20 @@
+﻿
+namespace apcurium.MK.Common.Enumeration.PayPal
+{
+    public static class PaymentStates
+    {
+        public const string Approved = "approved";
+
+        public const string Created = "created";
+
+        public const string Pending = "pending";
+
+        public const string Completed = "completed";
+
+        public const string Failed = "failed";
+
+        public const string Canceled = "canceled";
+
+        public const string Expired = "expired";
+    }
+}
