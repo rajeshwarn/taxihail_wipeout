7dd3764d52078493057e407019789c0631ec7fba
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-02-03 15:40:45 -0500

    Fix for unpair button being visible when paypal was enabled

--- Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
+++ Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
@@ -20,6 +20,7 @@ using apcurium.MK.Common.Extensions;
 using ServiceStack.Text;
 using apcurium.MK.Booking.Maps;
 using System.Net;
+using apcurium.MK.Common.Enumeration;
 using ServiceStack.ServiceClient.Web;
 
 namespace apcurium.MK.Booking.Mobile.ViewModels
@@ -516,7 +517,8 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
             IsCancelButtonVisible = _bookingService.IsOrderCancellable(statusId);
 
 			// Unpair button is only available for RideLinqCMT
-			if (paymentSettings.PaymentMode == PaymentMethod.RideLinqCmt) 
+			if (paymentSettings.PaymentMode == PaymentMethod.RideLinqCmt
+                && _accountService.CurrentAccount.Settings.ChargeTypeId == ChargeTypes.CardOnFile.Id) 
 			{
 				IsUnpairButtonVisible = await _bookingService.IsPaired(Order.Id);
 			} 
