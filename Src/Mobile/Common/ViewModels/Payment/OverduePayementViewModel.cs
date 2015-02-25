c70d843ee092e4d5cf43662a32258a7289c53e24
Dominique Savoie <dominique.savoie@apcurium.com>
2015-02-25 11:18:27 -0500

    MKTAXI-2492 => Added stup ViewModel for OverduePayement.

---
+++ Src/Mobile/Common/ViewModels/Payment/OverduePayementViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using apcurium.MK.Booking.Mobile.AppServices;
+using System.Windows.Input;
+using apcurium.MK.Booking.Mobile.Extensions;
+
+namespace apcurium.MK.Booking.Mobile.ViewModels.Payment
+{
+	public class OverduePayementViewModel : PageViewModel
+	{
+		private IAccountService _accountService;
+
+		public OverduePayementViewModel(IAccountService accountService)
+		{
+			_accountService = accountService;
+		}
+
+		public override void OnViewStarted(bool firstTime)
+		{
+			base.OnViewStarted(firstTime);
+
+		}
+
+
+		public string TransactionNumber
+		{
+			get { return "AVX1212121"; }
+			set;
+		}
+
+		public DateTime DateOfRide
+		{
+			get;
+			set;
+		}
+
+		public double Amount
+		{
+			get;
+			set;
+		}
+
+		public ICommand Retry
+		{
+			get
+			{
+				return this.GetCommand(() => { });
+			}
+		}
+
+		public ICommand AddNewCard
+		{
+			get
+			{
+				return this.GetCommand(() => { });
+			}
+		}
+	}
+}
+
