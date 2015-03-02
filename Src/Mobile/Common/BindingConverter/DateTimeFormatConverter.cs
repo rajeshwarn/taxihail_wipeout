8dcfa949e1e085a5dd4a3f8ae9311433186f0b49
Dominique Savoie <dominique.savoie@apcurium.com>
2015-03-02 14:22:06 -0500

    MKTAXI-2504 => Added Converters for iOS.

--- Src/Mobile/Common/BindingConverter/DateTimeFormatConverter.cs
+++ Src/Mobile/Common/BindingConverter/DateTimeFormatConverter.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Cirrious.CrossCore.Converters;
 
 namespace apcurium.MK.Booking.Mobile.BindingConverter
