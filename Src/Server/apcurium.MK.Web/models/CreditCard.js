1c2e6962df69e4c803ffb8ceac2184ef318d70b8
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-03-11 09:36:22 -0400

    MKTAXI-2526: New files for payment screen

---
+++ Src/Server/apcurium.MK.Web/models/CreditCard.js
@@ -0,0 +1,52 @@
+﻿(function () {
+
+    TaxiHail.CreditCard = Backbone.Model.extend({
+
+        urlRoot: TaxiHail.parameters.apiRoot + "/account/creditcards",
+
+        updateCreditCard: function () {
+            var updatedCreditCard = this;
+
+            return $.ajax({
+                type: 'POST',
+                url: 'api/account/creditcards',
+                data: updatedCreditCard,
+                dataType: 'json'
+            });
+        },
+
+        deleteCreditCard: function() {
+            return $.ajax({
+                type: 'DELETE',
+                url: 'api/account/creditcards',
+                dataType: 'json'
+            });
+        },
+
+        determineCompany: function(cardNumber) {
+            //var visaPattern = new RegExp("/^4\d{3}-?\d{4}-?\d{4}-?\d");
+            //var mastercardPattern = new RegExp("^5[1-5][0-9]{14}$");
+            //var amexPattern = new RegExp("/^3[47]\d{13}$/");
+
+            var patterns = {
+                visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+                electron: /^(4026|417500|4405|4508|4844|4913|4917)\d+$/,
+                mastercard: /^5[1-5][0-9]{14}$/,
+                amex: /^3[47]\d{13}$/
+            };
+
+            if (patterns.visa.test(cardNumber)) {
+                return { id: 0, display: "Visa" };
+            } else if (patterns.mastercard.test(cardNumber)) {
+                return { id: 1, display: "MasterCard" };
+            } else if (patterns.amex.test(cardNumber)) {
+                return { id: 2, display: "Amex" };
+            } else if (patterns.electron.test(cardNumber)) {
+                return { id: 3, display: "Visa Electron" };
+            } else {
+                return { id: 4, display: "Credit Card Generic" };
+            }
+        }
+    });
+
+}());
