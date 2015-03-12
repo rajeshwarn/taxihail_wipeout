4222815e80f3540d869ef5617b78f0eafaed9205
Dominique Savoie <dominique.savoie@apcurium.com>
2015-03-12 15:29:14 -0400

    MKTAXI-2508 => Changed the promotion to use meter amount instead of totalOrderAmount.

--- Src/Server/MK.Booking.Api/Jobs/OrderStatusUpdater.cs
+++ Src/Server/MK.Booking.Api/Jobs/OrderStatusUpdater.cs
@@ -384,7 +384,7 @@ namespace apcurium.MK.Booking.Api.Jobs
                 if (promoUsed != null)
                 {
                     var promoDomainObject = _promoRepository.Get(promoUsed.PromoId);
-                    amountSaved = promoDomainObject.GetAmountSaved(totalOrderAmount);
+                    amountSaved = promoDomainObject.GetAmountSaved(Convert.ToDecimal(meterAmount));
                     totalOrderAmount = totalOrderAmount - amountSaved;
                 }
                 
