ed31bc9630952879fc5d7a776f3578fcc8dcc4db
Dominique Savoie <dominique.savoie@apcurium.com>
2015-03-24 10:37:03 -0400

    MKTAXI-2453 => Added OrderStatusChanged handler to automatically send a receipt for pay in car when the settings allow it.

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/MailSender.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/MailSender.cs
@@ -22,20 +22,24 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
 {
     public class MailSender : IIntegrationEventHandler,
         IEventHandler<PayPalExpressCheckoutPaymentCompleted>,
-        IEventHandler<CreditCardPaymentCaptured>
+        IEventHandler<CreditCardPaymentCaptured>,
+        IEventHandler<OrderStatusChanged>
     {
         private readonly ICommandBus _commandBus;
         private readonly Func<BookingDbContext> _contextFactory;
         private readonly ICreditCardDao _creditCardDao;
+        private readonly IServerSettings _serverSettings;
 
         public MailSender(Func<BookingDbContext> contextFactory,
             ICommandBus commandBus,
-            ICreditCardDao creditCardDao
+            ICreditCardDao creditCardDao,
+            IServerSettings serverSettings
             )
         {
             _contextFactory = contextFactory;
             _commandBus = commandBus;
             _creditCardDao = creditCardDao;
+            _serverSettings = serverSettings;
         }
 
         public void Handle(CreditCardPaymentCaptured @event)
@@ -72,15 +76,42 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
                         card = _creditCardDao.FindByToken(orderPayment.CardToken);
                     }
 
-                    if (orderPayment != null)
-                    {
-                        var command = SendReceiptCommandBuilder.GetSendReceiptCommand(order, account,
-                            orderStatus.VehicleNumber, orderStatus.DriverInfos,
-                            Convert.ToDouble(orderPayment.Meter), 0, Convert.ToDouble(orderPayment.Tip), 0, orderPayment,
-                            card);
-                        
-                        _commandBus.Send(command);
-                    }
+                    var command = orderPayment != null
+                        ? GetSendReceiptCommandForCreditCard(order, account, orderStatus, orderPayment, card)
+                        : GetSendReceiptCommandForPayInCar(order, account, orderStatus);
+
+                    _commandBus.Send(command);
+                }
+            }
+        }
+
+        private static SendReceipt GetSendReceiptCommandForPayInCar(OrderDetail order, AccountDetail account, OrderStatusDetail orderStatus)
+        {
+            return SendReceiptCommandBuilder.GetSendReceiptCommand(order, account, orderStatus.VehicleNumber, orderStatus.DriverInfos, 0, 0, 0, 0);
+        }
+
+        private static SendReceipt GetSendReceiptCommandForCreditCard(OrderDetail order, AccountDetail account, OrderStatusDetail orderStatus, OrderPaymentDetail orderPayment, CreditCardDetails card)
+        {
+            return SendReceiptCommandBuilder.GetSendReceiptCommand(
+                order, 
+                account,
+                orderStatus.VehicleNumber, 
+                orderStatus.DriverInfos,
+                Convert.ToDouble(orderPayment.Meter), 
+                0,
+                Convert.ToDouble(orderPayment.Tip), 
+                0, 
+                orderPayment,
+                card);
+        }
+
+        public void Handle(OrderStatusChanged @event)
+        {
+            if (@event.IsCompleted)
+            {
+                if (_serverSettings.ServerData.SendReceiptForPayInCar)
+                {
+                    SendReceipt(@event.SourceId);
                 }
             }
         }
