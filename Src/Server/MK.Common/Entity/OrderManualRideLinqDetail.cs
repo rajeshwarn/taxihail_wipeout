6f0752358ac635f59c0c3b75d3228b650cd62b8b
Dominique Savoie <dominique.savoie@apcurium.com>
2015-04-10 15:54:27 -0400

    MKTAXI-2578 => Fixed merge error

--- Src/Server/MK.Common/Entity/OrderManualRideLinqDetail.cs
+++ Src/Server/MK.Common/Entity/OrderManualRideLinqDetail.cs
@@ -18,8 +18,6 @@ namespace apcurium.MK.Common.Entity
 
         public DateTime? EndTime { get; set; }
 
-		public string DriverId { get; set; }
-
         public bool IsCancelled { get; set; }
 
         public double? Distance { get; set; }
