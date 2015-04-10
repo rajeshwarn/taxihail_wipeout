a0c5ccd0983e1d2720aea01f2fd77775d29ce5c0
Dominique Savoie <dominique.savoie@apcurium.com>
2015-04-10 16:06:47 -0400

    MKTAXI-2578 => Update for android strings + added summary view.

--- Src/Mobile/Android/Activities/ManualRideLinqSummaryActivity.cs
+++ Src/Mobile/Android/Activities/ManualRideLinqSummaryActivity.cs
@@ -5,6 +5,7 @@ using System.Text;
 using apcurium.MK.Booking.Mobile.ViewModels;
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -13,19 +14,18 @@ using Android.Widget;
 
 namespace apcurium.MK.Booking.Mobile.Client.Activities
 {
-    public class ManualRideLinqStatusActivity : BaseBindingActivity<ManualRideLinqStatusViewModel>
+    [Activity(Theme = "@style/MainTheme",
+        Label = "ManualRideLinqSummaryActivity",
+        ScreenOrientation = ScreenOrientation.Portrait
+      )]  
+    public class ManualRideLinqSummaryActivity : BaseBindingActivity<ManualRideLinqSummaryViewModel>
     {
         protected override void OnViewModelSet()
         {
             base.OnViewModelSet();
 
             SetContentView(Resource.Layout.View_ManualRideLinqSummary);
-
-            var im = (InputMethodManager)GetSystemService(InputMethodService);
-            if (im != null)
-            {
-                im.ToggleSoftInput(ShowFlags.Forced, HideSoftInputFlags.None);
-            }
         }
+
     }
 }
