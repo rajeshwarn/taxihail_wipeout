4c25cd91cf0cb945a3c773655334cbe40c51e60e
Alex Proteau <alex.proteau@apcurium.com>
2015-04-10 15:52:53 -0400

    Testing

--- Src/Server/DatabaseInitializer/Services/EventsPlayBackService.cs
+++ Src/Server/DatabaseInitializer/Services/EventsPlayBackService.cs
@@ -9,6 +9,7 @@ using Infrastructure.Messaging;
 using Infrastructure.Serialization;
 using Infrastructure.Sql.EventSourcing;
 using ServiceStack.Text;
+using System.Diagnostics;
 
 #endregion
 
@@ -36,21 +37,33 @@ namespace DatabaseInitializer.Services
             }
         }
 
+        public class CallStats
+        {
+            public long Count { get; set; }
+            public long TotalTime { get; set; }
+
+            public long LastTime { get; set; }
+        }
+
         public void ReplayAllEvents(DateTime? after = null)
         {
             var skip = 0;
             var hasMore = true;
-            const int pageSize = 5000;
+            const int pageSize = 100000;
             after = after ?? DateTime.MinValue;
+
+            var stats = new Dictionary<string, CallStats>();
 
             Console.WriteLine("Replaying event since {0}", after);
 
             while(hasMore)
             {
                 List<Event> events;
+
+                Console.WriteLine("Getting next events...");
                 using (var context = _contextFactory.Invoke())
                 {
-                    context.Database.CommandTimeout = 2000;
+                    context.Database.CommandTimeout = 0;
                     // order by date then by version in case two events happened at the same time
                     events = context.Set<Event>()
                                     .OrderBy(x => x.EventDate)
@@ -61,6 +74,8 @@ namespace DatabaseInitializer.Services
                                     .ToList();
                 }
 
+                Console.WriteLine("Done getting next events...");
+
                 if (events.Count == 0)
                 {
                     Console.WriteLine("No event to be replayed");
@@ -70,17 +85,53 @@ namespace DatabaseInitializer.Services
                 hasMore = events.Count == pageSize;
                 Console.WriteLine("Number of events played: " + (hasMore ? skip : (skip + events.Count)));
                 skip += pageSize;
-                
+
+
+
+                var sw = Stopwatch.StartNew();
+                long start = 0;
+                long end = sw.ElapsedMilliseconds;
+
+
                 if (events.Any())
                 {
+                    int eCount = 0;
                     foreach (var @event in events)
                     {
                         try
                         {
-                            _eventBus.Publish(new Envelope<IEvent>(Deserialize(@event))
+                            var ev = Deserialize(@event);
+
+                            _eventBus.Publish(new Envelope<IEvent>(ev)
                             {
                                 CorrelationId = @event.CorrelationId
                             });
+
+                            start = end;
+                            end = sw.ElapsedMilliseconds;
+                            
+
+                            if ( !stats.ContainsKey(ev.GetType().FullName ))
+                            {
+                                stats.Add(ev.GetType().FullName, new CallStats());
+                            }
+                            stats[ev.GetType().FullName].Count++;
+                            stats[ev.GetType().FullName].TotalTime += end - start;
+                            stats[ev.GetType().FullName].LastTime = end - start;
+
+                            
+
+                            eCount++;
+
+                            if ( eCount % 5000 == 0)
+                            {
+                                PrintStats(stats);
+                            }
+                            
+                            start = end;
+                            end = sw.ElapsedMilliseconds;
+
+                            //Console.Write("Playing event : " + eCount);
                         }
                         catch
                         {
@@ -96,6 +147,16 @@ namespace DatabaseInitializer.Services
                 
             }
             
+        }
+
+        private void PrintStats(Dictionary<string, CallStats> stats)
+        {
+            Console.WriteLine("*****Printing stats*****");
+            foreach (var key in stats.Keys)
+            {
+                Console.WriteLine(string.Format("For event {0}, average excecution {1}ms, last call : {2}ms, total count : {3} ", key, stats[key].TotalTime / stats[key].Count, stats[key].LastTime, stats[key].Count));
+            }
+            Console.WriteLine("************************");
         }
 
         private IVersionedEvent Deserialize(Event @event)
