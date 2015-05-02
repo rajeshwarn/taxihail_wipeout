6f1797c9c361662d743a8ce3b22a0e3f6c841db2
Dominique Savoie <dominique.savoie@apcurium.com>
2015-05-02 10:54:40 -0400

    MKTAXI-2608 => Removed unused using statements

--- Src/Mobile/Common/ViewModels/PromotionViewModel.cs
+++ Src/Mobile/Common/ViewModels/PromotionViewModel.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 using apcurium.MK.Booking.Api.Contract.Resources;
