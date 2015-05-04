db4c9212f6acfd36ba7a14b95e10f47790cd557b
Dominique Savoie <dominique.savoie.1@gmail.com>
2015-05-04 18:51:13 -0400

    Backout changeset 084a308872b6ead694dcfd67a6ae20a1b76a413b

--- Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
+++ Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
@@ -699,7 +699,7 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 								this.Services().Message.ShowMessage(this.Services().Localize["CmtRideLinqErrorTitle"], this.Services().Localize["UnpairErrorMessage"]);
 							}
 						},
-						this.Services().Localize["NoButton"], () => { });
+						this.Services().Localize["Cancel"], () => {});
 				});
 			}
 		}
