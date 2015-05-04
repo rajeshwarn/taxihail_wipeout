3d050b0e3f301b8ce317cfb1e23b1dee4d5334f6
Dominique Savoie <dominique.savoie.1@gmail.com>
2015-05-04 18:46:17 -0400

    MKTAXI-2607 => Changed dialog unpair warning dialog response to Yes, No (from Pay In Car, Cancel).

--- Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
+++ Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
@@ -699,7 +699,7 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 								this.Services().Message.ShowMessage(this.Services().Localize["CmtRideLinqErrorTitle"], this.Services().Localize["UnpairErrorMessage"]);
 							}
 						},
-						this.Services().Localize["Cancel"], () => {});
+						this.Services().Localize["NoButton"], () => { });
 				});
 			}
 		}
