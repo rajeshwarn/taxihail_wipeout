1ed12f3175d15574e48038ed1e19c72c97aec9e3
Dominique Savoie <dominique.savoie@apcurium.com>
2015-05-04 15:27:21 -0400

    MKTAXI-2668 => Added new setting to allow the merged booking flow during classic (no manual ridelinq) mode.

--- Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
+++ Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
@@ -130,6 +130,10 @@ namespace MK.Common.Configuration
         [Display(Name = "Twitter Consumer Key", Description="Twitter API settings")]
 		public string TwitterConsumerKey{ get; protected set; }
 
+        [SendToClient, CustomizableByCompany]
+        [Display(Name = "Use merged book taxi button", Description = "Replace book now and book later buttons with a single button that will display both options in a popup. (This feature does not work with manual CMT ridelinq pairing.)")]
+        public bool IsMergedBookTaxiButtonEnabled { get; set; }
+
         [RequiredAtStartup, SendToClient, CustomizableByCompany]
         [Display(Name = "Twitter Consumer Secret", Description = "Twitter API settings")]
         public string TwitterConsumerSecret { get; protected set; }
