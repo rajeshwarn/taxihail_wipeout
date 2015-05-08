0ee1308af64ed48403d4697a15d0e33bc0cf7f1f
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-05-08 13:30:05 -0400

    Better handling of HoneyBadger exceptions

--- Src/Server/HoneyBadger.Test/AvailabilityFixture.cs
+++ Src/Server/HoneyBadger.Test/AvailabilityFixture.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
 using apcurium.MK.Booking.Test;
+using apcurium.MK.Common.Diagnostic;
 
 namespace HoneyBadger.Test
 {
@@ -10,7 +12,7 @@ namespace HoneyBadger.Test
         [SetUp]
         public void Setup()
         {
-            _sut = new HoneyBadgerServiceClient(new TestServerSettings());
+            _sut = new HoneyBadgerServiceClient(new TestServerSettings(), new Logger());
         }
 
         private HoneyBadgerServiceClient _sut;
