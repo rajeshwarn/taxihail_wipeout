9f5118d24c09dfcbf8256c5f6cc548a0583ac7a5
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-05-21 15:56:17 -0400

    Fix push missing info in resource

--- Src/Server/MK.Booking/MK.Booking/Services/Impl/NotificationService.cs
+++ Src/Server/MK.Booking/MK.Booking/Services/Impl/NotificationService.cs
@@ -41,6 +41,7 @@ namespace apcurium.MK.Booking.Services.Impl
         private readonly IConfigurationDao _configurationDao;
         private readonly IOrderDao _orderDao;
         private readonly IAccountDao _accountDao;
+        private readonly ICreditCardDao _creditCardDao;
         private readonly IStaticMap _staticMap;
         private readonly ISmsService _smsService;
         private readonly IGeocoding _geocoding;
@@ -58,6 +59,7 @@ namespace apcurium.MK.Booking.Services.Impl
             IConfigurationDao configurationDao,
             IOrderDao orderDao,
             IAccountDao accountDao,
+            ICreditCardDao creditCardDao,
             IStaticMap staticMap,
             ISmsService smsService,
             IGeocoding geocoding,
@@ -71,6 +73,7 @@ namespace apcurium.MK.Booking.Services.Impl
             _configurationDao = configurationDao;
             _orderDao = orderDao;
             _accountDao = accountDao;
+            _creditCardDao = creditCardDao;
             _staticMap = staticMap;
             _smsService = smsService;
             _geocoding = geocoding;
@@ -276,11 +279,14 @@ namespace apcurium.MK.Booking.Services.Impl
                 }
                 else
                 {
+                    var card = _creditCardDao.FindByAccountId(order.AccountId).First();
+                    var last4Digits = card.Last4Digits;
                     successMessage = string.Format(
                         isAutomaticPairingEnabled
                             ? _resources.Get("PushNotification_OrderPairingSuccessfulUnpair", order.ClientLanguageCode)
                             : _resources.Get("PushNotification_OrderPairingSuccessful", order.ClientLanguageCode),
                         order.IBSOrderId,
+                        last4Digits,
                         autoTipPercentage);
                 }
                 
