13ccab49257fb5ff202bebf6f1272ca9173cfae4
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-05-25 12:01:33 -0400

    MKTAXI-2755: Ability to define market fees via web admin portal

--- Src/Server/MK.Booking/MK.Booking/Database/BookingDbContext.cs
+++ Src/Server/MK.Booking/MK.Booking/Database/BookingDbContext.cs
@@ -67,6 +67,7 @@ namespace apcurium.MK.Booking.Database
             modelBuilder.Entity<OrderReportDetail>().ToTable("OrderReportDetail", SchemaName);
             modelBuilder.Entity<OrderNotificationDetail>().ToTable("OrderNotificationDetail", SchemaName);
             modelBuilder.Entity<OverduePaymentDetail>().ToTable("OverduePaymentDetail", SchemaName);
+            modelBuilder.Entity<FeesDetail>().ToTable("FeesDetail", SchemaName);
 
             modelBuilder.Entity<AccountChargeQuestion>().ToTable("AccountChargeQuestion", SchemaName);
             modelBuilder.Entity<AccountChargeDetail>().ToTable("AccountChargeDetail", SchemaName)
