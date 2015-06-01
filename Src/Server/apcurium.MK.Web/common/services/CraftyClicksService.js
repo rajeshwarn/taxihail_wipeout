ff2726d52b91d08a8e17e063e862ec1e2efd002b
YaoMax <maximvlassov@gmail.com>
2015-06-01 11:13:11 -0400

    MKTAXI-2528 web service added;

---
+++ Src/Server/apcurium.MK.Web/common/services/CraftyClicksService.js
@@ -0,0 +1,27 @@
+﻿// Crafty clicks address service
+
+(function () {
+
+    TaxiHail.craftyclicks = {
+
+        getCraftyClicksAdresses: function (postcode) {
+
+            if (TaxiHail.parameters.craftyclicksapikey)
+            {
+                var defer = $.Deferred();
+
+                $.ajax({
+                    url: "http://pcls1.craftyclicks.co.uk/json/",
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    data: JSON.stringify({ postcode: postcode, key: TaxiHail.parameters.craftyclicksapikey, response: "data_formatted", sort: "asc", include_geocode: true })
+                }).then(defer.resolve, defer.reject);
+
+                return defer.promise();
+            }
+
+            return null;
+        }
+    };
+}());
