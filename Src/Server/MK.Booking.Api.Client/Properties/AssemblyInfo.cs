b3bced951e4018cfc7a5f802af9665d761df0a7a
Brian Ricard <brian.ricard@apcurium.com>
2015-06-12 14:40:32 -0400

    Version bump - 2.2.7

--- Src/Server/MK.Booking.Api.Client/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api.Client/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.6.2")]
-[assembly: AssemblyVersion("2.2.6.2")]
-[assembly: AssemblyFileVersion("2.2.6.2")]
+// [assembly: AssemblyVersion("2.2.7")]
+[assembly: AssemblyVersion("2.2.7")]
+[assembly: AssemblyFileVersion("2.2.7")]
 
