3cb34920da246169edc35c2337013cacd078049f
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-06-12 11:55:35 -0400

    MKTAXI-2772: New service to contact the driver

---
+++ Src/Server/MK.Booking.Api/Services/DriverService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using apcurium.MK.Booking.Api.Contract.Requests;
+using apcurium.MK.Booking.EventHandlers.Integration;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+
+namespace apcurium.MK.Booking.Api.Services
+{
+    public class DriverService : Service
+    {
+        private readonly IIbsOrderService _ibsOrderService;
+
+        public DriverService(IIbsOrderService ibsOrderService)
+        {
+            _ibsOrderService = ibsOrderService;
+        }
+
+        public object Post(SendMessageToDriverRequest request)
+        {
+            try
+            {
+                _ibsOrderService.SendMessageToDriver(request.Message, request.VehicleNumber);
+            }
+            catch (Exception ex)
+            {
+                throw new HttpError(HttpStatusCode.InternalServerError, ex.Message);
+            }
+            
+            return new HttpResult(HttpStatusCode.OK);
+        }
+    }
+}
