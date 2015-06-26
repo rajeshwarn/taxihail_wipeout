6929917d5758bedbee3ac1ad0a9f69f62e304af2
Brian Ricard <brian.ricard@apcurium.com>
2015-06-26 11:06:46 -0400

    Version bump - 2.2.13

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.11")]
-[assembly: AssemblyVersion("2.2.11")]
-[assembly: AssemblyFileVersion("2.2.11")]
+// [assembly: AssemblyVersion("2.2.13")]
+[assembly: AssemblyVersion("2.2.13")]
+[assembly: AssemblyFileVersion("2.2.13")]
 
