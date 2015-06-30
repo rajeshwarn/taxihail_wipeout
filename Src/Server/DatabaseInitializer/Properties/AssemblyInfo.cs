bc9b694e07b0e096cc564750e520ebc97d118719
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-06-30 16:56:26 -0400

    Version bump to 2.2.91

--- Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
+++ Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.9.1")]
-[assembly: AssemblyVersion("2.2.9.1")]
-[assembly: AssemblyFileVersion("2.2.9.1")]
+// [assembly: AssemblyVersion("2.2.91")]
+[assembly: AssemblyVersion("2.2.91")]
+[assembly: AssemblyFileVersion("2.2.91")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
