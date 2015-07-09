1b8f3e9def7614dd7de2485985367226f5f6b6a7
Dominique Savoie <dominique.savoie@apcurium.com>
2015-07-09 11:45:28 -0400

    version bump to 1.5.276

--- Src/Server/apcurium.MK.Web.Tests/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web.Tests/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.275")]
-[assembly: AssemblyVersion("1.5.275")]
-[assembly: AssemblyFileVersion("1.5.275")]
+// [assembly: AssemblyVersion("1.5.276")]
+[assembly: AssemblyVersion("1.5.276")]
+[assembly: AssemblyFileVersion("1.5.276")]
