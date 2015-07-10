8b13e9cbacd4acb3c0d2c35d1917cfe352439a97
Dominique Savoie <dominique.savoie@apcurium.com>
2015-07-10 13:56:03 -0400

    Fixed Server build.

---
+++ Src/Server/HoneyBadger/Responses/CmtGeoContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyBadger.Responses
+{
+    public class CmtGeoContent : HoneyBadgerContent
+    {
+        public int ETASeconds { get; set; }
+    }
+}
