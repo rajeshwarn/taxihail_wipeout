84ddae2986eaed3eca21602de330c59c2ae9c95e
Maxim Vlassov <maxim.vlasov@apcurium.com>
2015-07-13 10:59:11 -0400

    mktaxi-2869: style of notes to driver changed for ios;

--- Src/Mobile/iOS/Views/Order/OrderReviewView.cs
+++ Src/Mobile/iOS/Views/Order/OrderReviewView.cs
@@ -28,7 +28,7 @@ namespace apcurium.MK.Booking.Mobile.Client.Views.Order
             BackgroundColor = UIColor.Clear;
 
             txtNote.BackgroundColor = UIColor.FromRGB(242, 242, 242);
-            txtNote.Font = UIFont.FromName(FontName.HelveticaNeueLight, 18f);
+            txtNote.Font = UIFont.FromName(FontName.HelveticaNeueBold, 18f);
             txtNote.Placeholder = Localize.GetValue("NotesToDriveLabel");
 	        txtNote.PlaceholderColor = UIColor.FromRGB(75, 75, 75);
             txtNote.ShowCloseButtonOnKeyboard();
