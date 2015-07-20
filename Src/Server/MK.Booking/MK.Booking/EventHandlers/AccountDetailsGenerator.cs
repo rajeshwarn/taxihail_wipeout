84e99702976b4ac457ac7429cce46a736aaa1282
maxim vlasov <maximvlasov@apcurium.com>
2015-07-20 16:12:46 -0400

    mktaxi-2963: update account gets default company's code when countrycode is null during events replay;

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/AccountDetailsGenerator.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/AccountDetailsGenerator.cs
@@ -119,18 +119,7 @@ namespace apcurium.MK.Booking.EventHandlers
                 if (@event.Country == null || (@event.Country != null && string.IsNullOrEmpty(@event.Country.Code)))
                 {
                     var currentCultureInfo = CultureInfo.GetCultureInfo(_serverSettings.ServerData.PriceFormat);
-
-                    string countryCode;
-
-                    if (currentCultureInfo != null)
-                    {
-                        countryCode = (new RegionInfo(currentCultureInfo.LCID)).TwoLetterISORegionName;
-                    }
-                    else
-                    {
-                        countryCode = "CA";
-                    }
-
+                    string countryCode = (new RegionInfo(currentCultureInfo.LCID)).TwoLetterISORegionName;
                     @event.Country = CountryCode.GetCountryCodeByIndex(CountryCode.GetCountryCodeIndexByCountryISOCode(countryCode)).CountryISOCode;
                 }
 
@@ -201,6 +190,13 @@ namespace apcurium.MK.Booking.EventHandlers
                 if (settings.ProviderId == _serverSettings.ServerData.DefaultBookingSettings.ProviderId)
                 {
                     settings.ProviderId = null;
+                }
+
+                if (@event.Country == null || (@event.Country != null && string.IsNullOrEmpty(@event.Country.Code)))
+                {
+                    var currentCultureInfo = CultureInfo.GetCultureInfo(_serverSettings.ServerData.PriceFormat);
+                    string countryCode = (new RegionInfo(currentCultureInfo.LCID)).TwoLetterISORegionName;
+                    @event.Country = CountryCode.GetCountryCodeByIndex(CountryCode.GetCountryCodeIndexByCountryISOCode(countryCode)).CountryISOCode;
                 }
 
                 settings.NumberOfTaxi = @event.NumberOfTaxi;
