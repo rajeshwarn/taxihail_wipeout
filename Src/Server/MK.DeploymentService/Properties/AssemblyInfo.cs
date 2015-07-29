9a2fce65bee59d1f9d5e8b69581cdce75d9537e9
Brian Ricard <brian.ricard@apcurium.com>
2015-07-29 13:19:50 -0400

    Version bump - 2.2.24

--- Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
+++ Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.23")]
-[assembly: AssemblyVersion("2.2.23")]
-[assembly: AssemblyFileVersion("2.2.23")]
+// [assembly: AssemblyVersion("2.2.24")]
+[assembly: AssemblyVersion("2.2.24")]
+[assembly: AssemblyFileVersion("2.2.24")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
--- Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.23")]
-[assembly: AssemblyVersion("2.2.23")]
-[assembly: AssemblyFileVersion("2.2.23")]
+// [assembly: AssemblyVersion("2.2.24")]
+[assembly: AssemblyVersion("2.2.24")]
+[assembly: AssemblyFileVersion("2.2.24")]
 
 
 
