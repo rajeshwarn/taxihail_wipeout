68105af86b77a013dc77e01f02cb1e3e7f5b740a
Brian Ricard <brian.ricard@apcurium.com>
2015-07-30 08:36:25 -0400

    Version bump - 2.2.25

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.24")]
-[assembly: AssemblyVersion("2.2.24")]
-[assembly: AssemblyFileVersion("2.2.24")]
+// [assembly: AssemblyVersion("2.2.25")]
+[assembly: AssemblyVersion("2.2.25")]
+[assembly: AssemblyFileVersion("2.2.25")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.2.24")]
-[assembly: AssemblyVersion("2.2.24")]
-[assembly: AssemblyFileVersion("2.2.24")]
+// [assembly: AssemblyVersion("2.2.25")]
+[assembly: AssemblyVersion("2.2.25")]
+[assembly: AssemblyFileVersion("2.2.25")]
 
