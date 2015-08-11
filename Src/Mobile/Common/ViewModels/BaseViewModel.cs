70e940f885ce7957d0c3c63bd0becd860e8dd8e3
Dominique.Savoie <dominique.savoie@apcurium.com>
2015-08-11 13:45:10 -0400

    MKTAXI-2726 => ShowViewModelAndRemoveFromHistory now has it's parameter optional

--- Src/Mobile/Common/ViewModels/BaseViewModel.cs
+++ Src/Mobile/Common/ViewModels/BaseViewModel.cs
@@ -161,7 +161,7 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
             return ShowViewModel<TViewModel>(parameterValues);
         }
 
-        protected void ShowViewModelAndRemoveFromHistory<TViewModel>(object parameter) where TViewModel : IMvxViewModel
+        protected void ShowViewModelAndRemoveFromHistory<TViewModel>(object parameter = null) where TViewModel : IMvxViewModel
         {
             var dictionary = parameter.ToSimplePropertyDictionary();
             dictionary = dictionary ?? new Dictionary<string,string>();
