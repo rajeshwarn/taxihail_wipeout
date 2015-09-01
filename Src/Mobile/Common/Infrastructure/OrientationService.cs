49e675e7de0db0fc2ae35d3e006bc4206337b810
Maxim Vlassov <maxim.vlasov@apcurium.com>
2015-09-01 14:25:33 -0400

    mktaxi-3060: display turn for android #4;

--- Src/Mobile/Common/Infrastructure/OrientationService.cs
+++ Src/Mobile/Common/Infrastructure/OrientationService.cs
@@ -2,13 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 
 namespace apcurium.MK.Booking.Mobile.AppServices.Impl
