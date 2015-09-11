a54d5a4321c8b256d9df1b09b8c2f6ae58d6eea4
Dominique Savoie <dominique.savoie@apcurium.com>
2015-09-11 16:05:33 -0400

    MKTAXI-3122 => Added Call to TaxiHailServer from Mobile.

---
+++ Src/Server/MK.Booking.Api.Client/TaxiHail/FlightInformationServiceClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using apcurium.MK.Booking.Api.Contract.Requests;
+using apcurium.MK.Booking.Api.Contract.Resources;
+using CMTPayment.Extensions;
+
+namespace apcurium.MK.Booking.Api.Client.TaxiHail
+{
+	public class FlightInformationServiceClient : BaseServiceClient
+	{
+
+		public Task<FlightInformation> GetTerminal(FlightInformationRequest request)
+		{
+			return Client.PostAsync(request);
+		}
+	}
+}
