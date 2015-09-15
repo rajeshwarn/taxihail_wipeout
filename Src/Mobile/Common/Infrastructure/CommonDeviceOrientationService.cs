757c917b40a3a984475017436efea9738ca41aa8
maxim vlasov <maximvlasov@apcurium.com>
2015-09-15 09:27:09 -0400

    mktaxi-3060:android orientation service filter added;

--- Src/Mobile/Common/Infrastructure/CommonDeviceOrientationService.cs
+++ Src/Mobile/Common/Infrastructure/CommonDeviceOrientationService.cs
@@ -5,13 +5,157 @@ using System.Text;
 
 namespace apcurium.MK.Booking.Mobile.Infrastructure
 {
+	public enum CoordinateSystemOrientation
+	{
+		LeftHanded,
+		RightHanded
+	}
+
 	public abstract class CommonDeviceOrientationService
 	{
+		public struct Vector3
+		{
+			public double x, y, z;
+
+			public Vector3(double x, double y, double z)
+			{
+				this.x = x;
+				this.y = y;
+				this.z = z;
+			}
+
+			public void Normalize()
+			{
+				double vectorLength = Math.Pow(x * x + y * y + z * z, 0.5);
+
+				x /= vectorLength;
+				y /= vectorLength;
+				z /= vectorLength;
+			}
+		}
+
+		class Filter
+		{
+			struct FilterValue
+			{
+				public int value;
+				public long timestamp;
+			}
+
+			FilterValue[] _buffer = new FilterValue[1000];
+			int _pointer = -1;
+			int _bufferLength = 0;
+			const int MaximumRandomDeviation = 10;
+
+			public void AddValue(int value, long time)
+			{
+				_pointer++;
+				if (_pointer == 1000)
+					_pointer = 0;
+
+				_buffer[_pointer].value = value;
+				_buffer[_pointer].timestamp = time;
+
+				if (_bufferLength < 1000)
+					_bufferLength++;
+			}
+
+			FilterValue ReadValueFromEnd(int position)
+			{
+				while (position >= _bufferLength)
+				{
+					position -= _bufferLength;
+				}
+
+				while (position < 0)
+				{
+					position += _bufferLength;
+				}
+
+				int positionFromBufferStartPointer = _pointer - position;
+
+				if (positionFromBufferStartPointer < 0)
+					positionFromBufferStartPointer += _bufferLength;
+
+				return _buffer[positionFromBufferStartPointer];
+			}
+
+			public int StatisticalFilter()
+			{
+				if (_bufferLength == 0)
+					return -1;
+
+				bool timeAchieved = false;
+				long time = 0;
+
+				FilterValue fv1 = ReadValueFromEnd(0);
+				FilterValue fv2 = ReadValueFromEnd(0);
+
+				int maximumValueDifference = 0;
+
+				for (int i = 0; i < _bufferLength; i++)
+				{
+					fv2 = ReadValueFromEnd(i);
+
+					time += fv1.timestamp - fv2.timestamp;
+
+					int valueDifference = Math.Abs(fv2.value - fv1.value);
+
+					if (valueDifference > 180)
+						valueDifference = 360 - valueDifference;
+
+					maximumValueDifference = Math.Max(maximumValueDifference, valueDifference);
+
+					fv1 = fv2;
+
+					if (time > 250)
+					{
+						timeAchieved = true;
+						break;
+					}
+				}
+
+				if (!timeAchieved)
+				{
+					fv1 = ReadValueFromEnd(0);
+					fv2 = ReadValueFromEnd(0);
+
+					for (int i = 0; i < Math.Min(_bufferLength, 5); i++)
+					{
+						fv2 = ReadValueFromEnd(i);
+
+						int valDiff2 = Math.Abs(fv2.value - fv1.value);
+
+						if (valDiff2 > 180)
+							valDiff2 = 360 - valDiff2;
+
+						maximumValueDifference = Math.Max(maximumValueDifference, valDiff2);
+					}
+				}
+
+				if (maximumValueDifference <= MaximumRandomDeviation)
+				{
+					return ReadValueFromEnd(0).value;
+				}
+				else
+				{
+					return -1;
+				}
+			}
+		}
+
 		const double RadiansToDegrees = 360 / (2 * Math.PI);
 		const double thetaTrustedAngle = 40;
 
 		public event Action<int> NotifyAngleChanged;
 		bool _isStarted = false;
+		Filter filter = new Filter();
+		CoordinateSystemOrientation _coordinateSystemOrientation;
+
+		public CommonDeviceOrientationService(CoordinateSystemOrientation coordinateSystemOrientation)
+		{
+			_coordinateSystemOrientation = coordinateSystemOrientation;
+		}
 
 		public bool Start()
 		{
@@ -33,9 +177,14 @@ namespace apcurium.MK.Booking.Mobile.Infrastructure
 			return !_isStarted;
 		}
 
-		protected int GetZRotationAngle(double x, double y, double z)
+		protected int GetZRotationAngle(Vector3 vector)
 		{
-			int angle = 90 - (int)Math.Round(Math.Atan2(-y, x) * RadiansToDegrees);
+			int orientation = 1;
+
+			if (_coordinateSystemOrientation == CoordinateSystemOrientation.LeftHanded)
+				orientation = -1;
+
+			int angle = 90 - (int)Math.Round(Math.Atan2(-vector.y * orientation, vector.x * orientation) * RadiansToDegrees);
 
 			while (angle >= 360)
 			{
@@ -50,9 +199,10 @@ namespace apcurium.MK.Booking.Mobile.Infrastructure
 			return angle;
 		}
 
-		protected bool TrustZRotation(double x, double y, double z)
+		protected bool TrustZRotation(Vector3 vector)
 		{
-			double theta = Math.Asin(ClampToOne(z)) * RadiansToDegrees;
+			vector.Normalize();
+			double theta = Math.Asin(vector.z) * RadiansToDegrees;
 
 			if (Math.Abs(theta) < thetaTrustedAngle)
 			{
@@ -64,21 +214,6 @@ namespace apcurium.MK.Booking.Mobile.Infrastructure
 			}
 		}
 
-		double ClampToOne(double value)
-		{
-			if (value > 1)
-			{
-				value = 1;
-			}
-
-			if (value < -1)
-			{
-				value = -1;
-			}
-
-			return value;
-		}
-
 		public abstract bool IsAvailable();
 
 		protected abstract bool StartService();
@@ -86,14 +221,29 @@ namespace apcurium.MK.Booking.Mobile.Infrastructure
 		protected abstract bool StopService();
 
 		/// <summary>
-		/// Angles the changed event.
+		/// timestamp of the event in milliseconds
 		/// </summary>
-		/// <param name="angle">Angle is in degrees and assumed to be from 0 to 359 clockwise direction</param>
-		public void AngleChangedEvent(int angle)
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="z"></param>
+		/// <param name="timestamp"></param>
+		public void OrientationChanged(double x, double y, double z, long timestamp)
 		{
-			if (NotifyAngleChanged != null)
+			Vector3 v = new Vector3(x, y, z);
+			v.Normalize();
+
+			if (TrustZRotation(v))
 			{
-				NotifyAngleChanged(angle);
+				int rotation = GetZRotationAngle(v);
+
+				filter.AddValue(rotation, timestamp);
+
+				int filteredAngle = filter.StatisticalFilter();
+
+				if (NotifyAngleChanged != null && filteredAngle != -1)
+				{
+					NotifyAngleChanged(filteredAngle);
+				}
 			}
 		}
 	}
