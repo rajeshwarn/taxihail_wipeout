b359f8cf18f891656928b4a6606d55b9c3acfcf2
Dominique Savoie <dominique.savoie@apcurium.com>
2015-09-18 11:13:50 -0400

    MKTAXI-3122 => Added isPickup to the AirportInformationService get terminal signature.

--- Src/Mobile/Common/AppServices/Impl/AirportInformationService.cs
+++ Src/Mobile/Common/AppServices/Impl/AirportInformationService.cs
@@ -8,14 +8,18 @@ namespace apcurium.MK.Booking.Mobile.AppServices.Impl
 {
 	public class AirportInformationService : BaseService, IAirportInformationService
 	{
-		public async Task<string> GetTerminal(DateTime date, string flightNumber, string carrierCode, string airportId)
+
+
+
+		public async Task<string> GetTerminal(DateTime date, string flightNumber, string carrierCode, string airportId, bool isPickup)
 		{
 			var flightInformationRequest = new FlightInformationRequest
 			{
 				AirportId = airportId,
 				CarrierCode = carrierCode,
 				Date = date,
-				FlightNumber = flightNumber
+				FlightNumber = flightNumber,
+				IsPickup = isPickup
 			};
 
 			var airportInfo = await UseServiceClientAsync<FlightInformationServiceClient, FlightInformation>(service => 
