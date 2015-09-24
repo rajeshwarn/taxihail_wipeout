cba79607802bd102f989cc29ca767f7a1fabd2c5
Pierre Garcia <pierre.garcia@apcurium.com>
2015-09-24 07:07:55 -0700

    MKTAXI-3185 check for status before cancellling order server side

--- Src/Server/MK.Booking.Api/Services/CancelOrderService.cs
+++ Src/Server/MK.Booking.Api/Services/CancelOrderService.cs
@@ -14,6 +14,8 @@ using Infrastructure.Messaging;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceInterface;
 using apcurium.MK.Common.Extensions;
+using apcurium.MK.Common;
+using apcurium.MK.Common.Configuration;
 
 #endregion
 
@@ -26,15 +28,17 @@ namespace apcurium.MK.Booking.Api.Services
         private readonly ICommandBus _commandBus;
         private readonly IOrderDao _orderDao;
         private readonly IUpdateOrderStatusJob _updateOrderStatusJob;
+        private readonly Resources.Resources _resources;
 
         public CancelOrderService(ICommandBus commandBus, IIBSServiceProvider ibsServiceProvider,
-            IOrderDao orderDao, IAccountDao accountDao, IUpdateOrderStatusJob updateOrderStatusJob)
+            IOrderDao orderDao, IAccountDao accountDao, IUpdateOrderStatusJob updateOrderStatusJob, IServerSettings serverSettings)
         {
             _ibsServiceProvider = ibsServiceProvider;
             _orderDao = orderDao;
             _accountDao = accountDao;
             _updateOrderStatusJob = updateOrderStatusJob;
             _commandBus = commandBus;
+            _resources = new Resources.Resources(serverSettings);
         }
 
         public object Post(CancelOrder request)
@@ -51,11 +55,15 @@ namespace apcurium.MK.Booking.Api.Services
             {
                 throw new HttpError(HttpStatusCode.Unauthorized, "Can't cancel another account's order");
             }
-
             if (order.IBSOrderId.HasValue)
             {
                 var currentIbsAccountId = _accountDao.GetIbsAccountId(account.Id, order.CompanyKey);
-                if (currentIbsAccountId.HasValue)
+                var orderDetail = _orderDao.FindOrderStatusById(order.Id);
+
+                if (currentIbsAccountId.HasValue &&
+                    (orderDetail.IBSStatusId == VehicleStatuses.Common.Assigned
+                    || orderDetail.IBSStatusId == VehicleStatuses.Common.Arrived
+                    || orderDetail.IBSStatusId == VehicleStatuses.Common.Waiting))
                 {
                     //We need to try many times because sometime the IBS cancel method doesn't return an error but doesn't cancel the ride... after 5 time, we are giving up. But we assume the order is completed.
                     Task.Factory.StartNew(() =>
@@ -63,15 +71,18 @@ namespace apcurium.MK.Booking.Api.Services
                         Func<bool> cancelOrder = () => _ibsServiceProvider.Booking(order.CompanyKey).CancelOrder(order.IBSOrderId.Value, currentIbsAccountId.Value, order.Settings.Phone);
                         cancelOrder.Retry(new TimeSpan(0, 0, 0, 10), 5);
                     });
+
+                    var command = new Commands.CancelOrder { OrderId = request.OrderId };
+                    _commandBus.Send(command);
+
+                    UpdateStatusAsync(command.Id);
+
+                    return new HttpResult(HttpStatusCode.OK);
                 }
+                throw new HttpError(HttpStatusCode.BadRequest, _resources.Get("CancelOrdeError_wosLOADED"));
             }
-            
-            var command = new Commands.CancelOrder { OrderId = request.OrderId };
-            _commandBus.Send(command);
 
-            UpdateStatusAsync(command.Id);
-
-            return new HttpResult(HttpStatusCode.OK);
+            return new HttpResult(HttpStatusCode.BadRequest);
         }
 
         private void UpdateStatusAsync(Guid id)
