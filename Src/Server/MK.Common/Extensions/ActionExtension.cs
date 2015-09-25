5b38fb90d0218d2854d4fc6581b52e4463a83b1a
Dominique Savoie <dominique.savoie@apcurium.com>
2015-09-25 12:17:48 -0400

    MKTAXI-3196 => Added a conditional retry for tasks.

--- Src/Server/MK.Common/Extensions/ActionExtension.cs
+++ Src/Server/MK.Common/Extensions/ActionExtension.cs
@@ -25,12 +25,39 @@ namespace apcurium.MK.Common.Extensions
 				catch (Exception ex)
 				{
 					exceptions.Add(ex);
-					Thread.Sleep(retryInterval);
 				}
+
+				await Task.Delay(retryInterval);
 			}
 
 			throw new AggregateException(exceptions);
 		}
+
+	    public static async Task<T> Retry<T>(this Task<T> action, TimeSpan retryInterval, Func<Exception, bool> stopCondition, int retryCount = 3)
+	    {
+			var exceptions = new List<Exception>();
+
+		    var stop = false;
+
+			for (var retry = 0; retry < retryCount && !stop; retry++)
+			{
+				try
+				{
+					var result = await action;
+					return result;
+				}
+				catch (Exception ex)
+				{
+					exceptions.Add(ex);
+
+					stop = stopCondition(ex);
+				}
+
+				await Task.Delay(retryInterval);
+			}
+
+			throw new AggregateException(exceptions);
+	    }
 
         public static void Retry(this Action action, TimeSpan retryInterval, int retryCount = 3)
         {
