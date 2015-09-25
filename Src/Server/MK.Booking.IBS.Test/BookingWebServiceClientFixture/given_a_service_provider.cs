2cf2c44cdb08778c3cb046033a03261e8cef01bb
APCURIUM\julia.vigo <julia.vigo@apcurium.com>
2015-09-25 16:30:01 -0400

    first pass + need to test

--- Src/Server/MK.Booking.IBS.Test/BookingWebServiceClientFixture/given_a_service_provider.cs
+++ Src/Server/MK.Booking.IBS.Test/BookingWebServiceClientFixture/given_a_service_provider.cs
@@ -30,7 +30,7 @@ namespace MK.Booking.IBS.Test.BookingWebServiceClientFixture
             Console.WriteLine(nbOfAvailableCars.Count);
 
             // IBS Fare Estimate
-            var fareEstimate = service.GetFareEstimate(45.498247, -73.656673, 45.4987, -73.658, null, null, null, null, null, null,null);
+            var fareEstimate = service.GetFareEstimate(45.498247, -73.656673, 45.4987, -73.658, null, null, null, null, null, null, null, 0);
             Console.WriteLine(fareEstimate.Distance);
             Console.WriteLine(fareEstimate.FareEstimate);
             Console.WriteLine(fareEstimate.Tolls);
