6f98929a24139906a1d3afb3b91b87b571cf8206
Pierre Garcia <pierre.garcia@apcurium.com>
2015-09-28 09:57:07 -0700

    MKTAXI-3202 Added script for DB migration

--- Src/Server/DatabaseInitializer/Migrations/201509281644515_Booking_MKTAXI_3199.cs
+++ Src/Server/DatabaseInitializer/Migrations/201509281644515_Booking_MKTAXI_3199.cs
@@ -1,13 +1,14 @@
 namespace apcurium.MK.Booking.Migrations
 {
+    using Common;
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Booking_MKTAXI_3199 : DbMigration
     {
         public override void Up()
         {
-            AddColumn("Booking.CreditCardDetails", "Label", c => c.String());
+            AddColumn("Booking.CreditCardDetails", "Label", c => c.String(false, null, null, null, CreditCardConstants.Personal));
         }
         
         public override void Down()
