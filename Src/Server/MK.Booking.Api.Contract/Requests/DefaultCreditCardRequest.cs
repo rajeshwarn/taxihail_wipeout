14e1fcf013973e962270906501ff530cda7d28d1
Pierre Garcia <pierre.garcia@apcurium.com>
2015-09-29 06:40:08 -0700

    MKTAXI-3202 Moved Request in correct folder

--- Src/Server/MK.Booking.Api.Contract/Requests/DefaultCreditCardRequest.cs
+++ Src/Server/MK.Booking.Api.Contract/Requests/DefaultCreditCardRequest.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace apcurium.MK.Booking.Api.Services
+namespace apcurium.MK.Booking.Api.Contract.Requests
 {
     [Authenticate]
     [Route("/account/creditcard/changedefault", "POST")]
