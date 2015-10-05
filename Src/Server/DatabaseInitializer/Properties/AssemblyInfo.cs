2606eec1f5d313e996716e6babff2c6c8d042918
BrianRicard <brian.ricard@apcurium.com>
2015-10-05 10:53:55 -0400

    Version bump - 3.1.9 (wrong file was uploaded to Testflight)

--- Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
+++ Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.1.8")]
-[assembly: AssemblyVersion("3.1.8")]
-[assembly: AssemblyFileVersion("3.1.8")]
+// [assembly: AssemblyVersion("3.1.9")]
+[assembly: AssemblyVersion("3.1.9")]
+[assembly: AssemblyFileVersion("3.1.9")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
