4fed566127baf141800b98b95838c4c0c81ac53b
APCURIUM\julia.vigo <julia.vigo@apcurium.com>
2015-10-09 12:54:28 -0400

    MKTAXI-3292 change setting name

--- Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
+++ Src/Server/MK.Common/Configuration/TaxiHailSetting.cs
@@ -485,7 +485,7 @@ namespace MK.Common.Configuration
         public bool ShowOrientedPins { get; protected set; }
 
         [SendToClient, CustomizableByCompany]
-        [Display(Name = "Display - Use Tip Incentive", Description = "Offering a guaranteed bonus to drivers to boost the odds of getting a taxi.")]
+        [Display(Name = "Configuration - Enable Driver Bonus", Description = "Offering a guaranteed bonus to drivers to boost the odds of getting a taxi.")]
         public bool IsDriverBonusEnabled { get; protected set; }
 
         [CustomizableByCompany]
