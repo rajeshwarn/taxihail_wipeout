d889ff0afe977c584e273f9cd9c5c5b38111eaca
APCURIUM\mathieu.savaria <mathieu.savaria@apcurium.com>
2015-10-20 11:01:52 -0400

    Fixed ibs methods not working in external market

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/IIBSOrderService.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/Integration/IIBSOrderService.cs
@@ -4,9 +4,10 @@ namespace apcurium.MK.Booking.EventHandlers.Integration
     public interface IIbsOrderService
     {
         void ConfirmExternalPayment(Guid orderId, int ibsOrderId, decimal totalAmount, decimal tipAmount, decimal meterAmount, string type, string provider, string transactionId,
-           string authorizationCode, string cardToken, int accountId, string name, string phone, string email, string os, string userAgent);
+           string authorizationCode, string cardToken, int accountId, string name, string phone, string email, string os, string userAgent, string companyKey = null);
 
-        void SendPaymentNotification(double totalAmount, double taxedMeterAmount, double tipAmount, string authorizationCode, string vehicleNumber);
-        void SendMessageToDriver(string message, string vehicleNumber, string company);
+        void SendPaymentNotification(double totalAmount, double taxedMeterAmount, double tipAmount, string authorizationCode, string vehicleNumber, string companyKey = null);
+        
+        void SendMessageToDriver(string message, string vehicleNumber, string companyKey = null);
     }
 }
