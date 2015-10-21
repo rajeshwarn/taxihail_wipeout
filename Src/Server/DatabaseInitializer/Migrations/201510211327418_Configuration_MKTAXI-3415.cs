89fd63777424bfae1fe18c9aefbeff52d2e3e106
Pierre Garcia <pierre.garcia@apcurium.com>
2015-10-21 06:52:14 -0700

    Added Migration Script

---
+++ Src/Server/DatabaseInitializer/Migrations/201510211327418_Configuration_MKTAXI-3415.cs
@@ -0,0 +1,18 @@
+namespace apcurium.MK.Common.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Configuration_MKTAXI3415 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Config.NotificationSettings", "DriverBailedPush", c => c.Boolean(null, true));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("Config.NotificationSettings", "DriverBailedPush");
+        }
+    }
+}
