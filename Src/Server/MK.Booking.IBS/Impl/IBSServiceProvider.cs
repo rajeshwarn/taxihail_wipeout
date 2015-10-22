1463752706c333258811a33e6fbc17cf1706e0e8
Léon Pelletier <leon.pelletier@apcurium.com>
2015-10-22 12:15:13 -0400

    MTA-17 - Integrate service type settings service

--- Src/Server/MK.Booking.IBS/Impl/IBSServiceProvider.cs
+++ Src/Server/MK.Booking.IBS/Impl/IBSServiceProvider.cs
@@ -5,6 +5,7 @@ using apcurium.MK.Common.Extensions;
 using CustomerPortal.Client;
 using apcurium.MK.Booking.IBS.ChargeAccounts;
 using apcurium.MK.Common.Enumeration;
+using apcurium.MK.Common.Provider;
 
 namespace apcurium.MK.Booking.IBS.Impl
 {
@@ -13,14 +14,16 @@ namespace apcurium.MK.Booking.IBS.Impl
         private readonly IServerSettings _serverSettings;
         private readonly ILogger _logger;
         private readonly ITaxiHailNetworkServiceClient _taxiHailNetworkService;
+        private readonly IServiceTypeSettingsProvider _serviceTypeSettingsProvider;
         private readonly Dictionary<string, IBSSettingContainer> _ibsSettings = new Dictionary<string, IBSSettingContainer>();
 
         public IBSServiceProvider(IServerSettings serverSettings, ILogger logger,
-            ITaxiHailNetworkServiceClient taxiHailNetworkService)
+            ITaxiHailNetworkServiceClient taxiHailNetworkService, IServiceTypeSettingsProvider serviceTypeSettingsProvider)
         {
             _serverSettings = serverSettings;
             _logger = logger;
             _taxiHailNetworkService = taxiHailNetworkService;
+            _serviceTypeSettingsProvider = serviceTypeSettingsProvider;
         }
 
         public IAccountWebServiceClient Account(string companyKey)
@@ -51,8 +54,8 @@ namespace apcurium.MK.Booking.IBS.Impl
                 
                 if (serviceType.Value == ServiceType.Luxury)
                 {
-                    //var luxuryIbsSettings = _serviceTypeService.GetIbsSettingsForLuxury(_serverSettings.ServerData.TaxiHail.ApplicationKey);
-                    //ibsSettings.WebServicesUrl = luxuryIbsSettings.ServerData.IBS.WebServicesUrl;
+                    var luxuryIBSWebServiceUrl = _serviceTypeSettingsProvider.GetIBSWebServicesUrl(serviceType.Value);
+                    ibsSettings.WebServicesUrl = luxuryIBSWebServiceUrl;
                     return ibsSettings;
                 }
             }
