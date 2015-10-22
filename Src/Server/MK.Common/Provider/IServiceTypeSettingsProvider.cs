8fd7f8295e73ed292085329ac3b99fb60d7c09d9
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-10-22 12:23:32 -0400

    ServiceTypeSettingsProvider returns IBSSettingsContainer

--- Src/Server/MK.Common/Provider/IServiceTypeSettingsProvider.cs
+++ Src/Server/MK.Common/Provider/IServiceTypeSettingsProvider.cs
@@ -1,9 +1,10 @@
+using apcurium.MK.Common.Configuration;
 using apcurium.MK.Common.Enumeration;
 
 namespace apcurium.MK.Common.Provider
 {
     public interface IServiceTypeSettingsProvider
     {
-        string GetIBSWebServicesUrl(ServiceType serviceType);
+        IBSSettingContainer GetIBSSettings(ServiceType serviceType);
     }
 }
