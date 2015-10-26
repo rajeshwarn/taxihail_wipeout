1aab9c9638657ebe63ed382ae2f1c7ceae270975
maxim vlasov <maximvlasov@apcurium.com>
2015-10-26 14:51:50 -0400

    mktaxi-3424: date/time and vehicle number information fixed for receipt;

--- Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IReportDao.cs
+++ Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IReportDao.cs
@@ -6,5 +6,7 @@ namespace apcurium.MK.Booking.ReadModel.Query.Contract
     public interface IReportDao
     {
         IEnumerable<OrderReportDetail> GetOrderReports(DateTime startDate, DateTime endDate);
+
+		OrderReportDetail GetOrderReportWithOrderId(Guid orderId);
     }
 }
