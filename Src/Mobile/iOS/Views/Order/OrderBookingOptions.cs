430a87ff79b19a8271cb02b910ff54d56544df9c
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-10-27 09:51:43 -0400

    Fix crash at startup

--- Src/Mobile/iOS/Views/Order/OrderBookingOptions.cs
+++ Src/Mobile/iOS/Views/Order/OrderBookingOptions.cs
@@ -5,7 +5,6 @@ using apcurium.MK.Booking.Mobile.ViewModels.Orders;
 using apcurium.MK.Booking.Mobile.Client.Localization;
 using apcurium.MK.Booking.Mobile.Client.Extensions.Helpers;
 using Cirrious.MvvmCross.Binding.Touch.Views;
-using apcurium.MK.Common.Enumeration;
 
 namespace apcurium.MK.Booking.Mobile.Client.Views.Order
 {
@@ -34,11 +33,8 @@ namespace apcurium.MK.Booking.Mobile.Client.Views.Order
 
 			Initialize();
 
-			var serviceType = ((OrderOptionsViewModel)DataContext).SelectedVehicleType.ServiceType;
-            this.Services().Message.ShowMessage(null, this.Services().Localize["BookATaxi_Message" + (serviceType == ServiceType.Luxury ? "_Luxury" : "")]);
-
 			this.DelayBind (() => {
-				lblDescription.Text = Localize.GetValue("BookATaxi_Message" + (serviceType == ServiceType.Luxury ? "_Luxury" : ""));
+				lblDescription.Text = Localize.GetValue("BookATaxi_Message");
 				btnNow.SetTitle(Localize.GetValue("Now"), UIControlState.Normal);
 				btnLater.SetTitle(Localize.GetValue("BookItLaterButton"), UIControlState.Normal);
 				btnCancel.SetTitle(Localize.GetValue("Cancel"),UIControlState.Normal);
