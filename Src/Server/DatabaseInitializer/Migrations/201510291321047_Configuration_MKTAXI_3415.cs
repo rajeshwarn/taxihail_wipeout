d08957f55fec8f12afa27251a8d72ce3b7786e13
Dominique Savoie <dominique.savoie@apcurium.com>
2015-10-29 09:27:12 -0400

    Fixed migration conflict with Configuration_MKTAXI_3415 and Booking_MKTAXI_3347

--- Src/Server/DatabaseInitializer/Migrations/201510291321047_Configuration_MKTAXI_3415.cs
+++ Src/Server/DatabaseInitializer/Migrations/201510291321047_Configuration_MKTAXI_3415.cs
@@ -3,11 +3,11 @@ namespace apcurium.MK.Common.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Configuration_MKTAXI3415 : DbMigration
+    public partial class Configuration_MKTAXI_3415 : DbMigration
     {
         public override void Up()
         {
-            AddColumn("Config.NotificationSettings", "DriverBailedPush", c => c.Boolean(false, true));
+            AddColumn("Config.NotificationSettings", "DriverBailedPush", c => c.Boolean());
         }
         
         public override void Down()
