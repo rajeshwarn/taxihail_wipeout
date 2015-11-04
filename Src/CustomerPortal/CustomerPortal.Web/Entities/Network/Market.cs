42efba44b73703c6ee4dfa6a2de0f70962eac1fc
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-11-04 13:01:56 -0500

    Rename controller

---
+++ Src/CustomerPortal/CustomerPortal.Web/Entities/Network/Market.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MongoRepository;
+
+namespace CustomerPortal.Web.Entities.Network
+{
+    public class Market : IEntity
+    {
+        public Market()
+        {
+            Vehicles = new List<Vehicle>();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; } 
+    }
+
+    public class Vehicle
+    {
+        public string Id { get; set; }
+
+        public int NetworkVehicleId { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Logo")]
+        public string LogoName { get; set; }
+
+        [Display(Name = "Max # Passengers (0 = no limit)")]
+        public int MaxNumberPassengers { get; set; }
+    }
+}
---
+++ Src/CustomerPortal/CustomerPortal.Web/Entities/Network/ObsoleteEntities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoRepository;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerPortal.Web.Entities.Network
+{
+    [Obsolete]
+    public class NetworkVehicle : IEntity
+    {
+        public string Id { get; set; }
+
+        public int NetworkVehicleId { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Logo")]
+        public string LogoName { get; set; }
+
+        [Display(Name = "Max # Passengers (0 = no limit)")]
+        public int MaxNumberPassengers { get; set; }
+
+        public string Market { get; set; }
+    }
+}
