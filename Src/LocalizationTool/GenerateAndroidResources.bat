6ff7de8dbc6dd1310a9cf6ef848faf7ccc5fd30e
Pierre Garcia <pgarcia9988@gmail.com>
2015-11-06 16:06:16 -0500

    MKTAI_3487 Linked all resources

--- Src/LocalizationTool/GenerateAndroidResources.bat
+++ Src/LocalizationTool/GenerateAndroidResources.bat
@@ -1,6 +1,5 @@
 ﻿echo Generate String Resources
 output\LocalizationTool.exe -t=android -m="..\Mobile\Common\Localization\Master.resx" -d="..\Mobile\Android\Resources\Values\String.xml" -s="..\Mobile\Common\Settings\Settings.json"
-output\LocalizationTool.exe -t=android -m="..\Mobile\Common\Localization\Master.resx" -d="..\Mobile\TaxiHail.BlackBerry\Resources\Values\String.xml" -s="..\Mobile\Common\Settings\Settings.json"
 output\LocalizationTool.exe -t=ios -m="..\Mobile\Common\Localization\Master.resx" -d="..\Mobile\iOS\en.lproj\localizable.strings" -s="..\Mobile\Common\Settings\Settings.json"
 
 PAUSE
