2692df594515bcd4bd704e294d9f21894840be7f
Dominique Savoie <dominique.savoie@apcurium.com>
2015-11-09 11:51:48 -0500

    MKTAXI-3497 => Refactor of ErrorHandler to improve maintainability of the class.

--- Src/Mobile/Common/ErrorHandler.cs
+++ Src/Mobile/Common/ErrorHandler.cs
@@ -1,104 +1,122 @@
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using apcurium.MK.Booking.Mobile.AppServices;
+using apcurium.MK.Booking.Mobile.Extensions;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using apcurium.MK.Booking.Mobile.ViewModels;
 using apcurium.MK.Common.Diagnostic;
+using apcurium.MK.Common.Extensions;
+using Cirrious.CrossCore;
 using Cirrious.MvvmCross.ViewModels;
 using Cirrious.MvvmCross.Views;
 using ServiceStack.ServiceClient.Web;
-using TinyIoC;
 
 namespace apcurium.MK.Booking.Mobile
 {
     public class ErrorHandler : IErrorHandler
     {
+        private readonly IMvxViewDispatcher _dispatcher;
+        private readonly ILocalization _localize;
+        private readonly IMessageService _messageService;
+        private readonly ILogger _logger;
+
         public static DateTime LastConnectError = DateTime.MinValue;
         public static DateTime LastGeneralError = DateTime.MinValue;
 		public static DateTime LastUnauthorizedError = DateTime.MinValue;
 
-		private static bool _isErrorMessageDisplayed;
+		private static bool IsErrorMessageDisplayed;
+
+        public ErrorHandler(IMvxViewDispatcher dispatcher, ILocalization localize, IMessageService messageService, ILogger logger)
+        {
+            _dispatcher = dispatcher;
+            _localize = localize;
+            _messageService = messageService;
+            _logger = logger;
+        }
 
         public bool HandleError (Exception ex)
         {
-			var handled = false;
+            var webServiceException = ex as WebServiceException;
+            var webException = ex as WebException;
+            var statusCode = webServiceException.SelectOrDefault(service => (int?) service.StatusCode, null) ??
+                             webException.SelectOrDefault(service => (int?) service.Status, null);
 
-			if (ex is WebServiceException 
-				&& ((WebServiceException)ex).StatusCode == (int)HttpStatusCode.Unauthorized) 
+            var handled = false;
+            switch (statusCode)
             {
-				if (LastUnauthorizedError.Subtract(DateTime.Now).TotalSeconds < -5)
-				{
-					LastUnauthorizedError = DateTime.Now;
-
-					var localize = TinyIoCContainer.Current.Resolve<ILocalization>();
-					TinyIoCContainer.Current.Resolve<IMessageService> ().ShowMessage(
-						localize["ServiceErrorCallTitle"], 
-						localize["ServiceErrorUnauthorized"], 
-						() => {					
-							var dispatch = TinyIoCContainer.Current.Resolve<IMvxViewDispatcher> ();
-							dispatch.ShowViewModel(new MvxViewModelRequest (typeof(LoginViewModel), null, null, MvxRequestedBy.UserAction));
-
-							TinyIoCContainer.Current.Resolve<IOrderWorkflowService>().PrepareForNewOrder ();
-							TinyIoCContainer.Current.Resolve<IAccountService>().SignOut ();
-						});
-				}
-
-				handled = true;
-			}
-			else if ((ex is WebException 
-				&& (((WebException)ex).Status == WebExceptionStatus.ConnectFailure 
-					|| ((WebException)ex).Status == WebExceptionStatus.NameResolutionFailure))
-				|| (ex is WebServiceException
-					&& ((WebServiceException)ex).StatusCode == (int)HttpStatusCode.ServiceUnavailable))
-			{
-				if(LastConnectError.Subtract(DateTime.Now).TotalSeconds < -5)
-				{
-					LastConnectError = DateTime.Now;
-
-					if (!_isErrorMessageDisplayed)
-					{
-						_isErrorMessageDisplayed = true;
-
-						var localize = TinyIoCContainer.Current.Resolve<ILocalization>();
-						TinyIoCContainer.Current.Resolve<IMessageService> ().ShowMessage (
-							localize["NoConnectionTitle"], 
-							localize["NoConnectionMessage"],
-							() =>
-							{
-								_isErrorMessageDisplayed = false;
-							});
-					}
-
-                    LogError(ex);
-				}
-
-				handled = true;
-			}
-			else
-            {
-                // Handle all other generic exceptions
-
-                if (LastGeneralError.Subtract(DateTime.Now).TotalSeconds < -5)
+                case (int)HttpStatusCode.Unauthorized:
                 {
-                    LastGeneralError = DateTime.Now;
+                    HandleUnauthorizedErrorIfNeeded();
+                    handled = true;
+                    break;
+                }
+                case (int)HttpStatusCode.ServiceUnavailable:
+                case (int)WebExceptionStatus.ConnectFailure:
+                case (int)WebExceptionStatus.NameResolutionFailure:
+                {
+                    HandleConnectionError(ex);
+                    handled = true;
+                    break;
+                }
+                default:
+                {
+                    if (LastGeneralError.Subtract(DateTime.Now).TotalSeconds < -5)
+                    {
+                        LastGeneralError = DateTime.Now;
 
-                    LogError(ex);
+                        _logger.LogError(ex);
+                    }
+                    break;
                 }
             }
 
 			return handled;
         }
 
-        private void LogError(Exception ex)
+        private void HandleConnectionError(Exception ex)
         {
-            try
+            if (LastConnectError.Subtract(DateTime.Now).TotalSeconds >= -5)
             {
-                TinyIoCContainer.Current.Resolve<ILogger>().LogError(ex);
+                return;
             }
-            catch
+
+            LastConnectError = DateTime.Now;
+
+            if (!IsErrorMessageDisplayed)
             {
-                // Do nothing
+                IsErrorMessageDisplayed = true;
+                
+                _messageService.ShowMessage(
+                    _localize["NoConnectionTitle"], 
+                    _localize["NoConnectionMessage"], 
+                    () => IsErrorMessageDisplayed = false);
             }
+
+            _logger.LogError(ex);
+        }
+
+        private void HandleUnauthorizedErrorIfNeeded()
+        {
+            if (LastUnauthorizedError.Subtract(DateTime.Now).TotalSeconds >= -5)
+            {
+                return;
+            }
+
+            LastUnauthorizedError = DateTime.Now;
+
+            _messageService.ShowMessage(
+                _localize["ServiceErrorCallTitle"],
+                _localize["ServiceErrorUnauthorized"],
+                () => Task.Run(SignOutUser).FireAndForget());
+        }
+
+        private async Task SignOutUser()
+        {
+            _dispatcher.ShowViewModel(new MvxViewModelRequest(typeof (LoginViewModel), null, null, MvxRequestedBy.UserAction));
+
+            await Mvx.Resolve<IOrderWorkflowService>().PrepareForNewOrder();
+            Mvx.Resolve<IAccountService>().SignOut();
         }
     }
 }
