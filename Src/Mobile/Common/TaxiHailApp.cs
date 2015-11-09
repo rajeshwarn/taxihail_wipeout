f18efc2f8b995ed98f88fad344e77d76d37b4c64
maxim vlasov <maximvlasov@apcurium.com>
2015-11-09 15:01:29 -0500

    mktaxi-3433: code reformatting;

--- Src/Mobile/Common/TaxiHailApp.cs
+++ Src/Mobile/Common/TaxiHailApp.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using apcurium.MK.Booking.Api.Client;
 using apcurium.MK.Booking.Api.Client.Payments.PayPal;
 using apcurium.MK.Booking.Api.Client.TaxiHail;
@@ -31,7 +32,7 @@ using apcurium.MK.Booking.Mobile.AppServices.Social;
 using apcurium.MK.Booking.Mobile.Infrastructure.DeviceOrientation;
 using apcurium.MK.Common.Extensions;
 using apcurium.MK.Common;
-
+using apcurium.MK.Booking.Mobile.Extensions;
 
 namespace apcurium.MK.Booking.Mobile
 {
@@ -43,10 +44,7 @@ namespace apcurium.MK.Booking.Mobile
 		{
 			// early loading of libphone library
 			// do not remove, it impoves the time during the binding
-			Task.Run(() =>
-			{
-				System.Diagnostics.Trace.Write(CountryCode.CountryCodes[0].CountryName.Substring(0, 0));
-			});
+			(new Task(() => { Trace.Write(CountryCode.CountryCodes[0].CountryName.Substring(0, 0)); })).FireAndForget();
 
 			_container = TinyIoCContainer.Current;
 
