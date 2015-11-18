5322c419df96205919c9c6f526d3dce7d40aa98e
Dominique Savoie <dominique.savoie@apcurium.com>
2015-11-18 08:46:38 -0500

    MKTAXI-3563 => Fixed using

--- Src/Server/MK.Common/Extensions/LoggerExtensions.cs
+++ Src/Server/MK.Common/Extensions/LoggerExtensions.cs
@@ -4,12 +4,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using apcurium.MK.Common.Diagnostic;
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 namespace MK.Common.Android.Extensions
 {
