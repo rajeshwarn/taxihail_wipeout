9aa285ffc3d369f31fe4bf47b987a511246b8306
Dominique Savoie <dominique.savoie@apcurium.com>
2015-11-24 12:05:52 -0500

    MKTAXI-3316 => Fixed logic to extract the endpoint from the route attribute.

--- Src/Server/MK.Common/Extensions/ReturnExtensions.cs
+++ Src/Server/MK.Common/Extensions/ReturnExtensions.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using ServiceStack.ServiceHost;
 
@@ -19,14 +21,18 @@ namespace apcurium.MK.Common.Extensions
                 .Where(property => property.Value != null)
                 .ToArray();
 
-            var routeAttribute = requestType.GetCustomAttributes(typeof(RouteAttribute), true)
+            var routeAttributes = requestType.GetCustomAttributes(typeof(RouteAttribute), true)
                 .OfType<RouteAttribute>()
-                .FirstOrDefault(route => properties.All(p => route.Address.Contains(p.Name)));
+                .ToArray();
 
-            if (routeAttribute == null)
+            if (routeAttributes.None())
             {
                 return string.Empty;
             }
+
+            var routeAttribute = routeAttributes.Length == 1
+                ? routeAttributes.First()
+                : routeAttributes.SelectCorrectRouteAttribute(properties.Select(p => p.Name));
 
             var matches = Regex.Matches(routeAttribute.Address, "\\{[a-zA-Z]+\\}", RegexOptions.Compiled)
                 .Cast<Match>()
@@ -41,8 +47,29 @@ namespace apcurium.MK.Common.Extensions
                 address = address.Replace(propertyName, property.Value.ToString());
             }
 
-
             return address;
+        }
+
+        private static RouteAttribute SelectCorrectRouteAttribute(this IEnumerable<RouteAttribute> routes, IEnumerable<string> propertyInfos)
+        {
+            var activePropertyNames = propertyInfos as string[] ?? propertyInfos.ToArray();
+            var routeAttributes = routes as RouteAttribute[] ?? routes.ToArray();
+
+            var routeAttribute = routeAttributes.FirstOrDefault(route => activePropertyNames.All(p => route.Address.Contains(p)));
+
+            if (routeAttribute != null)
+            {
+                return routeAttribute;
+            }
+
+            return routeAttributes.Select(route => new
+                {
+                    Route = route,
+                    propertyNames = activePropertyNames.Where(p => route.Address.Contains(p)).ToArray()
+                })
+                .OrderByDescending(p => p.propertyNames.Length)
+                .Select(route => route.Route)
+                .FirstOrDefault();
         }
     }
 }
