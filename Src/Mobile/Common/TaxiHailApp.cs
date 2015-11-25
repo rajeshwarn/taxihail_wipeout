113a6d3f8dd52f1f5bde4470c5e78b0668f82d6d
maxim vlasov <maximvlasov@apcurium.com>
2015-11-25 12:40:55 -0500

    broken build fixed;

--- Src/Mobile/Common/TaxiHailApp.cs
+++ Src/Mobile/Common/TaxiHailApp.cs
@@ -135,7 +135,7 @@ namespace apcurium.MK.Booking.Mobile
 			// In debug, we should allow all certs to allow us to debug issues that might arise.
 			ServicePointManager.ServerCertificateValidationCallback += (p1, p2, p3, p4) => true;
 #else
-			ServicePointManager.ServerCertificateValidationCallback += ServerCertificateValidationCallback
+			ServicePointManager.ServerCertificateValidationCallback += ServerCertificateValidationCallback;
 #endif
 		}
 
