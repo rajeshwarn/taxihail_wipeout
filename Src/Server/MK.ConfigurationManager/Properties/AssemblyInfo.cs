f60cc5dd384f876398e4ea26f710c3f1ee4393f0
BrianRicard <brian.ricard@apcurium.com>
2015-11-25 14:22:50 -0500

    Version Bump 3.1.29

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.1.28")]
-[assembly: AssemblyVersion("3.1.28")]
-[assembly: AssemblyFileVersion("3.1.28")]
+// [assembly: AssemblyVersion("3.1.29")]
+[assembly: AssemblyVersion("3.1.29")]
+[assembly: AssemblyFileVersion("3.1.29")]
 
--- Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
+++ Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.1.28")]
-[assembly: AssemblyVersion("3.1.28")]
-[assembly: AssemblyFileVersion("3.1.28")]
+// [assembly: AssemblyVersion("3.1.29")]
+[assembly: AssemblyVersion("3.1.29")]
+[assembly: AssemblyFileVersion("3.1.29")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
