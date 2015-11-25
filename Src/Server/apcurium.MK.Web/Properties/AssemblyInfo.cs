1c5452fcc573870391dc5f65ec04b7d1ae31ba0e
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2015-11-25 09:06:28 -0500

    Version Bump 3.1.27

--- Src/Server/apcurium.MK.Web/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.26")]
-[assembly: AssemblyFileVersion("3.1.26")]
+[assembly: AssemblyVersion("3.1.27")]
+[assembly: AssemblyFileVersion("3.1.27")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
