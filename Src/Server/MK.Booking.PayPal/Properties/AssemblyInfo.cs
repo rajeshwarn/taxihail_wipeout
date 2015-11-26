f098c11a5c099ef322d4293e3a425ee71ecb5fc5
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2015-11-26 11:14:43 -0500

    Version Bump 3.1.30

--- Src/Server/MK.Booking.PayPal/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.PayPal/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.1.29")]
-[assembly: AssemblyVersion("3.1.29")]
-[assembly: AssemblyFileVersion("3.1.29")]
+// [assembly: AssemblyVersion("3.1.30")]
+[assembly: AssemblyVersion("3.1.30")]
+[assembly: AssemblyFileVersion("3.1.30")]
