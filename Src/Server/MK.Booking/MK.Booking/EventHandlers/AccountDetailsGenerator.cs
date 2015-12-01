28bce899d8309200adcf1761ac2a89cd980ec40c
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-12-01 08:22:02 -0500

    React

--- Src/Server/MK.Booking/MK.Booking/EventHandlers/AccountDetailsGenerator.cs
+++ Src/Server/MK.Booking/MK.Booking/EventHandlers/AccountDetailsGenerator.cs
@@ -210,7 +210,7 @@ namespace apcurium.MK.Booking.EventHandlers
                 settings.CustomerNumber = @event.CustomerNumber;
                 settings.PayBack = @event.PayBack;
 
-                if (!string.IsNullOrWhiteSpace(@event.Email))
+                if (@event.Email.HasValueTrimmed())
                 {
                     account.Email = @event.Email;
                 }
