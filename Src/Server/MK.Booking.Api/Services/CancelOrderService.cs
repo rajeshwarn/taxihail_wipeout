c593a6965debc6998a0eddc91900db97a14ab43f
unknown <Nicolas.Champanhac@nchampanhacpc.apcurium.local>
2015-12-03 13:35:49 -0500

    MKTAXI-3635 - correction in case of no IBSOrderId is present.

--- Src/Server/MK.Booking.Api/Services/CancelOrderService.cs
+++ Src/Server/MK.Booking.Api/Services/CancelOrderService.cs
@@ -5,8 +5,6 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using apcurium.MK.Booking.Api.Contract.Requests;
-using apcurium.MK.Booking.Api.Contract.Resources;
-using apcurium.MK.Booking.Api.Services.Payment;
 using apcurium.MK.Booking.IBS;
 using apcurium.MK.Booking.Jobs;
 using apcurium.MK.Booking.ReadModel.Query.Contract;
@@ -14,67 +12,81 @@ using Infrastructure.Messaging;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceInterface;
 using apcurium.MK.Common.Extensions;
+using apcurium.MK.Common.Configuration;
+using apcurium.MK.Common;
 
 #endregion
 
 namespace apcurium.MK.Booking.Api.Services
 {
-    public class CancelOrderService : Service
+	public class CancelOrderService : Service
     {
         private readonly IAccountDao _accountDao;
         private readonly IIBSServiceProvider _ibsServiceProvider;
         private readonly ICommandBus _commandBus;
         private readonly IOrderDao _orderDao;
         private readonly IUpdateOrderStatusJob _updateOrderStatusJob;
+		private readonly Resources.Resources _resources;
 
-        public CancelOrderService(ICommandBus commandBus, IIBSServiceProvider ibsServiceProvider,
-            IOrderDao orderDao, IAccountDao accountDao, IUpdateOrderStatusJob updateOrderStatusJob)
+		public CancelOrderService(ICommandBus commandBus, IIBSServiceProvider ibsServiceProvider,
+            IOrderDao orderDao, IAccountDao accountDao, IUpdateOrderStatusJob updateOrderStatusJob, IServerSettings serverSettings)
         {
             _ibsServiceProvider = ibsServiceProvider;
             _orderDao = orderDao;
             _accountDao = accountDao;
             _updateOrderStatusJob = updateOrderStatusJob;
             _commandBus = commandBus;
-        }
+			_resources = new Resources.Resources(serverSettings);
+		}
 
-        public object Post(CancelOrder request)
-        {
-            var order = _orderDao.FindById(request.OrderId);
-            var account = _accountDao.FindById(new Guid(this.GetSession().UserAuthId));
+		public object Post(CancelOrder request)
+		{
+			var order = _orderDao.FindById(request.OrderId);
+			var account = _accountDao.FindById(new Guid(this.GetSession().UserAuthId));
 
-            if (order == null)
-            {
-                return new HttpResult(HttpStatusCode.NotFound);
-            }
+			if (order == null)
+			{
+				return new HttpResult(HttpStatusCode.NotFound);
+			}
 
-            if (account.Id != order.AccountId)
-            {
-                throw new HttpError(HttpStatusCode.Unauthorized, "Can't cancel another account's order");
-            }
+			if (account.Id != order.AccountId)
+			{
+				throw new HttpError(HttpStatusCode.Unauthorized, "Can't cancel another account's order");
+			}
 
-            if (order.IBSOrderId.HasValue)
-            {
-                var currentIbsAccountId = _accountDao.GetIbsAccountId(account.Id, order.CompanyKey);
-                if (currentIbsAccountId.HasValue)
-                {
-                    //We need to try many times because sometime the IBS cancel method doesn't return an error but doesn't cancel the ride... after 5 time, we are giving up. But we assume the order is completed.
-                    Task.Factory.StartNew(() =>
-                    {
-                        Func<bool> cancelOrder = () => _ibsServiceProvider.Booking(order.CompanyKey).CancelOrder(order.IBSOrderId.Value, currentIbsAccountId.Value, order.Settings.Phone);
-                        cancelOrder.Retry(new TimeSpan(0, 0, 0, 10), 5);
-                    });
-                }
-            }
-            
-            var command = new Commands.CancelOrder { OrderId = request.OrderId };
-            _commandBus.Send(command);
+			if (order.IBSOrderId.HasValue)
+			{
+				var currentIbsAccountId = _accountDao.GetIbsAccountId(account.Id, order.CompanyKey);
+				var orderDetail = _orderDao.FindOrderStatusById(order.Id);
 
-            UpdateStatusAsync(command.Id);
+				if (currentIbsAccountId.HasValue
+					&& (!orderDetail.IBSStatusId.HasValue()
+					|| orderDetail.IBSStatusId.SoftEqual(VehicleStatuses.Common.Waiting)
+					|| orderDetail.IBSStatusId.SoftEqual(VehicleStatuses.Common.Assigned)
+					|| orderDetail.IBSStatusId.SoftEqual(VehicleStatuses.Common.Arrived)
+					|| orderDetail.IBSStatusId.SoftEqual(VehicleStatuses.Common.Scheduled)))
+				{
+					// We need to try many times because sometime the IBS cancel method doesn't return an error but doesn't cancel the ride... after 5 time, we are giving up. But we assume the order is completed.
+					Task.Factory.StartNew(() => {
+						Func<bool> cancelOrder = () => _ibsServiceProvider.Booking(order.CompanyKey).CancelOrder(order.IBSOrderId.Value, currentIbsAccountId.Value, order.Settings.Phone);
+						cancelOrder.Retry(new TimeSpan(0, 0, 0, 10), 5);
+					});
 
-            return new HttpResult(HttpStatusCode.OK);
-        }
+					var command = new Commands.CancelOrder { OrderId = request.OrderId };
+					_commandBus.Send(command);
 
-        private void UpdateStatusAsync(Guid id)
+					UpdateStatusAsync(command.OrderId);
+
+					return new HttpResult(HttpStatusCode.OK);
+				}
+
+				throw new HttpError(HttpStatusCode.BadRequest, _resources.Get("CancelOrderError"));
+			}
+
+			return new HttpResult(HttpStatusCode.BadRequest, _resources.Get("CancelOrderError_NoIBSOrderId"));
+		}
+
+		private void UpdateStatusAsync(Guid id)
         {
             new TaskFactory().StartNew(() =>
             {
