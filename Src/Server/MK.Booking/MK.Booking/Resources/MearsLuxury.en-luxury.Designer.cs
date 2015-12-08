b48183fb9dd198c814c5fef4918e45dfd4c0984c
Léon Pelletier <leon.pelletier@apcurium.com>
2015-12-08 16:16:51 -0500

    MTA - Add missing localization resources for iOS

--- Src/Server/MK.Booking/MK.Booking/Resources/MearsLuxury.en-luxury.Designer.cs
+++ Src/Server/MK.Booking/MK.Booking/Resources/MearsLuxury.en-luxury.Designer.cs
@@ -22,14 +22,14 @@ namespace apcurium.MK.Booking.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class MearsTaxi_en_luxury {
+    public class MearsLuxury_en_luxury {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal MearsTaxi_en_luxury() {
+        internal MearsLuxury_en_luxury() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace apcurium.MK.Booking.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("apcurium.MK.Booking.Resources.MearsTaxi.en-luxury", typeof(MearsTaxi_en_luxury).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("apcurium.MK.Booking.Resources.MearsLuxury.en-luxury", typeof(MearsLuxury_en_luxury).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
