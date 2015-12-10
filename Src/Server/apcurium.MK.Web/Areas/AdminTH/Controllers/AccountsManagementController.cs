e90e6dbefa00e1af3d4b42bc2236ffe4cce442a3
unknown <Nicolas.Champanhac@nchampanhacpc.apcurium.local>
2015-12-10 15:04:17 -0500

    development in progress

---
+++ Src/Server/apcurium.MK.Web/Areas/AdminTH/Controllers/AccountsManagementController.cs
@@ -0,0 +1,54 @@
+﻿using apcurium.MK.Booking.ReadModel.Query.Contract;
+using apcurium.MK.Booking.Security;
+using apcurium.MK.Common.Configuration;
+using apcurium.MK.Web.Areas.AdminTH.Models;
+using apcurium.MK.Web.Attributes;
+using ServiceStack.CacheAccess;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace apcurium.MK.Web.Areas.AdminTH.Controllers
+{
+	[AuthorizationRequired(RoleName.Admin)]
+	public class AccountsManagementController : ServiceStackController
+	{
+		private readonly IAccountDao _accountDao;
+
+		public AccountsManagementController(ICacheClient cache, IServerSettings serverSettings, IAccountDao accountDao)
+			: base(cache, serverSettings)
+		{
+			_accountDao = accountDao;
+		}
+
+		public ActionResult Index()
+		{
+			// first time we open Index without values
+			return View(new AccountsManagement());
+		}
+
+		//private AccountsManagement GetUsersForSearchCriteria()
+		//{
+		//	var settings = _accountDao.FindByNamePattern();
+		//	return new AccountsManagement();
+		//}
+
+		// POST: AdminTH/AccountsManagement/Search
+		[HttpPost]
+		[ValidateInput(false)]
+		public async Task<ActionResult> Search(FormCollection form)
+		{
+			if(!form.HasKeys())
+			{
+				TempData["SearchCriteriaEmpty"] = "Search criteria field should not be empty.";
+				return RedirectToAction("Index");
+			}
+
+			var accountsManagement = new AccountsManagement();
+			accountsManagement.SearchCriteria = form.GetValue("searchCriteria").AttemptedValue;
+
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Src/Server/apcurium.MK.Web/Areas/AdminTH/Models/AccountsManagement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace apcurium.MK.Web.Areas.AdminTH.Models
+{
+	public class AccountsManagement
+	{
+		public AccountsManagement()
+		{
+
+		}
+
+		[Display(Name = "Search Criteria")]
+		public string SearchCriteria { get; set; }
+	}
+}
