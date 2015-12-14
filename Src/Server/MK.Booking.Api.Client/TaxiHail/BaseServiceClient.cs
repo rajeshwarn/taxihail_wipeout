3aedd0b3f594e1ba860e024d55b126ca3691568f
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-12-14 12:50:19 -0500

    Fix runtime error with ibs libraries

--- Src/Server/MK.Booking.Api.Client/TaxiHail/BaseServiceClient.cs
+++ Src/Server/MK.Booking.Api.Client/TaxiHail/BaseServiceClient.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using apcurium.MK.Common.Extensions;
 #if CLIENT
