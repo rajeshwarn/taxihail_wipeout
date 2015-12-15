033c9e8b969d11786ab124bdbf1e786204753355
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-12-15 09:18:55 -0500

    If the order creation fails because of anything else than a bad request, show the generic error message instead of showing nothing and aborting

--- Src/Mobile/Common/AppServices/Orders/OrderWorkflowService.cs
+++ Src/Mobile/Common/AppServices/Orders/OrderWorkflowService.cs
@@ -298,24 +298,37 @@ namespace apcurium.MK.Booking.Mobile.AppServices.Orders
 			}
 			catch(WebServiceException e)
 			{
-			    string message;
+				// prevents an exception in the deserialization of the response body
+				if (e.StatusCode != (int)HttpStatusCode.BadRequest)
+				{
+					throw new OrderCreationException(GetUnhandledErrorMessageForOrderCreation());
+				}
+					
 			    var error = e.ResponseBody.FromJson<ErrorResponse>();
 
 			    if (e.StatusCode == (int)HttpStatusCode.BadRequest && error.ResponseStatus != null)
 			    {
-                    message = e.ErrorCode == "CreateOrder_PendingOrder" ? e.ErrorCode : error.ResponseStatus.Message;
+                    var message = e.ErrorCode == "CreateOrder_PendingOrder" ? e.ErrorCode : error.ResponseStatus.Message;
 
                     throw new OrderCreationException(message, error.ResponseStatus.Message);
 			    }
 
 			    // Unhandled errors
 				// if ibs3000, there's a problem with the account, use a different one
-			    message = _appSettings.Data.HideCallDispatchButton
-                    ? _localize["ServiceError_ErrorCreatingOrderMessage_NoCall"]
-                    : string.Format(_localize["ServiceError_ErrorCreatingOrderMessage"], _appSettings.Data.TaxiHail.ApplicationName, _appSettings.Data.DefaultPhoneNumberDisplay);
-
-				throw new OrderCreationException(message);		
+			    
+				throw new OrderCreationException(GetUnhandledErrorMessageForOrderCreation());		
 			}
+			catch(Exception)
+			{
+				throw new OrderCreationException(GetUnhandledErrorMessageForOrderCreation());
+			}
+		}
+
+		private string GetUnhandledErrorMessageForOrderCreation()
+		{
+			return _appSettings.Data.HideCallDispatchButton
+				? _localize["ServiceError_ErrorCreatingOrderMessage_NoCall"]
+				: string.Format(_localize["ServiceError_ErrorCreatingOrderMessage"], _appSettings.Data.TaxiHail.ApplicationName, _appSettings.Data.DefaultPhoneNumberDisplay);
 		}
 
 		public async Task SetVehicleType(int? vehicleTypeId)
