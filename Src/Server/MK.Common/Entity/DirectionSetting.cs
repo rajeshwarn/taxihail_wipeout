aadcddf2a9662f740256543f1f4f7a545a2af6d1
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-12-15 11:56:25 -0500

    Remove unnecessary PropertyEncrypt on containers + clarify TarifMode values

--- Src/Server/MK.Common/Entity/DirectionSetting.cs
+++ Src/Server/MK.Common/Entity/DirectionSetting.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace apcurium.MK.Common.Entity
 {
     public enum TarifMode
     {
+        [Display(Name = "Use TaxiHail fare calculation")]
         AppTarif,
+        [Display(Name = "Use IBS fare calculation")]
         Ibs,
+        [Display(Name = "Use both IBS and TaxiHail fare calculations")]
         Both,
+        [Display(Name = "Use newer IBS fare calculation using distance")]
         Ibs_Distance
     }
 
