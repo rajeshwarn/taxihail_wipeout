d84af011e756881a1b983ea129a1e6ad87ffd060
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2015-12-16 16:06:03 -0500

    Revert some tabs

--- Src/Server/MK.Booking.Api.Contract/Resources/Order.cs
+++ Src/Server/MK.Booking.Api.Contract/Resources/Order.cs
@@ -9,45 +9,45 @@ using Newtonsoft.Json;
 
 namespace apcurium.MK.Booking.Api.Contract.Resources
 {
-	public class Order : BaseDto
-	{
-		[JsonConverter(typeof(GuidJsonConverter))]
-		public Guid Id { get; set; }
+    public class Order : BaseDto
+    {
+        [JsonConverter(typeof(GuidJsonConverter))]
+        public Guid Id { get; set; }
 
-		public int? IBSOrderId { get; set; }
+        public int? IBSOrderId { get; set; }
 
-		public DateTime PickupDate { get; set; }
+        public DateTime PickupDate { get; set; }
 
-		public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
 
-		public string Note { get; set; }
+        public string Note { get; set; }
 
-		public Address PickupAddress { get; set; }
+        public Address PickupAddress { get; set; }
 
-		public Address DropOffAddress { get; set; }
+        public Address DropOffAddress { get; set; }
 
-		public BookingSettings Settings { get; set; }
+        public BookingSettings Settings { get; set; }
 
-		public double? Fare { get; set; }
+        public double? Fare { get; set; }
 
-		public double? Tax { get; set; }
+        public double? Tax { get; set; }
 
-		public double? Toll { get; set; }
+        public double? Toll { get; set; }
 
-		public double? Tip { get; set; }
+        public double? Tip { get; set; }
 
-		public double? Surcharge { get; set; }
+        public double? Surcharge { get; set; }
 
-		public bool IsRated { get; set; }
+        public bool IsRated { get; set; }
 
-		public long TransactionId { get; set; }
+        public long TransactionId { get; set; }
 
-		public OrderStatus Status { get; set; }
+        public OrderStatus Status { get; set; }
 
-		public string PromoCode { get; set; }
+        public string PromoCode { get; set; }
 
-		public bool IsManualRideLinq { get; set; }
+        public bool IsManualRideLinq { get; set; }
 
-		public double? TipIncentive { get; set; }
-	}
+        public double? TipIncentive { get; set; }
+    }
 }
