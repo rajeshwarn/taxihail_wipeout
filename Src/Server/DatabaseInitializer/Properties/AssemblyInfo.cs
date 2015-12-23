8b77ba6d20cbb4c551171903f58ec77cbc3d1c30
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2015-12-23 08:42:35 -0500

    Version Bump 4.0.15

--- Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
+++ Src/Server/DatabaseInitializer/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.0.14")]
-[assembly: AssemblyVersion("4.0.14")]
-[assembly: AssemblyFileVersion("4.0.14")]
+// [assembly: AssemblyVersion("4.0.15")]
+[assembly: AssemblyVersion("4.0.15")]
+[assembly: AssemblyFileVersion("4.0.15")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
--- Src/Server/ExportOrders/Properties/AssemblyInfo.cs
+++ Src/Server/ExportOrders/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.0.14")]
-[assembly: AssemblyVersion("4.0.14")]
-[assembly: AssemblyFileVersion("4.0.14")]
+// [assembly: AssemblyVersion("4.0.15")]
+[assembly: AssemblyVersion("4.0.15")]
+[assembly: AssemblyFileVersion("4.0.15")]
 
