52b8df8ccc88c01791d6a6db70b262a697debeb1
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2016-01-05 12:08:37 -0500

    Version Bump 3.2.23

--- Src/Server/MK.Booking.Maps/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Maps/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.2.22")]
-[assembly: AssemblyVersion("3.2.22")]
-[assembly: AssemblyFileVersion("3.2.22")]
+// [assembly: AssemblyVersion("3.2.23")]
+[assembly: AssemblyVersion("3.2.23")]
+[assembly: AssemblyFileVersion("3.2.23")]
 
 
