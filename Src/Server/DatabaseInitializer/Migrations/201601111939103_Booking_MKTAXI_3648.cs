0036e5fb9bbfad2c184e5798cd7f4c887026f0fe
Mathieu Masciotra <mathieu.masciotra@apcurium.com>
2016-01-11 14:43:17 -0500

    Sorry for your migration scripts, change in schema for ConfigurationChangeEntry

--- Src/Server/DatabaseInitializer/Migrations/201601111939103_Booking_MKTAXI_3648.cs
+++ Src/Server/DatabaseInitializer/Migrations/201601111939103_Booking_MKTAXI_3648.cs
@@ -3,7 +3,7 @@ namespace apcurium.MK.Booking.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Booking_MKTAXI3731 : DbMigration
+    public partial class Booking_MKTAXI_3648 : DbMigration
     {
         public override void Up()
         {
@@ -12,12 +12,12 @@ namespace apcurium.MK.Booking.Migrations
                 c => new
                     {
                         Id = c.Guid(nullable: false),
-                        AccountId = c.String(),
+                        AccountId = c.Guid(nullable: false),
                         AccountEmail = c.String(),
                         Date = c.DateTime(nullable: false),
                         OldValues = c.String(),
                         NewValues = c.String(),
-                        Type = c.String(),
+                        Type = c.Int(nullable: false),
                     })
                 .PrimaryKey(t => t.Id);
             
--- Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IConfigurationChangeService.cs
+++ Src/Server/MK.Booking/MK.Booking/ReadModel/Query/Contract/IConfigurationChangeService.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace apcurium.MK.Booking.ReadModel.Query.Contract
 {
     public interface IConfigurationChangeService
     {
         IList<ConfigurationChangeEntry> GetAll();
-        void Add(Dictionary<string, string> oldValues, Dictionary<string, string> newValues, ConfigurationChangeType type, string accountId, string email);
+        void Add(Dictionary<string, string> oldValues, Dictionary<string, string> newValues, ConfigurationChangeType type, Guid accountId, string email);
     }
 }
