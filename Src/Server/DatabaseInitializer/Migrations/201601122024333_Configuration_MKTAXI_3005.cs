63104cd75b16f2bfbd5c03e1c9dddd54bc7987fc
Dominique Savoie <dominique.savoie@apcurium.com>
2016-01-12 15:10:32 -0500

    MKTAXI-3005 => Migration conflict management

---
+++ Src/Server/DatabaseInitializer/Migrations/201601122024333_Configuration_MKTAXI_3005.cs
@@ -0,0 +1,18 @@
+namespace apcurium.MK.Common.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Configuration_MKTAXI_3005 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Config.PaymentSettings", "BraintreeClientSettings_DisplayPayPalLogo", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("Config.PaymentSettings", "BraintreeClientSettings_DisplayPayPalLogo");
+        }
+    }
+}
