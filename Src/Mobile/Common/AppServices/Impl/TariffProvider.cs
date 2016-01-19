ae579a789535f2510e0050d6c7294f6d9b8e8917
Dominique Savoie <dominique.savoie@apcurium.com>
2016-01-19 13:56:03 -0500

    MKTAXI-3799 => Modified Tariff provider to use the network override.

--- Src/Mobile/Common/AppServices/Impl/TariffProvider.cs
+++ Src/Mobile/Common/AppServices/Impl/TariffProvider.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using apcurium.MK.Booking.Api.Client.TaxiHail;
+using apcurium.MK.Booking.Api.Contract.Resources;
 using apcurium.MK.Booking.Mobile.Infrastructure;
 using apcurium.MK.Common.Entity;
 using apcurium.MK.Common.Provider;
@@ -21,6 +22,27 @@ namespace apcurium.MK.Booking.Mobile.AppServices.Impl
 
         public IEnumerable<Tariff> GetTariffs()
         {
+            //Case 1: If tariff is overridden by market settings if overriden in roaming market:
+            var marketSettings = new MarketSettings();
+
+            if (marketSettings.HashedMarket != null && marketSettings.OverrideEnableAppFareEstimates)
+            {
+                return new[]
+                {
+                    new Tariff()
+                    {
+                        Type = (int) TariffType.Default,
+                        FlatRate = marketSettings.FlatRate,
+                        KilometerIncluded = marketSettings.KilometerIncluded,
+                        KilometricRate = marketSettings.KilometricRate,
+                        MarginOfError = marketSettings.MarginOfError,
+                        MinimumRate = marketSettings.MinimumRate,
+                        PerMinuteRate = marketSettings.PerMinuteRate,
+                    },
+                };
+            }
+
+            //Case 2: Using tariff as set in local company.
             var cached = _cacheService.Get<Tariff[]>(CacheKey);
 			if (cached != null)
             {
