fdeca67caa61d2679037b24a48a16a72229cd5de
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2016-01-21 13:52:26 -0500

    Version Bump 3.2.30

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("3.2.29")]
-[assembly: AssemblyVersion("3.2.29")]
-[assembly: AssemblyFileVersion("3.2.29")]
+// [assembly: AssemblyVersion("3.2.30")]
+[assembly: AssemblyVersion("3.2.30")]
+[assembly: AssemblyFileVersion("3.2.30")]
 
