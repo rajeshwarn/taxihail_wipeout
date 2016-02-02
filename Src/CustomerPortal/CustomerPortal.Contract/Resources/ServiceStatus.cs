ac01a73322f07bdb2ad2df77633efdcad7a40e3d
Dominique Savoie <dominique.savoie@apcurium.com>
2016-02-02 13:28:11 -0500

    MKTAXI-3815 => ServiceStatus Resources.

---
+++ Src/CustomerPortal/CustomerPortal.Contract/Resources/ServiceStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomerPortal.Contract.Resources
+{
+    public class ServiceStatus
+    {
+        public bool IsIbsAvailable { get; set; }
+        public string IbsUrl { get; set; }
+        public bool? IsGeoAvailable { get; set; }
+        public string GeoUrl { get; set; }
+        public bool? IsHoneyBadgerAvailable { get; set; }
+        public string HoneyBadgerUrl { get; set; }
+        public bool IsSqlAvailable { get; set; }
+        public bool IsCustomerPortalAvailable { get; set; }
+        public string LastOrderUpdateId { get; set; }
+        public string LastOrderUpdateServer { get; set; }
+        public string LastOrderUpdateDate { get; set; }
+    }
+}
