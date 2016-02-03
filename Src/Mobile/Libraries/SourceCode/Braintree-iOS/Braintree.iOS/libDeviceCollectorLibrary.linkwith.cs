1d948e3da5f599f46d46d1c10f618a85bb0336eb
Dominique Savoie <dominique.savoie@apcurium.com>
2016-02-03 13:10:59 -0500

    MKTAXI-3005 => Removed braintree binding libraries (moved to Xamarin-bindings

--- Src/Mobile/Libraries/SourceCode/Braintree-iOS/Braintree.iOS/libDeviceCollectorLibrary.linkwith.cs
+++
@@ -1,9 +0,0 @@
-using ObjCRuntime;
-
-[assembly: LinkWith ("libDeviceCollectorLibrary.a", LinkTarget.Simulator 
-	| LinkTarget.Simulator64 
-	| LinkTarget.Arm64
-	| LinkTarget.ArmV7, 
-	ForceLoad = true,
-	Frameworks = "Accelerate SystemConfiguration Security AudioToolbox CoreLocation AVFoundation CoreMedia Foundation MessageUI MobileCoreServices UIKit",
-	IsCxx = true)] 
