4ffaa3cb30f906e33cbaa17ce5f09637286b03a9
Dominique Savoie <dominique.savoie@apcurium.com>
2016-02-05 10:07:44 -0500

    MKTAXI-3005 => Code cleaning (removed paypalsdks).

--- Src/Mobile/Libraries/SourceCode/PaypalSdkTouch.Unified/libPayPalMobile.linkwith.cs
+++
@@ -1,6 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libPayPalMobile.a", LinkTarget.ArmV7 | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64, SmartLink=true, ForceLoad = true, 
-                    Frameworks="AVFoundation AudioToolbox CoreLocation CoreMedia CoreVideo SystemConfiguration Security MessageUI OpenGLES MobileCoreServices",
-                    LinkerFlags="-lz -lxml2 -lc++ -lstdc++")]
