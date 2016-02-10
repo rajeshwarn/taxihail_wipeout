bacafec62911cdda28365d9333ccdc2730b70d19
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2016-02-10 08:24:15 -0500

    Version Bump 4.2.0

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.1.4")]
-[assembly: AssemblyVersion("4.1.4")]
-[assembly: AssemblyFileVersion("4.1.4")]
+// [assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyFileVersion("4.2.0")]
 
