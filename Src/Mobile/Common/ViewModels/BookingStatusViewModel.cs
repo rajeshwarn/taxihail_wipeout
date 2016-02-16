2b76a368ea1e5de32a9826e8092e18effe540201
Julia Vigo <julia.vigo@apcurium.com>
2016-02-16 12:23:50 -0500

    Code react

--- Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
+++ Src/Mobile/Common/ViewModels/BookingStatusViewModel.cs
@@ -981,7 +981,7 @@ namespace apcurium.MK.Booking.Mobile.ViewModels
 					StopOrientationServiceIfNeeded();
 				}
 
-				var statusInfoText = status.IBSStatusId.SoftEqual(VehicleStatuses.Common.NoShow) ? this.Services().Localize["NoShowMessage"] : status.IBSStatusDescription;
+				var statusInfoText = status.IBSStatusDescription;
 
 				var isLocalMarket = await _networkRoamingService.GetAndObserveMarketSettings()
 					.Select(marketSettings => marketSettings.IsLocalMarket)
