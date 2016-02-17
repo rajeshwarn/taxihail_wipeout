db98310abcb66abcff34cade7e8d2002fa75730c
Nicolas <Nicolas.Champanhac@nchampanhacpc.apcurium.local>
2016-02-17 11:43:38 -0500

    MKTAXI-3907 - Extra 'tolls' line in receipt for Arro

--- Src/Server/MK.Booking/MK.Booking/Services/Impl/NotificationService.cs
+++ Src/Server/MK.Booking/MK.Booking/Services/Impl/NotificationService.cs
@@ -697,7 +697,7 @@ namespace apcurium.MK.Booking.Services.Impl
                     ShowToll2 = hasCmtTollDetails && cmtRideLinqFields.Tolls.Length >= 2 && cmtRideLinqFields.Tolls.Length <= 4,
                     ShowToll3 = hasCmtTollDetails && cmtRideLinqFields.Tolls.Length >= 3 && cmtRideLinqFields.Tolls.Length <= 4,
                     ShowToll4 = hasCmtTollDetails && cmtRideLinqFields.Tolls.Length == 4,
-                    ShowTollTotal = (hasCmtTollDetails && cmtRideLinqFields.Tolls.Length > 4) || Math.Abs(toll) >= 0.01,
+                    ShowTollTotal = (hasCmtTollDetails && cmtRideLinqFields.Tolls.Length > 4) || (Math.Abs(toll) >= 0.01 && !hasCmtTollDetails),
                     ShowRideLinqLastFour = isCmtRideLinqReceipt,
                     ShowTripId = isCmtRideLinqReceipt,
                     ShowTax = Math.Abs(tax) >= 0.01 || isCmtRideLinqReceipt,
