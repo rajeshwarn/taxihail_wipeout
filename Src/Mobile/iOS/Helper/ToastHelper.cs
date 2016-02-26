a41710461120efe8640c18f6489e94a11416f4ec
Dominique Savoie <dominique.savoie@apcurium.com>
2016-02-26 12:18:27 -0500

    MKTAXI-3960 => Fixed issue with crash on startup for iOS.

--- Src/Mobile/iOS/Helper/ToastHelper.cs
+++ Src/Mobile/iOS/Helper/ToastHelper.cs
@@ -2,6 +2,10 @@
 using UIKit;
 using apcurium.MK.Booking.Mobile.Client.Controls;
 using apcurium.MK.Booking.Mobile.Client.Views;
+using System.Threading.Tasks;
+using apcurium.MK.Common;
+using Cirrious.CrossCore;
+using apcurium.MK.Booking.Mobile.Extensions;
 
 namespace apcurium.MK.Booking.Mobile.Client.Helper
 {
@@ -9,25 +13,43 @@ namespace apcurium.MK.Booking.Mobile.Client.Helper
     {
         public static ToastView Toast { get; set;}
 
-        public static bool Show(string message)
-        {
-            if(UIApplication.SharedApplication.Windows != null 
-                && UIApplication.SharedApplication.Windows.Length > 0 
-                && UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController != null
-                && UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController is SplashView)
-            {
-                return false;
-            }
+		public static bool Show(string message)
+		{
+			try
+			{
+				UIApplication.EnsureUIThread();
 
-            UIApplication.SharedApplication.InvokeOnMainThread(() =>
-                {
-                    Toast = new ToastView(message);
+				if(UIApplication.SharedApplication.Windows != null 
+					&& UIApplication.SharedApplication.Windows.Length > 0 
+					&& UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController != null
+					&& UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController is SplashView)
+				{
+					return false;
+				}
 
-                    Toast.Show();
-                });
+				UIApplication.SharedApplication.InvokeOnMainThread(() =>
+					{
+						Toast = new ToastView(message);
 
-            return true;
-        }
+						Toast.Show();
+					});
+
+				return true;
+			}
+			catch
+			{
+				// Attempted to show Toast before the app is are ready.
+				Task.Run(async () =>
+					{
+						// retrying asynchronously in 5 seconds to make sure the app is loaded
+						await Task.Delay(TimeSpan.FromSeconds(5));
+						var service = Mvx.Resolve<IConnectivityService>();
+						UIApplication.SharedApplication.InvokeOnMainThread(() => service.HandleToastInNewView());
+					}).FireAndForget();
+
+				return false;
+			}
+		}
 
         public static void Dismiss()
         {
