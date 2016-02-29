3e33c45e3c16ec5c931364fb0715854a64018e42
Dominique Savoie <dominique.savoie@apcurium.com>
2016-02-29 14:21:09 -0500

    MKTAXI-3960 => code react

--- Src/Mobile/iOS/Helper/ToastHelper.cs
+++ Src/Mobile/iOS/Helper/ToastHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UIKit;
 using apcurium.MK.Booking.Mobile.Client.Controls;
 using apcurium.MK.Booking.Mobile.Client.Views;
@@ -7,6 +8,8 @@ using apcurium.MK.Common;
 using Cirrious.CrossCore;
 using apcurium.MK.Booking.Mobile.Extensions;
 using apcurium.MK.Common.Diagnostic;
+using apcurium.MK.Common.Extensions;
+using MK.Common.Android.Extensions;
 
 namespace apcurium.MK.Booking.Mobile.Client.Helper
 {
@@ -14,14 +17,32 @@ namespace apcurium.MK.Booking.Mobile.Client.Helper
     {
         public static ToastView Toast { get; set;}
 
+        private static bool IsTopMostViewSplashScreen()
+        {
+            var topMostWindow = UIApplication.SharedApplication.Windows.LastOrDefault();
+
+            return topMostWindow.SelectOrDefault(window => window.RootViewController is SplashView);
+        }
+
 		public static bool Show(string message)
 		{
 			try
 			{
-				if(UIApplication.SharedApplication.Windows != null 
-					&& UIApplication.SharedApplication.Windows.Length > 0 
-					&& UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController != null
-					&& UIApplication.SharedApplication.Windows[UIApplication.SharedApplication.Windows.Length-1].RootViewController is SplashView)
+			    if (UIApplication.SharedApplication.Windows == null || UIApplication.SharedApplication.Windows.None())
+			    {
+                    // Will retry to show toast since we might not be ready yet.
+                    Task.Run(async () =>
+                    {
+                        // retrying asynchronously in 5 seconds to make sure the app is loaded
+                        await Task.Delay(TimeSpan.FromSeconds(5));
+                        var service = Mvx.Resolve<IConnectivityService>();
+                        UIApplication.SharedApplication.InvokeOnMainThread(() => service.HandleToastInNewView());
+                    }).FireAndForget();
+
+			        return false;
+			    }
+
+				if(IsTopMostViewSplashScreen())
 				{
 					return false;
 				}
@@ -37,9 +58,12 @@ namespace apcurium.MK.Booking.Mobile.Client.Helper
 			}
 			catch(Exception ex)
 			{
-				Mvx.Resolve<ILogger>().LogError(ex);
-				// Retrying to show Toast.
-				Task.Run(async () =>
+                var logger = Mvx.Resolve<ILogger>();
+
+			    logger.LogMessage("An error occurred while attempting to show toast, will retry in 5 seconds");
+                logger.LogErrorWithCaller(ex);
+                // Will retry to show toast since we might not be ready yet.
+                Task.Run(async () =>
 					{
 						// retrying asynchronously in 5 seconds to make sure the app is loaded
 						await Task.Delay(TimeSpan.FromSeconds(5));
