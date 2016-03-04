59b856a60f7274668e0bdd886c6de90bf4376fa7
aproteau <alex.proteau@apcurium.com>
2016-03-04 12:29:40 -0500

    Version Bump 4.2.14

--- Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.2.11")]
-[assembly: AssemblyVersion("4.2.11")]
-[assembly: AssemblyFileVersion("4.2.11")]
+// [assembly: AssemblyVersion("4.2.14")]
+[assembly: AssemblyVersion("4.2.14")]
+[assembly: AssemblyFileVersion("4.2.14")]
 
 
--- Src/Server/apcurium.MK.Web/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.11")]
-[assembly: AssemblyFileVersion("4.2.11")]
+[assembly: AssemblyVersion("4.2.14")]
+[assembly: AssemblyFileVersion("4.2.14")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
