e0e6d4bc6b45368753597392c4f6ee541ae77654
Dominique Savoie <dominique.savoie@apcurium.com>
2016-03-04 16:02:42 -0500

    MKTAXI-3822 => update service continues.

--- Src/Server/MK.Booking.Api/Services/RulesService.cs
+++ Src/Server/MK.Booking.Api/Services/RulesService.cs
@@ -1,23 +1,25 @@
 ﻿#region
 
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Web;
 using apcurium.MK.Booking.Api.Contract.Requests;
 using apcurium.MK.Booking.Api.Contract.Resources;
 using apcurium.MK.Booking.Commands;
+using apcurium.MK.Booking.ReadModel;
 using apcurium.MK.Booking.ReadModel.Query.Contract;
 using apcurium.MK.Common;
 using apcurium.MK.Common.Entity;
 using AutoMapper;
 using Infrastructure.Messaging;
-using ServiceStack.Common.Web;
-using ServiceStack.ServiceInterface;
 
 #endregion
 
 namespace apcurium.MK.Booking.Api.Services
 {
-    public class RulesService : Service
+    public class RulesService : BaseApiService
     {
         private readonly ICommandBus _commandBus;
         private readonly IRuleDao _dao;
@@ -29,7 +31,7 @@ namespace apcurium.MK.Booking.Api.Services
         }
 
 
-        public object Get(RuleRequest request)
+        public IList<RuleDetail> Get()
         {
             return _dao.GetAll();
         }
@@ -38,17 +40,17 @@ namespace apcurium.MK.Booking.Api.Services
         {
 			if (request.ZoneRequired && request.ExcludeCircularZone)
 			{
-				throw new HttpError(HttpStatusCode.BadRequest, ErrorCode.Rule_TwoTypeZoneVerificationSelected.ToString());
+				throw new HttpException((int)HttpStatusCode.BadRequest, ErrorCode.Rule_TwoTypeZoneVerificationSelected.ToString());
 			}
 
             //Check if rate with same name already exists
             if ((request.Type != RuleType.Default) && (_dao.GetAll().Any(x => x.Name == request.Name)))
             {
-                throw new HttpError(HttpStatusCode.Conflict, ErrorCode.Rule_DuplicateName.ToString());
+                throw new HttpException((int)HttpStatusCode.Conflict, ErrorCode.Rule_DuplicateName.ToString());
             }
             if (_dao.GetAll().Any(x => x.Priority == request.Priority && x.Category == (int) request.Category))
             {
-                throw new HttpError(HttpStatusCode.Conflict, ErrorCode.Rule_InvalidPriority.ToString());
+                throw new HttpException((int)HttpStatusCode.Conflict, ErrorCode.Rule_InvalidPriority.ToString());
             }
 
             var command = Mapper.Map<CreateRule>(request);
@@ -61,18 +63,18 @@ namespace apcurium.MK.Booking.Api.Services
             };
         }
 
-        public object Put(RuleRequest request)
+        public void Put(RuleRequest request)
         {
 			if (request.ZoneRequired && request.ExcludeCircularZone)
 			{
-				throw new HttpError(HttpStatusCode.BadRequest, ErrorCode.Rule_TwoTypeZoneVerificationSelected.ToString());
+				throw new HttpException((int)HttpStatusCode.BadRequest, ErrorCode.Rule_TwoTypeZoneVerificationSelected.ToString());
 			}
 
             //Check if rate with same name already exists
             if ((request.Type != RuleType.Default) &&
                 (_dao.GetAll().Any(x => x.Id != request.Id && x.Name == request.Name)))
             {
-                throw new HttpError(HttpStatusCode.Conflict, ErrorCode.Rule_DuplicateName.ToString());
+                throw new HttpException((int)HttpStatusCode.Conflict, ErrorCode.Rule_DuplicateName.ToString());
             }
             //if (_dao.GetAll().Any(x => x.Priority == request.Priority && x.Category == (int)request.Category))
             if (
@@ -80,23 +82,19 @@ namespace apcurium.MK.Booking.Api.Services
                     .Where(x => x.Id != request.Id)
                     .Any(x => x.Priority == request.Priority && x.Category == (int) request.Category))
             {
-                throw new HttpError(HttpStatusCode.Conflict, ErrorCode.Rule_InvalidPriority.ToString());
+                throw new HttpException((int)HttpStatusCode.Conflict, ErrorCode.Rule_InvalidPriority.ToString());
             }
 
             var command = Mapper.Map<UpdateRule>(request);
 
             _commandBus.Send(command);
-
-            return new HttpResult(HttpStatusCode.OK, "OK");
         }
 
-        public object Delete(RuleRequest request)
+        public void Delete(Guid ruleId)
         {
-            var command = new DeleteRule {CompanyId = AppConstants.CompanyId, RuleId = request.Id};
+            var command = new DeleteRule {CompanyId = AppConstants.CompanyId, RuleId = ruleId };
 
             _commandBus.Send(command);
-
-            return new HttpResult(HttpStatusCode.OK, "OK");
         }
     }
 }
