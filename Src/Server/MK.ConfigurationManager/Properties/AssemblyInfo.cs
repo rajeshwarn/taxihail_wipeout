1ce6590cb4364a52fe6771247fda44d5bcd0930e
aproteau <alex.proteau@apcurium.com>
2016-03-04 10:43:32 -0500

    Version Bump 4.2.13

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.2.12")]
-[assembly: AssemblyVersion("4.2.12")]
-[assembly: AssemblyFileVersion("4.2.12")]
+// [assembly: AssemblyVersion("4.2.13")]
+[assembly: AssemblyVersion("4.2.13")]
+[assembly: AssemblyFileVersion("4.2.13")]
 
--- Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.2.12")]
-[assembly: AssemblyVersion("4.2.12")]
-[assembly: AssemblyFileVersion("4.2.12")]
+// [assembly: AssemblyVersion("4.2.13")]
+[assembly: AssemblyVersion("4.2.13")]
+[assembly: AssemblyFileVersion("4.2.13")]
 
 
 
