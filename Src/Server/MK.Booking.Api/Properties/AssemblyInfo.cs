90b480959b6e4ac66e111e41e0991bed4010471e
APCURIUM\Alexandre.Marcoux <alexandre.marcoux@apcurium.com>
2016-03-09 12:55:10 -0500

    Version Bump 4.3.9

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("4.3.7")]
-[assembly: AssemblyVersion("4.3.7")]
-[assembly: AssemblyFileVersion("4.3.7")]
+// [assembly: AssemblyVersion("4.3.9")]
+[assembly: AssemblyVersion("4.3.9")]
+[assembly: AssemblyFileVersion("4.3.9")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
