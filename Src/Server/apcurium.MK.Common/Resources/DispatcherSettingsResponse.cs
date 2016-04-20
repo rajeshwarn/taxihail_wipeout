42d1558d30808c4122040c2eb4b3c979de5b7749
Nicolas <Nicolas.Champanhac@apcurium.local>
2016-04-20 14:01:01 -0400

    compilation issues solved

---
+++ Src/Server/apcurium.MK.Common/Resources/DispatcherSettingsResponse.cs
@@ -0,0 +1,13 @@
+namespace apcurium.MK.Common.Resources
+{
+    public class DispatcherSettingsResponse
+    {
+        public string Market { get; set; }
+
+        public int NumberOfOffersPerCycle { get; set; }
+
+        public int NumberOfCycles { get; set; }
+
+        public int DurationOfOfferInSeconds { get; set; }
+    }
+}
