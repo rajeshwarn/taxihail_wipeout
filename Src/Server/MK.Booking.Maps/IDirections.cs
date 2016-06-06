10c10233f6d44313b40d2bb293f3f000ff846031
Jonathan DeBruin <jdebruin@mobile-knowledge.com>
2016-06-06 12:59:58 -0400

    ARRO-1063 - Arro Taxihail 3.5.13 crashes when calculating Fare Estimate ARRO-1062 - Royal Taxi Changing Destination...

--- Src/Server/MK.Booking.Maps/IDirections.cs
+++ Src/Server/MK.Booking.Maps/IDirections.cs
@@ -6,10 +6,10 @@ namespace apcurium.MK.Booking.Maps
 {
     public interface IDirections
     {
-        Direction GetDirection(double? originLat, double? originLng, double? destLat, double? destLng, 
+        Direction GetDirection(double originLat, double originLng, double destLat, double destLng, 
             int? vehicleTypeId = null, DateTime? date = default(DateTime?), bool forEta = false, Tariff overriddenTariff = null);
 
-        Task<Direction> GetDirectionAsync(double? originLat, double? originLng, double? destLat, double? destLng, 
+        Task<Direction> GetDirectionAsync(double originLat, double originLng, double destLat, double destLng, 
             int? vehicleTypeId = null, DateTime? date = default(DateTime?), bool forEta = false, Tariff overriddenTariff = null);
 
         Direction GetEta(double originLat, double originLng, double destinationLat, double destinationLng);
