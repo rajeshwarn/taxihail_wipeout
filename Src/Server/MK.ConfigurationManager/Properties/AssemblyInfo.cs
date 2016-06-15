b0f1412d5e8b17c96f3d4c666b6385791eb9e8c1
Yves Rochon <yrochon@cmtgroup.com>
2016-06-15 13:42:03 -0400

    DevSetup tool messed up again :( fixed assemblyinfo.cs files again.

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.5.2")]
 [assembly: AssemblyVersion("0.5.3")]
-[assembly: AssemblyFileVersion"0.5.3")]
+[assembly: AssemblyFileVersion("0.5.3")]
 
--- Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
+++ Src/Server/apcurium.MK.Web.SelfHost/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.5.2")]
 [assembly: AssemblyVersion("0.5.3")]
-[assembly: AssemblyFileVersion"0.5.3")]
+[assembly: AssemblyFileVersion("0.5.3")]
 
 
 
