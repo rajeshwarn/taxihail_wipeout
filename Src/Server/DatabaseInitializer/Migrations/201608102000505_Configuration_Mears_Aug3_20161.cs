340b01ea945280e9c515d0e71761cf709c9def51
Yves Rochon <yrochon@cmtgroup.com>
2016-08-10 16:06:53 -0400

    Added, then later removed a field and the migration had not been updated.

---
+++ Src/Server/DatabaseInitializer/Migrations/201608102000505_Configuration_Mears_Aug3_20161.cs
@@ -0,0 +1,16 @@
+namespace apcurium.MK.Common.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Configuration_Mears_Aug3_20161 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
