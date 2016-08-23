a8542ba60b155c91b6aea14783a64a68a385a115
Yves Rochon <yrochon@cmtgroup.com>
2016-08-23 14:36:02 -0400

    Fixed groveRideNow.resx and version bump to 3.5.25

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.5.24")]
-[assembly: AssemblyVersion("3.5.24")]
-[assembly: AssemblyFileVersion("3.5.24")]
+[assembly: AssemblyVersion("3.5.25")]
+[assembly: AssemblyFileVersion("3.5.25")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
