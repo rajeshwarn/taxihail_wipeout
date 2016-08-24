b1458ab08de61c569ae945898edf472942573ebf
Yves Rochon <yrochon@cmtgroup.com>
2016-08-24 15:21:08 -0400

    Merged 2 if-elseif statements into 1 in the home activity, version bump to 3.5.27

--- Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.5.26")]
-[assembly: AssemblyVersion("3.5.26")]
-[assembly: AssemblyFileVersion("3.5.26")]
+[assembly: AssemblyVersion("3.5.27")]
+[assembly: AssemblyFileVersion("3.5.27")]
 
 
