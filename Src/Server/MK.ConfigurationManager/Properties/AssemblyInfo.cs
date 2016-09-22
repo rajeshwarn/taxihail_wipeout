0d526e20305eb1e0c4cbd9ab32d6bb1c954708f5
Yves Rochon <yrochon@cmtgroup.com>
2016-09-22 10:49:34 -0400

    Version bump to 3.5.40, android manifest file changes to specify location hardware permissions.

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.5.36")]
-[assembly: AssemblyVersion("3.5.39")]
-[assembly: AssemblyFileVersion("3.5.39")]
+[assembly: AssemblyVersion("3.5.40")]
+[assembly: AssemblyFileVersion("3.5.40")]
 
