5dec70d6e24b0a81f7fbca19a28d49881943c327
Yves Rochon <yrochon@cmtgroup.com>
2016-10-03 16:31:12 -0400

    Version bump to 3.5.43, created new filename for TAX-112 Laclede resource file.

--- Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
+++ Src/Server/MK.DeploymentService/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.5.36")]
-[assembly: AssemblyVersion("3.5.42")]
-[assembly: AssemblyFileVersion("3.5.42")]
+[assembly: AssemblyVersion("3.5.43")]
+[assembly: AssemblyFileVersion("3.5.43")]
 
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.xml", Watch = true)]
