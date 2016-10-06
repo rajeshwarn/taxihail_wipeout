f892683572cf6e26f552119830b5d1e108f4f8ef
Yves Rochon <yrochon@cmtgroup.com>
2016-10-06 10:29:39 -0400

    Version bump to 3.6.11, updated release notes

--- Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
+++ Src/Server/MK.ConfigurationManager/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("3.6.7")]
-[assembly: AssemblyVersion("3.6.10")]
-[assembly: AssemblyFileVersion("3.6.10")]
+[assembly: AssemblyVersion("3.6.11")]
+[assembly: AssemblyFileVersion("3.6.11")]
 
