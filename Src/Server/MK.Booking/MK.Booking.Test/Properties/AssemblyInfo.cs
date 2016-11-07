4529a3b5e93d8c2c5400116eacf96f6468714176
Yves Rochon <yrochon@cmtgroup.com>
2016-11-07 10:04:57 -0500

    Updated release notes, and version bump to 0.5.11

--- Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking/MK.Booking.Test/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.5.2")]
-[assembly: AssemblyVersion("0.5.10")]
-[assembly: AssemblyFileVersion("0.5.10")]
+[assembly: AssemblyVersion("0.5.11")]
+[assembly: AssemblyFileVersion("0.5.11")]
 
 
