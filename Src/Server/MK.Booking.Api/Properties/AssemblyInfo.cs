6afdda837d7ea156a6ba32f880f20ac6484d4f67
Yves Rochon <yrochon@cmtgroup.com>
2016-11-14 09:45:24 -0500

    Version bump to 0.5.12 and updated release notes for TAX-221, TAX-223 and TAX-224.

--- Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
+++ Src/Server/MK.Booking.Api/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("0.5.2")]
-[assembly: AssemblyVersion("0.5.11")]
-[assembly: AssemblyFileVersion("0.5.11")]
+[assembly: AssemblyVersion("0.5.12")]
+[assembly: AssemblyFileVersion("0.5.12")]
 
 //[assembly: Log] 
 //[assembly: Log(AttributeTargetMembers="regex:get_.*|set_.*" , OnEntryOptions = LogOptions.IncludeParameterValue, OnSuccessOptions = LogOptions.IncludeReturnValue)]
